6ad8d5799b2d1f95420769cff74a6a3e7763921c
jinhai <haijin.chn@gmail.com>
2023-12-12 15:48:28 +0800

    Introduce actix web template
    Signed-off-by: jinhai <haijin.chn@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, web, App, HttpServer, Responder};
+
+#[get("/")]
+async fn index() -> impl Responder {
+    "Hello, World!"
+}
+
+#[get("/{name}")]
+async fn hello(name: web::Path<String>) -> impl Responder {
+    format!("Hello {}!", &name)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(index).service(hello))
+        .bind(("127.0.0.1", 9090))?
+        .run()
+        .await
 }
