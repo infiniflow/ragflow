f167a9a64655251edc7db02d57ac6e6475aaa80f
yangqianjuan <1072483500@qq.com>
2024-01-29 11:10:33 +0800

    feat: add react-dev-inspector to locate code in the IDE faster

---
+++ web/src/app.tsx
@@ -0,0 +1,6 @@
+import React, { ReactNode } from "react";
+import { Inspector } from "react-dev-inspector";
+
+export function rootContainer(container: ReactNode) {
+  return React.createElement(Inspector, null, container);
+}
