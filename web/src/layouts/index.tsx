eec529f8c5c6bf11d71aa218cdf93fb1475b4fbd
balibabu <cike8899@users.noreply.github.com>
2024-03-04 19:28:30 +0800

    feat: set width of chunk text to 100% and add Skeleton to Preview of document and remove react-pdf (#94)
    * feat: remove react-pdf
    
    * feat: add Skeleton to Preview of document
    
    * feat: set width of chunk text to 100%

--- web/src/layouts/index.tsx
+++ web/src/layouts/index.tsx
@@ -1,15 +1,14 @@
 import { Divider, Layout, theme } from 'antd';
 import React from 'react';
-import { useTranslation } from 'react-i18next';
 import { Outlet } from 'umi';
 import '../locales/config';
 import Header from './components/header';
+
 import styles from './index.less';
 
 const { Content } = Layout;
 
 const App: React.FC = () => {
-  const { t } = useTranslation();
   const {
     token: { colorBgContainer, borderRadiusLG },
   } = theme.useToken();
@@ -25,6 +24,7 @@ const App: React.FC = () => {
             background: colorBgContainer,
             borderRadius: borderRadiusLG,
             overflow: 'auto',
+            display: 'flex',
           }}
         >
           <Outlet />
