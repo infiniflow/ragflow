39f1feaccb39cd10a8909cfc2edd1619e7961865
加帆 <Jiafan@users.noreply.github.com>
2024-04-19 08:44:51 +0800

    Bug fix pdf parse index out of range (#440)
    ### What problem does this PR solve?
    
    fix a bug comes when parse some pdf file #436 
    
    ### Type of change
    
    - [☑️ ] Bug Fix (non-breaking change which fixes an issue)

--- deepdoc/parser/pdf_parser.py
+++ deepdoc/parser/pdf_parser.py
@@ -830,10 +830,13 @@ class HuParser:
         pn = [bx["page_number"]]
         top = bx["top"] - self.page_cum_height[pn[0] - 1]
         bott = bx["bottom"] - self.page_cum_height[pn[0] - 1]
-        if pn[-1] - 1 >= len(self.page_images): return ""
+        page_images_cnt = len(self.page_images)
+        if pn[-1] - 1 >= page_images_cnt: return ""
         while bott * ZM > self.page_images[pn[-1] - 1].size[1]:
             bott -= self.page_images[pn[-1] - 1].size[1] / ZM
             pn.append(pn[-1] + 1)
+            if pn[-1] - 1 >= page_images_cnt:
+                return ""
 
         return "@@{}\t{:.1f}\t{:.1f}\t{:.1f}\t{:.1f}##" \
             .format("-".join([str(p) for p in pn]),
