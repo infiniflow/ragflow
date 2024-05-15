34dcf05e144d0eb1f052fc47f8576790a10b98c7
balibabu <cike8899@users.noreply.github.com>
2024-05-15 14:53:36 +0800

    fix: .knowledgebase folder can be deleted bug and change "Add file to knowledge base" to "Link file to knowledge base" bug (#786)
    ### What problem does this PR solve?
    fix: .knowledgebase folder can be deleted bug 
    fix: change "Add file to knowledge base" to "Link file to knowledge
    base" bug
    #783 #784
    
    ### Type of change
    
    - [x] Bug Fix (non-breaking change which fixes an issue)

--- web/src/locales/zh-traditional.ts
+++ web/src/locales/zh-traditional.ts
@@ -458,7 +458,7 @@ export default {
       knowledgeBase: '知識庫',
       size: '大小',
       action: '操作',
-      addToKnowledge: '添加到知識庫',
+      addToKnowledge: '鏈接知識庫',
       pleaseSelect: '請選擇',
       newFolder: '新建文件夾',
       uploadFile: '上傳文件',
