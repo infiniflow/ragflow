a42e37d3cc2eaa93a1c464e817310f67382e100d
balibabu <cike8899@users.noreply.github.com>
2024-05-16 20:15:02 +0800

    feat: Support for conversational streaming (#809)
    ### What problem does this PR solve?
    
    feat: Support for conversational streaming
    #709
    
    ### Type of change
    
    
    - [x] New Feature (non-breaking change which adds functionality)

--- web/src/pages/chat/share/index.tsx
+++ web/src/pages/chat/share/index.tsx
@@ -1,51 +1,11 @@
-import { useEffect } from 'react';
-import {
-  useCreateSharedConversationOnMount,
-  useSelectCurrentSharedConversation,
-  useSendSharedMessage,
-} from '../shared-hooks';
 import ChatContainer from './large';
 
 import styles from './index.less';
 
 const SharedChat = () => {
-  const { conversationId } = useCreateSharedConversationOnMount();
-  const {
-    currentConversation,
-    addNewestConversation,
-    removeLatestMessage,
-    ref,
-    loading,
-    setCurrentConversation,
-  } = useSelectCurrentSharedConversation(conversationId);
-
-  const {
-    handlePressEnter,
-    handleInputChange,
-    value,
-    loading: sendLoading,
-  } = useSendSharedMessage(
-    currentConversation,
-    addNewestConversation,
-    removeLatestMessage,
-    setCurrentConversation,
-  );
-
-  useEffect(() => {
-    console.info(location.href);
-  }, []);
-
   return (
     <div className={styles.chatWrapper}>
-      <ChatContainer
-        value={value}
-        handleInputChange={handleInputChange}
-        handlePressEnter={handlePressEnter}
-        loading={loading}
-        sendLoading={sendLoading}
-        conversation={currentConversation}
-        ref={ref}
-      ></ChatContainer>
+      <ChatContainer></ChatContainer>
     </div>
   );
 };
--- web/src/pages/chat/utils.ts
+++ web/src/pages/chat/utils.ts
@@ -1,5 +1,7 @@
+import { MessageType } from '@/constants/chat';
 import { IConversation, IReference } from '@/interfaces/database/chat';
 import { EmptyConversationId, variableEnabledFieldMap } from './constants';
+import { IClientConversation, IMessage } from './interface';
 
 export const excludeUnEnabledVariables = (values: any) => {
   const unEnabledFields: Array<keyof typeof variableEnabledFieldMap> =
@@ -20,7 +22,7 @@ export const getDocumentIdsFromConversionReference = (data: IConversation) => {
   const documentIds = data.reference.reduce(
     (pre: Array<string>, cur: IReference) => {
       cur.doc_aggs
-        .map((x) => x.doc_id)
+        ?.map((x) => x.doc_id)
         .forEach((x) => {
           if (pre.every((y) => y !== x)) {
             pre.push(x);
@@ -31,4 +33,21 @@ export const getDocumentIdsFromConversionReference = (data: IConversation) => {
     [],
   );
   return documentIds.join(',');
+};
+
+export const buildMessageItemReference = (
+  conversation: IClientConversation,
+  message: IMessage,
+) => {
+  const assistantMessages = conversation.message
+    ?.filter((x) => x.role === MessageType.Assistant)
+    .slice(1);
+  const referenceIndex = assistantMessages.findIndex(
+    (x) => x.id === message.id,
+  );
+  const reference = message?.reference
+    ? message?.reference
+    : conversation.reference[referenceIndex];
+
+  return reference;
 };
