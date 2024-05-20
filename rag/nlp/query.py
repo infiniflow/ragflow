2b362837127c47c5c3899eb32f6d2aa32892eee3
KevinHuSh <kevinhu.sh@gmail.com>
2024-05-20 12:23:51 +0800

    fix english query bug (#840)
    ### What problem does this PR solve?
    
    #834 
    
    ### Type of change
    
    - [x] Bug Fix (non-breaking change which fixes an issue)

--- rag/nlp/query.py
+++ rag/nlp/query.py
@@ -36,7 +36,7 @@ class EsQueryer:
         patts = [
             (r"是*(什么样的|哪家|一下|那家|啥样|咋样了|什么时候|何时|何地|何人|是否|是不是|多少|哪里|怎么|哪儿|怎么样|如何|哪些|是啥|啥是|啊|吗|呢|吧|咋|什么|有没有|呀)是*", ""),
             (r"(^| )(what|who|how|which|where|why)('re|'s)? ", " "),
-            (r"(^| )('s|'re|is|are|were|was|do|does|did|don't|doesn't|didn't|has|have|be|there|you|me|your|my|mine|just|please|may|i|should|would|wouldn't|will|won't|done|go|for|with|so|the|a|an|by|i'm|it's|he's|she's|they|they're|you're|as|by|on|in|at|up|out|down)", " ")
+            (r"(^| )('s|'re|is|are|were|was|do|does|did|don't|doesn't|didn't|has|have|be|there|you|me|your|my|mine|just|please|may|i|should|would|wouldn't|will|won't|done|go|for|with|so|the|a|an|by|i'm|it's|he's|she's|they|they're|you're|as|by|on|in|at|up|out|down) ", " ")
         ]
         for r, p in patts:
             txt = re.sub(r, p, txt, flags=re.IGNORECASE)
@@ -44,7 +44,7 @@ class EsQueryer:
 
     def question(self, txt, tbl="qa", min_match="60%"):
         txt = re.sub(
-            r"[ \r\n\t,，。？?/`!！&]+",
+            r"[ \r\n\t,，。？?/`!！&\^%%]+",
             " ",
             rag_tokenizer.tradi2simp(
                 rag_tokenizer.strQ2B(
@@ -53,9 +53,10 @@ class EsQueryer:
 
         if not self.isChinese(txt):
             tks = rag_tokenizer.tokenize(txt).split(" ")
-            q = copy.deepcopy(tks)
-            for i in range(1, len(tks)):
-                q.append("\"%s %s\"^2" % (tks[i - 1], tks[i]))
+            tks_w = self.tw.weights(tks)
+            q = [re.sub(r"[ \\\"']+", "", tk)+"^{:.4f}".format(w) for tk, w in tks_w]
+            for i in range(1, len(tks_w)):
+                q.append("\"%s %s\"^%.4f" % (tks_w[i - 1][0], tks_w[i][0], max(tks_w[i - 1][1], tks_w[i][1])*2))
             if not q:
                 q.append(txt)
             return Q("bool",
