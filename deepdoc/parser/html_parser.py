b3798601395c95d35bcbccf3f5ca09752124ae79
Kevin Hu <kevinhu.sh@gmail.com>
2024-05-30 09:22:25 +0800

    fix create dialog bug

--- deepdoc/parser/html_parser.py
+++ deepdoc/parser/html_parser.py
@@ -1,4 +1,16 @@
 # -*- coding: utf-8 -*-
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
 from rag.nlp import find_codec
 import readability
 import html_text
@@ -8,7 +20,7 @@ def get_encoding(file):
     with open(file,'rb') as f:
         tmp = chardet.detect(f.read())
         return tmp['encoding']
-    
+
 class RAGFlowHtmlParser:
     def __call__(self, fnm, binary=None):
         txt = ""
@@ -18,7 +30,7 @@ class RAGFlowHtmlParser:
         else:
             with open(fnm, "r",encoding=get_encoding(fnm)) as f:
                 txt = f.read()
-            
+
         html_doc = readability.Document(txt)
         title = html_doc.title()
         content = html_text.extract_text(html_doc.summary(html_partial=True))
