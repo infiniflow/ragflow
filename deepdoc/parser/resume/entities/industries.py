117a72cefa00f23b429592f96c607b68a0e37889
Jin Hai <haijin.chn@gmail.com>
2024-06-01 07:42:35 +0800

    Add license
    Signed-off-by: Jin Hai <haijin.chn@gmail.com>

--- deepdoc/parser/resume/entities/industries.py
+++ deepdoc/parser/resume/entities/industries.py
@@ -1,3 +1,15 @@
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
 
 TBL = {"1":{"name":"IT/通信/电子","parent":"0"},
 "2":{"name":"互联网","parent":"0"},
