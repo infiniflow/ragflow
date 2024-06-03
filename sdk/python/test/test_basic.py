192bde2a969215cb8463ff7a91676a7749d8f770
cecilia-uu <konghui1996@163.com>
2024-06-03 20:24:29 +0800

    added delete_dataset method

--- sdk/python/test/test_basic.py
+++ sdk/python/test/test_basic.py
@@ -10,3 +10,6 @@ class TestCase(TestSdk):
 
     def test_create_dataset(self):
         assert ragflow.ragflow.RAGFLow('123', 'url').create_dataset('abc') == 'abc'
+
+    def test_delete_dataset(self):
+        assert ragflow.ragflow.RAGFLow('123', 'url').delete_dataset('abc') == 'abc'
