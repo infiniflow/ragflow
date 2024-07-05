edc61e9b4c7510f8a1c6982ddf73c32db5d1d65e
balibabu <cike8899@users.noreply.github.com>
2024-07-05 15:52:24 +0800

    feat: save the parameters of the generate operator to the form field … (#1390)
    ### What problem does this PR solve?
    feat: save the parameters of the generate operator to the form field of
    the node #918
    
    ### Type of change
    
    
    - [x] New Feature (non-breaking change which adds functionality)

--- web/src/locales/en.ts
+++ web/src/locales/en.ts
@@ -570,12 +570,15 @@ The above is the content you need to summarize.`,
       retrievalDescription: `This component is for the process of retrieving relevent information from knowledge base. So, knowledgebases should be selected. If there's nothing retrieved, the 'Empty response' will be returned.`,
       generateDescription: `This component is used to call LLM to generate text. Be careful about the prompt setting.`,
       categorizeDescription: `This component is used to categorize text. Please specify the name, description and examples of the category. Every single category leads to different downstream components.`,
-      relevantDescription: `This component is used to judge if the retrieved information is relevent to user's question. 'Yes' represents that they're relevant. 'No' represents they're irrelevant.`,
+      relevantDescription: `This component is used to judge if the output of upstream is relevent to user's latest question. 'Yes' represents that they're relevant. 'No' represents they're irrelevant.`,
       rewriteQuestionDescription: `This component is used to refine user's quesion. Typically, when a user's original question can't retrieve relevant information from knowledge base, this component help you change the question into a proper one which might be more consistant with the expressions in knowledge base. Only 'Retrieval' can be its downstreams.`,
       messageDescription:
         'This component is used to send user static information.',
       keywordDescription:
         'This component is used to send user static information.',
+      promptText: `Please summarize the following paragraphs. Be careful with the numbers, do not make things up. Paragraphs as following:
+        {input}
+  The above is the content you need to summarize.`,
     },
     footer: {
       profile: 'All rights reserved @ React',
