9bf6f7c9a0ee0ef875aed675056aeb819451a3dd
Kevin Hu <kevinhu.sh@gmail.com>
2024-07-17 12:28:54 +0800

    refine generate (#1562)
    ### What problem does this PR solve?
    
    
    
    ### Type of change
    
    - [x] Refactoring

--- graph/component/generate.py
+++ graph/component/generate.py
@@ -72,7 +72,7 @@ class Generate(ComponentBase):
         prompt = self._param.prompt
 
         retrieval_res = self.get_input()
-        input = "\n- ".join(retrieval_res["content"])
+        input = "\n- ".join(retrieval_res["content"]) if "content" in retrieval_res else ""
         for para in self._param.parameters:
             cpn = self._canvas.get_component(para["component_id"])["obj"]
             _, out = cpn.output(allow_partial=False)
