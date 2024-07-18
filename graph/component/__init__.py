1a98e6c23c780977e973ff8c1055731eb7ff0e5f
H <43509927+guoyuhao2330@users.noreply.github.com>
2024-07-18 14:02:14 +0800

    Update __init__.py

--- graph/component/__init__.py
+++ graph/component/__init__.py
@@ -12,7 +12,6 @@ from .keyword import KeywordExtract, KeywordExtractParam
 from .baidu import Baidu, BaiduParam
 from .duckduckgo import DuckDuckGo, DuckDuckGoParam
 from .wikipedia import Wikipedia, WikipediaParam
-from .arxiv import ArXiv, ArXivParam
 
 
 def component_class(class_name):
