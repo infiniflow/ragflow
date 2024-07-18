b4709a598c25cb6f5f06d3e4393144e4e1b3fc7a
narendra-bluebash <narendra.dangi@bluebash.co>
2024-07-18 16:26:13 +0530

    fix [Bug]: mistake in ragflow/rag/nlp/query.py this file #1595

--- rag/nlp/query.py
+++ rag/nlp/query.py
@@ -73,7 +73,7 @@ class EsQueryer:
             tks_w = [(re.sub(r"^[\+-]", "", tk), w) for tk, w in tks_w if tk]
             q = ["{}^{:.4f}".format(tk, w) for tk, w in tks_w if tk]
             for i in range(1, len(tks_w)):
-                q.append("\"%s %s\"^%.4f" % (tks_w[i - 1][0], tks_w[i][0], max(tks_w[i - 1][1], tks_w[i][1])*2))
+                q.append("%s %s^%.4f" % (tks_w[i - 1][0], tks_w[i][0], max(tks_w[i - 1][1], tks_w[i][1])*2))
             if not q:
                 q.append(txt)
             return Q("bool",
