ca93f9e8bd944cb7f59f54bdd03bb1b7f66aad7d
hwzhuhao <923196325@qq.com>
2024-07-20 16:57:41 +0800

    modify the encryption to first perform base64 encoding and then encrypt

--- api/utils/t_crypt.py
+++ api/utils/t_crypt.py
@@ -11,10 +11,11 @@ def crypt(line):
         file_utils.get_project_base_directory(),
         "conf",
         "public.pem")
-    rsa_key = RSA.importKey(open(file_path).read())
+    rsa_key = RSA.importKey(open(file_path).read(),"Welcome")
     cipher = Cipher_pkcs1_v1_5.new(rsa_key)
-    return base64.b64encode(cipher.encrypt(
-        line.encode('utf-8'))).decode("utf-8")
+    password_base64 = base64.b64encode(line.encode('utf-8')).decode("utf-8")
+    encrypted_password = cipher.encrypt(password_base64.encode())
+    return base64.b64encode(encrypted_password).decode('utf-8')
 
 
 if __name__ == "__main__":
