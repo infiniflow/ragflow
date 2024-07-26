549d67e281352e0ce1025966e35de39e7fd76b14
balibabu <cike8899@users.noreply.github.com>
2024-07-26 19:00:19 +0800

    fix: test chunk by @tanstack/react-query #1306 (#1719)
    ### What problem does this PR solve?
    
    fix: test chunk by @tanstack/react-query #1306
    
    ### Type of change
    
    - [x] Bug Fix (non-breaking change which fixes an issue)

--- web/src/pages/add-knowledge/components/knowledge-testing/index.tsx
+++ web/src/pages/add-knowledge/components/knowledge-testing/index.tsx
@@ -1,7 +1,5 @@
 import { useTestChunkRetrieval } from '@/hooks/knowledge-hooks';
 import { Flex, Form } from 'antd';
-import { useEffect } from 'react';
-import { useDispatch } from 'umi';
 import TestingControl from './testing-control';
 import TestingResult from './testing-result';
 
@@ -9,23 +7,16 @@ import styles from './index.less';
 
 const KnowledgeTesting = () => {
   const [form] = Form.useForm();
-  const testChunk = useTestChunkRetrieval();
+  const { testChunk } = useTestChunkRetrieval();
 
-  const dispatch = useDispatch();
-
-  const handleTesting = async () => {
+  const handleTesting = async (documentIds: string[] = []) => {
     const values = await form.validateFields();
     testChunk({
       ...values,
+      doc_ids: Array.isArray(documentIds) ? documentIds : [],
       vector_similarity_weight: 1 - values.vector_similarity_weight,
     });
   };
-
-  useEffect(() => {
-    return () => {
-      dispatch({ type: 'testingModel/reset' });
-    };
-  }, [dispatch]);
 
   return (
     <Flex className={styles.testingWrapper} gap={16}>
