2544f6f6b43de8297f7b79c01b0a9f9e52d33986
Kevin Hu <kevinhu.sh@gmail.com>
2024-07-29 19:25:45 +0800

    Update graph/component/google.py

--- graph/component/google.py
+++ graph/component/google.py
@@ -13,7 +13,6 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 #
-import random
 from abc import ABC
 from serpapi import GoogleSearch
 import pandas as pd
