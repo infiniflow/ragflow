11dae8320db6c745388ad1582e75aa035780272f
Kevin Hu <kevinhu.sh@gmail.com>
2024-07-29 19:24:24 +0800

    Update graph/component/bing.py

--- graph/component/bing.py
+++ graph/component/bing.py
@@ -13,7 +13,6 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 #
-import random
 from abc import ABC
 from functools import partial
 import requests
