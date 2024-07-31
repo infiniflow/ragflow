07581e2f3cccd641f95c2fbf203486b64c32328c
android <android@gmail.com>
2024-07-31 15:34:11 +0700

    theme

--- web/src/theme/theme.ts
+++ web/src/theme/theme.ts
@@ -1,10 +1,10 @@
 module.exports = {
-  'primary-color': '#338AFF', // '#338AFF',
+  'primary-color': '#1c8eda', // '#338AFF',
   'border-radius-base': '4px',
   // 'menu-dark-color': '',
   // 'menu-dark-danger-color': '',
-  'menu-dark-bg': '#092140',
-  'menu-dark-item-active-bg': '#092140'
+  'menu-dark-bg': '#1c8eda',
+  'menu-dark-item-active-bg': '#1c8eda',
 
   // 'menu-dark-arrow-color': '',
   // 'menu-dark-inline-submenu-bg': '',
