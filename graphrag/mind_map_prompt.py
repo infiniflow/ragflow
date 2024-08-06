3fd7db40ea9ed303a5a9630c24daf7c36c647ad7
Kevin Hu <kevinhu.sh@gmail.com>
2024-08-06 09:24:53 +0800

    refine mindmap (#1817)
    ### What problem does this PR solve?
    
    #1594
    ### Type of change
    
    - [x] Refactoring

--- graphrag/mind_map_prompt.py
+++ graphrag/mind_map_prompt.py
@@ -14,28 +14,20 @@
 #  limitations under the License.
 #
 MIND_MAP_EXTRACTION_PROMPT = """
- - Role: You're a talent text processor.
+- Role: You're a talent text processor to summarize a piece of text into a mind map.
 
- - Step of task:
-   1. Generate a title for user's 'TEXT'。
-   2. Classify the 'TEXT' into sections as you see fit.
-   3. If the subject matter is really complex, split them into sub-sections. 
+- Step of task:
+  1. Generate a title for user's 'TEXT'。
+  2. Classify the 'TEXT' into sections of a mind map.
+  3. If the subject matter is really complex, split them into sub-sections and sub-subsections. 
+  4. Add a shot content summary of the bottom level section.
 
- - Output requirement:
-   - In language of 
-   - MUST IN FORMAT OF MARKDOWN
-   
-Output:
-## <Title>
-  <Section Name>
-  <Section Name>
-    <Subsection Name>
-    <Subsection Name>
-  <Section Name>
-    <Subsection Name>
-    
+- Output requirement:
+  - Always try to maximize the number of sub-sections. 
+  - In language of 'Text'
+  - MUST IN FORMAT OF MARKDOWN
+
 -TEXT-
 {input_text}
 
-Output:
 """
