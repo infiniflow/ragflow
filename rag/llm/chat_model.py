c4d67a94432ff9b02f346b0565a4713e06b220b8
Zhedong Cen <cenzhedong2@126.com>
2024-08-07 17:51:23 +0800

    fix LocalAI add bug

--- rag/llm/chat_model.py
+++ rag/llm/chat_model.py
@@ -102,7 +102,7 @@ class XinferenceChat(Base):
         if not base_url:
             raise ValueError("Local llm url cannot be None")
         if base_url.split("/")[-1] != "v1":
-            self.base_url = os.path.join(base_url, "v1")
+            base_url = os.path.join(base_url, "v1")
         key = "xxx"
         super().__init__(key, model_name, base_url)
 
@@ -373,8 +373,8 @@ class LocalAIChat(Base):
         if not base_url:
             raise ValueError("Local llm url cannot be None")
         if base_url.split("/")[-1] != "v1":
-            self.base_url = os.path.join(base_url, "v1")
-        self.client = OpenAI(api_key="empty", base_url=self.base_url)
+            base_url = os.path.join(base_url, "v1")
+        self.client = OpenAI(api_key="empty", base_url=base_url)
         self.model_name = model_name.split("___")[0]
 
 
