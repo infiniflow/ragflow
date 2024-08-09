d1ab5296e33a746d57db658314a39639b1c3c864
Zhedong Cen <cenzhedong2@126.com>
2024-08-09 10:10:39 +0800

    Enhance the robustness of the code

--- api/apps/llm_app.py
+++ api/apps/llm_app.py
@@ -22,7 +22,6 @@ from api.db.db_models import TenantLLM
 from api.utils.api_utils import get_json_result
 from rag.llm import EmbeddingModel, ChatModel, RerankModel,CvModel
 import requests
-import ast
 
 @manager.route('/factories', methods=['GET'])
 @login_required
@@ -114,7 +113,7 @@ def add_llm():
     if factory == "VolcEngine":
         # For VolcEngine, due to its special authentication method
         # Assemble volc_ak, volc_sk, endpoint_id into api_key
-        temp = list(ast.literal_eval(req["llm_name"]).items())[0]
+        temp = list(eval(req["llm_name"]).items())[0]
         llm_name = temp[0]
         endpoint_id = temp[1]
         api_key = '{' + f'"volc_ak": "{req.get("volc_ak", "")}", ' \
@@ -132,7 +131,7 @@ def add_llm():
         api_key = "xxxxxxxxxxxxxxx"
     elif factory == "OpenAI-API-Compatible":
         llm_name = req["llm_name"]+"___OpenAI-API"
-        api_key = req["api_key"]
+        api_key = req.get("api_key","xxxxxxxxxxxxxxx") 
     else:
         llm_name = req["llm_name"]
         api_key = "xxxxxxxxxxxxxxx"
