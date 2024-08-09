f2f326b2b9c78be4731041990b5944d4a31bb799
Zhedong Cen <cenzhedong2@126.com>
2024-08-09 17:45:19 +0800

    add support for Upstage

--- rag/llm/chat_model.py
+++ rag/llm/chat_model.py
@@ -988,3 +988,10 @@ class LeptonAIChat(Base):
         if not base_url:
             base_url = os.path.join("https://"+model_name+".lepton.run","api","v1")
         super().__init__(key, model_name, base_url)
+
+
+class UpstageChat(Base):
+    def __init__(self, key, model_name, base_url="https://api.upstage.ai/v1/solar"):
+        if not base_url:
+            base_url = "https://api.upstage.ai/v1/solar"
+        super().__init__(key, model_name, base_url)
