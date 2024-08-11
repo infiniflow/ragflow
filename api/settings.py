01eec6840255c2de8f742cf8e170a29f23e8cde4
Tuan <tuantd22@xanhsm.com>
2024-08-12 00:08:42 +0700

    LLM_FACTORY

--- api/settings.py
+++ api/settings.py
@@ -69,6 +69,7 @@ default_llm = {
         "embedding_model": "text-embedding-ada-002",
         "image2text_model": "gpt-4-vision-preview",
         "asr_model": "whisper-1",
+        "rerank_model": "",
     },
     "Azure-OpenAI": {
         "chat_model": "azure-gpt-35-turbo",
@@ -122,10 +123,10 @@ if LLM_FACTORY not in default_llm:
     print(
         "\33[91m【ERROR】\33[0m:",
         f"LLM factory {LLM_FACTORY} has not supported yet, switch to 'Tongyi-Qianwen/QWen' automatically, and please check the API_KEY in service_conf.yaml.")
-    LLM_FACTORY = "Tongyi-Qianwen"
+    LLM_FACTORY = "OpenAI"
 CHAT_MDL = default_llm[LLM_FACTORY]["chat_model"]
-EMBEDDING_MDL = default_llm["BAAI"]["embedding_model"]
-RERANK_MDL = default_llm["BAAI"]["rerank_model"]
+EMBEDDING_MDL = default_llm[LLM_FACTORY]["embedding_model"]
+RERANK_MDL = default_llm[LLM_FACTORY]["rerank_model"]
 ASR_MDL = default_llm[LLM_FACTORY]["asr_model"]
 IMAGE2TEXT_MDL = default_llm[LLM_FACTORY]["image2text_model"]
 
