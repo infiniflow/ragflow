1f61f295db585ebe689f9b6e9c3ce4897ae16828
bill <yibie_jingnian@163.com>
2024-08-13 17:14:42 +0800

    feat: Modify PerfXCloud name #1853

--- web/src/pages/user-setting/setting-model/constant.ts
+++ web/src/pages/user-setting/setting-model/constant.ts
@@ -25,11 +25,11 @@ export const IconMap = {
   'OpenAI-API-Compatible': 'openai-api',
   cohere: 'cohere',
   Lepton: 'lepton',
-  TogetherAI:'together-ai',
-  PerfXCould: 'perfx-could',
+  TogetherAI: 'together-ai',
+  PerfXCloud: 'perfx-could',
   Upstage: 'upstage',
-  "novita.ai": 'novita-ai',
-  "SILICONFLOW": 'siliconflow'
+  'novita.ai': 'novita-ai',
+  SILICONFLOW: 'siliconflow',
 };
 
 export const BedrockRegionList = [
