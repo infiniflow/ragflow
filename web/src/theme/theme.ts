72585b9d155093755e42c28ac57ac6477600cafe
Jin Hai <haijin.chn@gmail.com>
2024-08-15 00:08:22 +0800

    Format file format from Windows/dos to Unix
    Signed-off-by: Jin Hai <haijin.chn@gmail.com>

--- web/src/theme/theme.ts
+++ web/src/theme/theme.ts
@@ -4,7 +4,7 @@ module.exports = {
   // 'menu-dark-color': '',
   // 'menu-dark-danger-color': '',
   'menu-dark-bg': '#092140',
-  'menu-dark-item-active-bg': '#092140'
+  'menu-dark-item-active-bg': '#092140',
 
   // 'menu-dark-arrow-color': '',
   // 'menu-dark-inline-submenu-bg': '',
