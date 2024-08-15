7c8e9935e1f42292f42c1cceca05556e5fee0643
bill <yibie_jingnian@163.com>
2024-08-15 16:03:12 +0800

    feat: Add MessageInput to the external chat page #1880

--- web/src/utils/file-util.ts
+++ web/src/utils/file-util.ts
@@ -85,3 +85,16 @@ export const downloadFile = ({
   downloadElement.click();
   document.body.removeChild(downloadElement);
 };
+
+const Units = ['bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+
+export const formatBytes = (x: string | number) => {
+  let l = 0,
+    n = (typeof x === 'string' ? parseInt(x, 10) : x) || 0;
+
+  while (n >= 1024 && ++l) {
+    n = n / 1024;
+  }
+
+  return n.toFixed(n < 10 && l > 0 ? 1 : 0) + ' ' + Units[l];
+};
