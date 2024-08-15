6b3a40be5c8952c1aa5267282ad41876b87def34
Jin Hai <haijin.chn@gmail.com>
2024-08-15 09:17:36 +0800

    Format file format from Windows/dos to Unix (#1949)
    ### What problem does this PR solve?
    
    Related source file is in Windows/DOS format, they are format to Unix
    format.
    
    ### Type of change
    
    - [x] Refactoring
    
    Signed-off-by: Jin Hai <haijin.chn@gmail.com>

--- web/src/pages/404.jsx
+++ web/src/pages/404.jsx
@@ -2,15 +2,18 @@ import { Button, Result } from 'antd';
 import { history } from 'umi';
 
 const NoFoundPage = () => {
-  return (<Result
-    status="404"
-    title="404"
-    subTitle="页面未找到，请输入正确的地址。"
-    extra={< Button type="primary" onClick={() => history.push('/')}>
-      返回主页
-    </Button>}
-  />
-  )
+  return (
+    <Result
+      status="404"
+      title="404"
+      subTitle="页面未找到，请输入正确的地址。"
+      extra={
+        <Button type="primary" onClick={() => history.push('/')}>
+          返回主页
+        </Button>
+      }
+    />
+  );
 };
 
 export default NoFoundPage;
