48753300e4f8a420324431e041e12a4b38adbf59
Kevin Hu <kevinhu.sh@gmail.com>
2024-08-29 14:25:50 +0800

    Update api/apps/sdk/dataset.py

--- api/apps/sdk/dataset.py
+++ api/apps/sdk/dataset.py
@@ -130,7 +130,7 @@ def list_datasets(tenant_id):
     page_number = int(request.args.get("page", 1))
     items_per_page = int(request.args.get("page_size", 1024))
     orderby = request.args.get("orderby", "create_time")
-    desc = bool(request.args.get("desc"))
+    desc = bool(request.args.get("desc", True))
     tenants = TenantService.get_joined_tenants_by_user_id(tenant_id)
     kbs = KnowledgebaseService.get_by_tenant_ids(
         [m["tenant_id"] for m in tenants], tenant_id, page_number, items_per_page, orderby, desc)
