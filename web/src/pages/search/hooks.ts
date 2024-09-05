2431bc95c85be61ebe80fb62fd660638d95559e1
bill <yibie_jingnian@163.com>
2024-09-05 19:31:47 +0800

    feat: Hide search Tab

--- web/src/pages/search/hooks.ts
+++ web/src/pages/search/hooks.ts
@@ -1,19 +1,38 @@
+import { MessageType } from '@/constants/chat';
 import { useTestChunkRetrieval } from '@/hooks/knowledge-hooks';
 import { useSendMessageWithSse } from '@/hooks/logic-hooks';
 import api from '@/utils/api';
-import { useCallback } from 'react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
+import { IMessage } from '../chat/interface';
 
 export const useSendQuestion = (kbIds: string[]) => {
   const { send, answer, done } = useSendMessageWithSse(api.ask);
   const { testChunk, loading } = useTestChunkRetrieval();
+  const [sendingLoading, setSendingLoading] = useState(false);
+
+  const message: IMessage = useMemo(() => {
+    return {
+      id: '',
+      content: answer.answer,
+      role: MessageType.Assistant,
+      reference: answer.reference,
+    };
+  }, [answer]);
 
   const sendQuestion = useCallback(
     (question: string) => {
+      setSendingLoading(true);
       send({ kb_ids: kbIds, question });
       testChunk({ kb_id: kbIds, highlight: true, question });
     },
     [send, testChunk, kbIds],
   );
 
-  return { sendQuestion, answer, loading };
+  useEffect(() => {
+    if (done) {
+      setSendingLoading(false);
+    }
+  }, [done]);
+
+  return { sendQuestion, message, loading, sendingLoading };
 };
--- web/src/pages/search/index.tsx
+++ web/src/pages/search/index.tsx
@@ -1,11 +1,13 @@
-import { Card, Flex, Input, Layout, List, Space } from 'antd';
-import SearchSidebar from './sidebar';
-
 import HightLightMarkdown from '@/components/highlight-markdown';
 import { ImageWithPopover } from '@/components/image';
+import MessageItem from '@/components/message-item';
 import { useSelectTestingResult } from '@/hooks/knowledge-hooks';
+import { IReference } from '@/interfaces/database/chat';
+import { Card, Flex, Input, Layout, List, Space } from 'antd';
 import { useState } from 'react';
 import { useSendQuestion } from './hooks';
+import SearchSidebar from './sidebar';
+
 import styles from './index.less';
 
 const { Content } = Layout;
@@ -14,7 +16,8 @@ const { Search } = Input;
 const SearchPage = () => {
   const [checkedList, setCheckedList] = useState<string[]>([]);
   const list = useSelectTestingResult();
-  const { sendQuestion } = useSendQuestion(checkedList);
+  const { sendQuestion, message, sendingLoading } =
+    useSendQuestion(checkedList);
 
   return (
     <Layout className={styles.searchPage}>
@@ -31,6 +34,13 @@ const SearchPage = () => {
                 onSearch={sendQuestion}
                 size="large"
               />
+              <MessageItem
+                item={message}
+                nickname="You"
+                reference={message.reference ?? ({} as IReference)}
+                loading={sendingLoading}
+                index={0}
+              ></MessageItem>
               <List
                 dataSource={list.chunks}
                 renderItem={(item) => (
