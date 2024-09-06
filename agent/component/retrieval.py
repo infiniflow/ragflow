6787b3cae0487d9cd2850466e483551bceb7c517
wwwlll <wingjson@gmail.com>
2024-09-06 10:41:40 +0800

    Update retrieval.py

--- agent/component/retrieval.py
+++ agent/component/retrieval.py
@@ -1,11 +1,3 @@
-'''
-Author: wingjson wingjson@outlook.com
-Date: 2024-09-06 08:24:57
-LastEditors: wingjson wingjson@outlook.com
-LastEditTime: 2024-09-06 08:26:06
-FilePath: \undefinede:\common\python\railchat\knowoffical\ragflow\agent\component\retrieval.py
-Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
-'''
 #
 #  Copyright 2024 The InfiniFlow Authors. All Rights Reserved.
 #
