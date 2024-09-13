9d4bb5767c0ebf544c1517ff2b6b5bc65443f417
Kevin Hu <kevinhu.sh@gmail.com>
2024-09-13 17:03:51 +0800

    make highlight friendly to English (#2417)
    ### What problem does this PR solve?
    
    #2415
    
    ### Type of change
    
    - [x] Performance Improvement

--- rag/nlp/__init__.py
+++ rag/nlp/__init__.py
@@ -214,7 +214,7 @@ def is_english(texts):
     eng = 0
     if not texts: return False
     for t in texts:
-        if re.match(r"[a-zA-Z]{2,}", t.strip()):
+        if re.match(r"[ `a-zA-Z.,':;/\"?<>!\(\)-]", t.strip()):
             eng += 1
     if eng / len(texts) > 0.8:
         return True
