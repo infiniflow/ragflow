3473751cfd99695156c76d1a9a20f48ed56124e3
Kevin Hu <kevinhu.sh@gmail.com>
2024-09-13 17:18:17 +0800

    fix empty children in mindmap

--- graphrag/mind_map_extractor.py
+++ graphrag/mind_map_extractor.py
@@ -66,7 +66,7 @@ class MindMapExtractor:
             obj = [obj]
         if isinstance(obj, list):
             for i in obj: keyset.add(i)
-            return [{"id": re.sub(r"\*+", "", i), "children": []} for i in obj]
+            return [{"id": re.sub(r"\*+", "", i), "children": []} for i in obj if re.sub(r"\*+", "", i)]
         arr = []
         for k, v in obj.items():
             k = self._key(k)
