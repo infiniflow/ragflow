d3262ca3783db4145c777c1e33505b7232067c11
Kevin Hu <kevinhu.sh@gmail.com>
2024-09-14 14:17:03 +0800

    refine the warning message for rewrite component (#2429)
    ### What problem does this PR solve?
    
    
    ### Type of change
    
    - [x] Performance Improvement

--- agent/component/rewrite.py
+++ agent/component/rewrite.py
@@ -54,7 +54,7 @@ class RewriteQuestion(Generate, ABC):
             setattr(self, "_loop", 0)
         if self._loop >= self._param.loop:
             self._loop = 0
-            raise Exception("Maximum loop time exceeds. Can't find relevant information.")
+            raise Exception("Sorry! Nothing relevant found.")
         self._loop += 1
         q = "Question: "
         for r, c in self._canvas.history[::-1]:
