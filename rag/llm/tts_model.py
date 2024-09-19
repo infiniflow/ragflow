f6bfbf8b0ed60c8362e7fa411073f03baabb1283
liuhua <10215101452@stu.ecun.edu.cn>
2024-09-19 16:46:26 +0800

    OpenAITTS

--- rag/llm/tts_model.py
+++ rag/llm/tts_model.py
@@ -13,7 +13,7 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 #
-from openai import OpenAI
+
 import requests
 from typing import Annotated, Literal
 from abc import ABC
