8b093036ba3eb5288f8363954e603fbcf32cb690
bill <yibie_jingnian@163.com>
2024-09-24 15:20:16 +0800

    fix: Web code build fails on ARM machines #2554

--- web/.umirc.ts
+++ web/.umirc.ts
@@ -30,7 +30,7 @@ export default defineConfig({
   copy: ['src/conf.json'],
   proxy: {
     '/v1': {
-      target: 'http://127.0.0.1:9380/',
+      target: 'http://127.0.0.1:9456/',
       changeOrigin: true,
       ws: true,
       logger: console,
