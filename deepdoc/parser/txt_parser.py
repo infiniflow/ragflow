2e5462ca010f84d838db25264509d227adfd1552
yaoqiankun <410728991@qq.com>
2024-09-30 11:32:01 +0800

    refactor parser_txt

--- deepdoc/parser/txt_parser.py
+++ deepdoc/parser/txt_parser.py
@@ -10,6 +10,8 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 #
+import itertools
+
 from deepdoc.parser.utils import get_text
 from rag.nlp import num_tokens_from_string
 
@@ -23,30 +25,15 @@ class RAGFlowTxtParser:
     def parser_txt(cls, txt, chunk_token_num=128, delimiter="\n!?;。；！？"):
         if not isinstance(txt, str):
             raise TypeError("txt type should be str!")
-        cks = [""]
-        tk_nums = [0]
 
-        def add_chunk(t):
-            nonlocal cks, tk_nums, delimiter
-            tnum = num_tokens_from_string(t)
-            if tnum < 8:
-                pos = ""
-            if tk_nums[-1] > chunk_token_num:
-                cks.append(t)
-                tk_nums.append(tnum)
-            else:
-                cks[-1] += t
-                tk_nums[-1] += tnum
+        groups = itertools.groupby(txt, key=lambda x: x not in delimiter)
+        strs = ["".join(k) for k, v in groups if k]
+        cks, ck, num = [], "", 0
+        for i, s in enumerate(strs):
+            ck += s
+            num += num_tokens_from_string(s)
+            if num > chunk_token_num or i == len(strs) - 1:
+                cks.append(ck)
+                ck, num = "", 0
 
-        s, e = 0, 1
-        while e < len(txt):
-            if txt[e] in delimiter:
-                add_chunk(txt[s: e + 1])
-                s = e + 1
-                e = s + 1
-            else:
-                e += 1
-        if s < e:
-            add_chunk(txt[s: e + 1])
-
-        return [[c,""] for c in cks]
+        return [[c, ""] for c in cks]
