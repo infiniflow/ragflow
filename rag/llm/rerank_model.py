5594f021e9dfe559c864b2763e71313742ad5c31
Wang Baoling <chinamerp@gmail.com>
2024-10-09 16:55:50 +0800

    fix: Fail to access model(xxxxx).'results'

--- rag/llm/rerank_model.py
+++ rag/llm/rerank_model.py
@@ -155,7 +155,7 @@ class YoudaoRerank(DefaultRerank):
 class XInferenceRerank(Base):
     def __init__(self, key="xxxxxxx", model_name="", base_url=""):
         if base_url.split("/")[-1] != "v1":
-            base_url = os.path.join(base_url, "v1")
+            base_url = os.path.join(base_url, "v1/rerank")
         self.model_name = model_name
         self.base_url = base_url
         self.headers = {
