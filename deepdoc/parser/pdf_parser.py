bc3429836cc59a3c494c2daa9ecf2689c564a53b
Kevin Hu <kevinhu.sh@gmail.com>
2024-10-10 10:05:42 +0800

    Update deepdoc/parser/pdf_parser.py

--- deepdoc/parser/pdf_parser.py
+++ deepdoc/parser/pdf_parser.py
@@ -50,7 +50,7 @@ class RAGFlowPdfParser:
                 if torch.cuda.is_available():
                     self.updown_cnt_mdl.set_param({"device": "cuda"})
             except Exception as e:
-                pass
+                logging.error(str(e))
         try:
             model_dir = os.path.join(
                 get_project_base_directory(),
