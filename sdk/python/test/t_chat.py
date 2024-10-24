c5c6a8a20a92b50e72d74516c48f2b15da6e29ae
liuhua <10215101452@stu.ecun.edu.cn>
2024-10-24 16:17:32 +0800

    Fix some issues in API and test

--- sdk/python/test/t_chat.py
+++ sdk/python/test/t_chat.py
@@ -1,5 +1,4 @@
 from ragflow import RAGFlow, Chat
-import time
 HOST_ADDRESS = 'http://127.0.0.1:9380'
 
 def test_create_chat_with_name(get_api_key_fixture):
@@ -12,13 +11,10 @@ def test_create_chat_with_name(get_api_key_fixture):
     document = {"displayed_name":displayed_name,"blob":blob}
     documents = []
     documents.append(document)
-    doc_ids = []
     docs= kb.upload_documents(documents)
     for doc in docs:
-        doc_ids.append(doc.id)
-    kb.async_parse_documents(doc_ids)
-    time.sleep(60)
-    rag.create_chat("test_create", datasets=[kb])
+        doc.add_chunk("This is a test to add chunk")
+    rag.create_chat("test_create", dataset_ids=[kb.id])
 
 
 def test_update_chat_with_name(get_api_key_fixture):
@@ -31,13 +27,10 @@ def test_update_chat_with_name(get_api_key_fixture):
     document = {"displayed_name": displayed_name, "blob": blob}
     documents = []
     documents.append(document)
-    doc_ids = []
     docs = kb.upload_documents(documents)
     for doc in docs:
-        doc_ids.append(doc.id)
-    kb.async_parse_documents(doc_ids)
-    time.sleep(60)
-    chat = rag.create_chat("test_update", datasets=[kb])
+        doc.add_chunk("This is a test to add chunk")
+    chat = rag.create_chat("test_update", dataset_ids=[kb.id])
     chat.update({"name": "new_chat"})
 
 
@@ -51,17 +44,27 @@ def test_delete_chats_with_success(get_api_key_fixture):
     document = {"displayed_name": displayed_name, "blob": blob}
     documents = []
     documents.append(document)
-    doc_ids = []
     docs = kb.upload_documents(documents)
     for doc in docs:
-        doc_ids.append(doc.id)
-    kb.async_parse_documents(doc_ids)
-    time.sleep(60)
-    chat = rag.create_chat("test_delete", datasets=[kb])
+        doc.add_chunk("This is a test to add chunk")
+    chat = rag.create_chat("test_delete", dataset_ids=[kb.id])
     rag.delete_chats(ids=[chat.id])
 
+def test_list_chats_with_success(get_api_key_fixture):
     API_KEY = get_api_key_fixture
     rag = RAGFlow(API_KEY, HOST_ADDRESS)
+    kb = rag.create_dataset(name="test_delete_chat")
+    displayed_name = "ragflow.txt"
+    with open("./ragflow.txt", "rb") as file:
+        blob = file.read()
+    document = {"displayed_name": displayed_name, "blob": blob}
+    documents = []
+    documents.append(document)
+    docs = kb.upload_documents(documents)
+    for doc in docs:
+        doc.add_chunk("This is a test to add chunk")
+    rag.create_chat("test_list_1", dataset_ids=[kb.id])
+    rag.create_chat("test_list_2", dataset_ids=[kb.id])
     rag.list_chats()
 
 
--- sdk/python/test/t_session.py
+++ sdk/python/test/t_session.py
@@ -10,16 +10,13 @@ def test_create_session_with_success(get_api_key_fixture):
     displayed_name = "ragflow.txt"
     with open("./ragflow.txt", "rb") as file:
         blob = file.read()
-    document = {"displayed_name": displayed_name, "blob": blob}
+    document = {"displayed_name":displayed_name,"blob":blob}
     documents = []
     documents.append(document)
-    doc_ids = []
-    docs = kb.upload_documents(documents)
+    docs= kb.upload_documents(documents)
     for doc in docs:
-        doc_ids.append(doc.id)
-    kb.async_parse_documents(doc_ids)
-    time.sleep(60)
-    assistant = rag.create_chat(name="test_create_session", datasets=[kb])
+        doc.add_chunk("This is a test to add chunk")
+    assistant=rag.create_chat("test_create", dataset_ids=[kb.id])
     assistant.create_session()
 
 
@@ -30,16 +27,13 @@ def test_create_conversation_with_success(get_api_key_fixture):
     displayed_name = "ragflow.txt"
     with open("./ragflow.txt","rb") as file:
         blob = file.read()
-    document = {"displayed_name":displayed_name,"blob":blob}
+    document = {"displayed_name": displayed_name, "blob": blob}
     documents = []
     documents.append(document)
-    doc_ids = []
-    docs= kb.upload_documents(documents)
+    docs = kb.upload_documents(documents)
     for doc in docs:
-        doc_ids.append(doc.id)
-    kb.async_parse_documents(doc_ids)
-    time.sleep(60)
-    assistant = rag.create_chat(name="test_create_conversation", datasets=[kb])
+        doc.add_chunk("This is a test to add chunk")
+    assistant = rag.create_chat("test_create", dataset_ids=[kb.id])
     session = assistant.create_session()
     question = "What is AI"
     for ans in session.ask(question, stream=True):
@@ -57,13 +51,10 @@ def test_delete_sessions_with_success(get_api_key_fixture):
     document = {"displayed_name":displayed_name,"blob":blob}
     documents = []
     documents.append(document)
-    doc_ids = []
     docs= kb.upload_documents(documents)
     for doc in docs:
-        doc_ids.append(doc.id)
-    kb.async_parse_documents(doc_ids)
-    time.sleep(60)
-    assistant = rag.create_chat(name="test_delete_session", datasets=[kb])
+        doc.add_chunk("This is a test to add chunk")
+    assistant=rag.create_chat("test_create", dataset_ids=[kb.id])
     session = assistant.create_session()
     assistant.delete_sessions(ids=[session.id])
 
@@ -74,16 +65,13 @@ def test_update_session_with_name(get_api_key_fixture):
     displayed_name = "ragflow.txt"
     with open("./ragflow.txt","rb") as file:
         blob = file.read()
-    document = {"displayed_name":displayed_name,"blob":blob}
+    document = {"displayed_name": displayed_name, "blob": blob}
     documents = []
     documents.append(document)
-    doc_ids = []
-    docs= kb.upload_documents(documents)
+    docs = kb.upload_documents(documents)
     for doc in docs:
-        doc_ids.append(doc.id)
-    kb.async_parse_documents(doc_ids)
-    time.sleep(60)
-    assistant = rag.create_chat(name="test_update_session", datasets=[kb])
+        doc.add_chunk("This is a test to add chunk")
+    assistant = rag.create_chat("test_create", dataset_ids=[kb.id])
     session = assistant.create_session(name="old session")
     session.update({"name": "new session"})
 
@@ -98,13 +86,10 @@ def test_list_sessions_with_success(get_api_key_fixture):
     document = {"displayed_name":displayed_name,"blob":blob}
     documents = []
     documents.append(document)
-    doc_ids = []
     docs= kb.upload_documents(documents)
     for doc in docs:
-        doc_ids.append(doc.id)
-    kb.async_parse_documents(doc_ids)
-    time.sleep(60)
-    assistant = rag.create_chat(name="test_list_session", datasets=[kb])
+        doc.add_chunk("This is a test to add chunk")
+    assistant=rag.create_chat("test_create", dataset_ids=[kb.id])
     assistant.create_session("test_1")
     assistant.create_session("test_2")
     assistant.list_sessions()
