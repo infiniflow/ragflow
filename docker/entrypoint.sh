71ddee5d742a64b375fd229b86076ab92fd29974
Guido Schmutz <schmutz68@gmail.com>
2024-11-10 19:59:59 +0100

    update entrypoint.sh to replace environment variables in service_conf.yaml using a template file

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# replace env variables in the service_conf.yaml file
+while IFS= read -r line || [[ -n "$line" ]]; do
+    # Use eval to interpret the variable with default values
+    eval "echo \"$line\"" >> /ragflow/docker/service_conf.yaml
+done < /ragflow/docker/service_conf.yaml.template
+
 # unset http proxy which maybe set by docker daemon
 export http_proxy=""; export https_proxy=""; export no_proxy=""; export HTTP_PROXY=""; export HTTPS_PROXY=""; export NO_PROXY=""
 
