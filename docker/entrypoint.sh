2ced515925e5f542406309faa7179ac8b5123f1b
jinhai <haijin.chn@gmail.com>
2024-11-14 10:32:23 +0800

    Print version when RAGFlow server startup
    Signed-off-by: jinhai <haijin.chn@gmail.com>

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -7,6 +7,23 @@ while IFS= read -r line || [[ -n "$line" ]]; do
     eval "echo \"$line\"" >> /ragflow/conf/service_conf.yaml
 done < /ragflow/conf/service_conf.yaml.template
 
+# update RAGFlow version
+# Get the latest tag
+last_tag=$(git describe --tags --abbrev=0)
+# Get the number of commits from the last tag
+commit_count=$(git rev-list --count "$last_tag..HEAD")
+# Get the short commit id
+last_commit=$(git rev-parse --short HEAD)
+
+version_info=""
+if [ "$commit_count" -eq 0 ]; then
+    version_info=$last_tag
+else
+    printf -v version_info "%s(%s~%d)" "$last_commit" "$last_tag" $commit_count
+fi
+# Replace the version in the versions.py file
+sed -i "s/\"dev\"/\"$version_info\"/" /api/versions.py
+
 # unset http proxy which maybe set by docker daemon
 export http_proxy=""; export https_proxy=""; export no_proxy=""; export HTTP_PROXY=""; export HTTPS_PROXY=""; export NO_PROXY=""
 
