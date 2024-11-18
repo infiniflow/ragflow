a143ef5b899e1fcb3827c5e2c8b3ab1c17b26a0a
bill <yibie_jingnian@163.com>
2024-11-18 17:10:17 +0800

    feat: Show task_executor heartbeat #3409

--- web/src/pages/user-setting/setting-system/task-bar-chat.tsx
+++ web/src/pages/user-setting/setting-system/task-bar-chat.tsx
@@ -1,57 +1,47 @@
-import { TaskExecutorElapsed } from '@/interfaces/database/user-setting';
+import { TaskExecutorHeartbeatItem } from '@/interfaces/database/user-setting';
 import { Divider, Flex } from 'antd';
-import { max } from 'lodash';
 import {
   Bar,
   BarChart,
   CartesianGrid,
+  Legend,
+  Rectangle,
   ResponsiveContainer,
   Tooltip,
+  XAxis,
 } from 'recharts';
 
+import { formatDate, formatTime } from '@/utils/date';
+import dayjs from 'dayjs';
+import { get } from 'lodash';
 import styles from './index.less';
 
 interface IProps {
-  data: TaskExecutorElapsed;
+  data: Record<string, TaskExecutorHeartbeatItem[]>;
 }
 
-const getColor = (value: number) => {
-  if (value > 120) {
-    return 'red';
-  } else if (value <= 120 && value > 50) {
-    return '#faad14';
-  }
-  return '#52c41a';
-};
-
-const getMaxLength = (data: TaskExecutorElapsed) => {
-  const lengths = Object.keys(data).reduce<number[]>((pre, cur) => {
-    pre.push(data[cur].length);
-    return pre;
-  }, []);
-  return max(lengths) ?? 0;
-};
-
-const fillEmptyElementByMaxLength = (list: any[], maxLength: number) => {
-  if (list.length === maxLength) {
-    return list;
-  }
-  return list.concat(
-    new Array(maxLength - list.length).fill({
-      value: 0,
-      actualValue: 0,
-      fill: getColor(0),
-    }),
-  );
-};
-
-const CustomTooltip = ({ active, payload }: any) => {
+const CustomTooltip = ({ active, payload, ...restProps }: any) => {
   if (active && payload && payload.length) {
+    const taskExecutorHeartbeatItem: TaskExecutorHeartbeatItem = get(
+      payload,
+      '0.payload',
+      {},
+    );
     return (
-      <div className="custom-tooltip">
-        <p
-          className={styles.taskBarTooltip}
-        >{`${payload[0].payload.actualValue}`}</p>
+      <div className="bg-slate-50 p-2 rounded-md border border-indigo-100">
+        <div className="font-semibold text-lg">
+          {formatDate(restProps.label)}
+        </div>
+        {Object.entries(taskExecutorHeartbeatItem).map(([key, val], index) => {
+          return (
+            <div key={index} className="flex gap-1">
+              <span className="font-semibold">{`${key}: `}</span>
+              <span>
+                {key === 'now' || key === 'boot_at' ? formatDate(val) : val}
+              </span>
+            </div>
+          );
+        })}
       </div>
     );
   }
@@ -60,32 +50,56 @@ const CustomTooltip = ({ active, payload }: any) => {
 };
 
 const TaskBarChat = ({ data }: IProps) => {
-  const maxLength = getMaxLength(data);
-  return (
-    <Flex gap="middle" vertical>
-      {Object.keys(data).map((key) => {
-        const list = data[key].map((x) => ({
-          value: x > 120 ? 120 : x,
-          actualValue: x,
-          fill: getColor(x),
-        }));
-        const nextList = fillEmptyElementByMaxLength(list, maxLength);
-        return (
-          <Flex key={key} className={styles.taskBar} vertical>
-            <b className={styles.taskBarTitle}>ID: {key}</b>
-            <ResponsiveContainer>
-              <BarChart data={nextList} barSize={20}>
-                <CartesianGrid strokeDasharray="3 3" />
-                <Tooltip content={<CustomTooltip></CustomTooltip>} />
-                <Bar dataKey="value" />
-              </BarChart>
-            </ResponsiveContainer>
-            <Divider></Divider>
-          </Flex>
-        );
-      })}
-    </Flex>
-  );
+  return Object.entries(data).map(([key, val]) => {
+    const data = val.map((x) => ({
+      ...x,
+      now: dayjs(x.now).valueOf(),
+      failed: 5,
+    }));
+    const firstItem = data[0];
+    const lastItem = data[data.length - 1];
+
+    const domain = [firstItem.now, lastItem.now];
+
+    return (
+      <Flex key={key} className={styles.taskBar} vertical>
+        <div className="flex gap-8">
+          <b className={styles.taskBarTitle}>ID: {key}</b>
+          <b className={styles.taskBarTitle}>Lag: {lastItem.lag}</b>
+          <b className={styles.taskBarTitle}>Pending: {lastItem.pending}</b>
+        </div>
+        <ResponsiveContainer>
+          <BarChart data={data} barSize={20}>
+            <XAxis
+              dataKey="now"
+              type="number"
+              scale={'time'}
+              domain={domain}
+              tickFormatter={(x) => formatTime(x)}
+              allowDataOverflow
+              angle={60}
+              padding={{ left: 20, right: 20 }}
+              tickMargin={20}
+            />
+            <CartesianGrid strokeDasharray="3 3" />
+            <Tooltip content={<CustomTooltip></CustomTooltip>} />
+            <Legend wrapperStyle={{ bottom: -22 }} />
+            <Bar
+              dataKey="done"
+              fill="#8884d8"
+              activeBar={<Rectangle fill="pink" stroke="blue" />}
+            />
+            <Bar
+              dataKey="failed"
+              fill="#82ca9d"
+              activeBar={<Rectangle fill="gold" stroke="purple" />}
+            />
+          </BarChart>
+        </ResponsiveContainer>
+        <Divider></Divider>
+      </Flex>
+    );
+  });
 };
 
 export default TaskBarChat;
