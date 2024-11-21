9fcc75f1076a79a1e193c5504035a7a126ebaad6
liuhua <10215101452@stu.ecun.edu.cn>
2024-11-21 15:11:06 +0800

    fix bugs

--- sdk/python/test/test_sdk_api/t_document.py
+++ sdk/python/test/test_sdk_api/t_document.py
@@ -1,5 +1,5 @@
 from ragflow_sdk import RAGFlow
-from sdk.python.test.common import HOST_ADDRESS
+from ..common import HOST_ADDRESS
 import pytest
 
 def test_upload_document_with_success(get_api_key_fixture):
--- sdk/python/test/test_sdk_api/t_session.py
+++ sdk/python/test/test_sdk_api/t_session.py
@@ -1,5 +1,5 @@
 from ragflow_sdk import RAGFlow,Agent
-from sdk.python.test.common import HOST_ADDRESS
+from ..common import HOST_ADDRESS
 import pytest
 
 
