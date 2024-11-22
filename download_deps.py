a10c2f2effacb3598ce2f5b0c0ab0c3731ff76d8
YANGBoSunning <396122554@qq.com>
2024-11-22 21:54:39 +0800

    Fix: Solve the problem of model files in the image being soft links pointing to a non-existent address. #3584 (#3586)
    ### What problem does this PR solve?
    
    Fix: Solve the problem of model files in the image being soft links
    pointing to a non-existent address. #3584
    
    ### Type of change
    
    - [x] Bug Fix (non-breaking change which fixes an issue)
    
    Co-authored-by: Kevin Hu <kevinhu.sh@gmail.com>

--- download_deps.py
+++ download_deps.py
@@ -27,7 +27,7 @@ repos = [
 def download_model(repo_id):
     local_dir = os.path.abspath(os.path.join("huggingface.co", repo_id))
     os.makedirs(local_dir, exist_ok=True)
-    snapshot_download(repo_id=repo_id, local_dir=local_dir)
+    snapshot_download(repo_id=repo_id, local_dir=local_dir, local_dir_use_symlinks=False)
 
 
 if __name__ == "__main__":
