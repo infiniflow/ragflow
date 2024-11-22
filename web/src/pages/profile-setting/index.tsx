95576ff64d7e29d615ebe285345984803fa0a2ac
bill <yibie_jingnian@163.com>
2024-11-22 15:07:53 +0800

    Feat: Add ProfileSetting page  #3221

---
+++ web/src/pages/profile-setting/index.tsx
@@ -0,0 +1,34 @@
+import { Button } from '@/components/ui/button';
+import { ArrowLeft } from 'lucide-react';
+import { Outlet } from 'umi';
+import { useGetPageTitle } from './hooks';
+import { SideBar } from './sidebar';
+
+export default function ProfileSetting() {
+  const title = useGetPageTitle();
+  return (
+    <div className="flex flex-col w-full h-screen bg-background text-foreground">
+      <header className="flex items-center border-b">
+        <div className="flex items-center border-r p-1.5">
+          <Button variant="ghost" size="icon">
+            <ArrowLeft className="w-5 h-5" />
+          </Button>
+        </div>
+        <div className="p-4">
+          <h1 className="text-2xl font-semibold tracking-tight">
+            Profile & settings
+          </h1>
+        </div>
+      </header>
+
+      <div className="flex flex-1 bg-muted/50">
+        <SideBar></SideBar>
+
+        <main className="flex-1 p-10">
+          <h1 className="text-3xl font-bold mb-6"> {title}</h1>
+          <Outlet></Outlet>
+        </main>
+      </div>
+    </div>
+  );
+}
