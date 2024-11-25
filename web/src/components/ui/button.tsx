f5c13bc7b1dabed1a28247183a2eb5373c7ccb06
bill <yibie_jingnian@163.com>
2024-11-25 16:07:49 +0800

    Feat: Add PricingCard component #3221

--- web/src/components/ui/button.tsx
+++ web/src/components/ui/button.tsx
@@ -13,11 +13,13 @@ const buttonVariants = cva(
         destructive:
           'bg-destructive text-destructive-foreground hover:bg-destructive/90',
         outline:
-          'border border-colors-outline-sentiment-primary bg-background hover:bg-accent hover:text-accent-foreground',
+          'border border-input bg-background hover:bg-accent hover:text-accent-foreground',
         secondary:
           'bg-secondary text-secondary-foreground hover:bg-secondary/80',
         ghost: 'hover:bg-accent hover:text-accent-foreground',
         link: 'text-primary underline-offset-4 hover:underline',
+        tertiary:
+          'bg-colors-text-core-standard text-foreground hover:bg-colors-text-core-standard/80',
       },
       size: {
         default: 'h-10 px-4 py-2',
