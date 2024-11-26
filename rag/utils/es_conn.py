2d8fa205d3d209074c6e2e8f46d5159a7757ec92
liuhua <10215101452@stu.ecun.edu.cn>
2024-11-26 18:33:40 +0800

    Fix enable/disable bug

--- rag/utils/es_conn.py
+++ rag/utils/es_conn.py
@@ -294,10 +294,10 @@ class ESConnection(DocStoreConnection):
                         f"Condition `{str(k)}={str(v)}` value type is {str(type(v))}, expected to be int, str or list.")
             scripts = []
             for k, v in newValue.items():
-                if not isinstance(k, str) or not v:
+                if (not isinstance(k, str) or not v) and k != "available_int":
                     continue
                 if isinstance(v, str):
-                    scripts.append(f"ctx._source.{k} = '{v}'")
+                    scripts.append(f"ctx._source.{k} = {json.dumps(v)}")
                 elif isinstance(v, int):
                     scripts.append(f"ctx._source.{k} = {v}")
                 else:
