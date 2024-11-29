2d239f94698e8ee0660cc72174c350c1ea8fb610
kunkeji <49889250+kunkeji@users.noreply.github.com>
2024-11-29 13:45:31 +0800

    Change Traditional Chinese to Simplified Chinese (#3742)
    ### What problem does this PR solve?
    
    Change Traditional Chinese to Simplified Chinese
    
    ### Type of change
    
    - [x] Other (please describe):

--- web/src/locales/zh.ts
+++ web/src/locales/zh.ts
@@ -75,7 +75,7 @@ export default {
       namePlaceholder: '请输入名称',
       doc: '文档',
       searchKnowledgePlaceholder: '搜索',
-      noMoreData: '沒有更多的數據了',
+      noMoreData: '没有更多数据了',
     },
     knowledgeDetails: {
       dataset: '数据集',
