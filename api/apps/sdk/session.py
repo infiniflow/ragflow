910d758562f7992fe84a321a13a3d4178709dd1d
Kevin Hu <kevinhu.sh@gmail.com>
2024-11-29 09:47:59 +0800

    Update api/apps/sdk/session.py

--- api/apps/sdk/session.py
+++ api/apps/sdk/session.py
@@ -318,7 +318,6 @@ def agent_completion(tenant_id, agent_id):
 
     canvas.add_user_input(msg[-1]["content"])
 
-    for m in conv.message:
         canvas.history.append((m["role"], m["content"]))
 
     if stream:
