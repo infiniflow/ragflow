25121de9b82830584bb488c73b6217fcc9a14dc8
Jin Hai <haijin.chn@gmail.com>
2024-12-03 10:33:35 +0800

    Web API test cases (#3812)
    ### What problem does this PR solve?
    
    1. Failed update dataset case
    2. Upload and parse text file
    
    ### Type of change
    
    - [x] Other (please describe): test cases
    
    ---------
    
    Signed-off-by: jinhai <haijin.chn@gmail.com>

--- sdk/python/test/test_frontend_api/common.py
+++ sdk/python/test/test_frontend_api/common.py
@@ -5,6 +5,7 @@ HOST_ADDRESS = os.getenv('HOST_ADDRESS', 'http://127.0.0.1:9380')
 
 DATASET_NAME_LIMIT = 128
 
+
 def create_dataset(auth, dataset_name):
     authorization = {"Authorization": auth}
     url = f"{HOST_ADDRESS}/v1/kb/create"
@@ -27,8 +28,53 @@ def rm_dataset(auth, dataset_id):
     res = requests.post(url=url, headers=authorization, json=json)
     return res.json()
 
+
 def update_dataset(auth, json_req):
     authorization = {"Authorization": auth}
     url = f"{HOST_ADDRESS}/v1/kb/update"
     res = requests.post(url=url, headers=authorization, json=json_req)
     return res.json()
+
+
+def upload_file(auth, dataset_id, path):
+    authorization = {"Authorization": auth}
+    url = f"{HOST_ADDRESS}/v1/document/upload"
+    base_name = os.path.basename(path)
+    json_req = {
+        "kb_id": dataset_id,
+    }
+
+    file = {
+        'file': open(f'{path}', 'rb')
+    }
+
+    res = requests.post(url=url, headers=authorization, files=file, data=json_req)
+    return res.json()
+
+def list_document(auth, dataset_id):
+    authorization = {"Authorization": auth}
+    url = f"{HOST_ADDRESS}/v1/document/list?kb_id={dataset_id}"
+    res = requests.get(url=url, headers=authorization)
+    return res.json()
+
+def get_docs_info(auth, doc_ids):
+    authorization = {"Authorization": auth}
+    json_req = {
+        "doc_ids": doc_ids
+    }
+    url = f"{HOST_ADDRESS}/v1/document/infos"
+    res = requests.post(url=url, headers=authorization, json=json_req)
+    return res.json()
+
+def parse_docs(auth, doc_ids):
+    authorization = {"Authorization": auth}
+    json_req = {
+        "doc_ids": doc_ids,
+        "run": 1
+    }
+    url = f"{HOST_ADDRESS}/v1/document/run"
+    res = requests.post(url=url, headers=authorization, json=json_req)
+    return res.json()
+
+def parse_file(auth, document_id):
+    pass
