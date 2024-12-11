1dacf9a55575c24e75a78fe2273dd9ba401ad00f
balibabu <cike8899@users.noreply.github.com>
2024-12-11 16:29:17 +0800

    Fix:  Fixed the issue where two consecutive indexes were displayed incorrectly #3839 (#3988)
    ### What problem does this PR solve?
    
    Fix: Fixed the issue where two consecutive indexes were displayed
    incorrectly #3839
    
    ### Type of change
    
    - [x] Bug Fix (non-breaking change which fixes an issue)

--- web/src/pages/chat/utils.ts
+++ web/src/pages/chat/utils.ts
@@ -44,8 +44,9 @@ export const buildMessageItemReference = (
 
 const oldReg = /(#{2}\d+\${2})/g;
 
+// To be compatible with the old index matching mode
 export const replaceTextByOldReg = (text: string) => {
   return text.replace(oldReg, function (substring) {
-    return `${substring.slice(0, -2)}@@`;
+    return `~~${substring.slice(2, -2)}==`;
   });
 };
