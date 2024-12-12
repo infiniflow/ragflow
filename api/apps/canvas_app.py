bc3b37e8a87afbae93f7fdfb28c4769c95f81ebf
Kevin Hu <kevinhu.sh@gmail.com>
2024-12-12 19:09:27 +0800

    Update canvas_app.py

--- api/apps/canvas_app.py
+++ api/apps/canvas_app.py
@@ -244,7 +244,6 @@ def test_db_connect():
                                     password=req["password"])
         elif req["db_type"] == 'mssql':
             import pyodbc
-            import pyodbc
             connection_string = (
                 f"DRIVER={{ODBC Driver 17 for SQL Server}};"
                 f"SERVER={req['host']},{req['port']};"
