93635674c34d2466446c29fc77a36f291ec6825f
balibabu <cike8899@users.noreply.github.com>
2024-12-13 16:55:13 +0800

    Feat: Reparse a file shall reuse existing chunks if possible #3793 (#4021)
    ### What problem does this PR solve?
    
    Feat: Reparse a file shall reuse existing chunks if possible #3793
    
    ### Type of change
    
    
    - [x] New Feature (non-breaking change which adds functionality)

--- web/src/locales/zh.ts
+++ web/src/locales/zh.ts
@@ -162,6 +162,7 @@ export default {
       autoKeywordsTip: `在查询此类关键词时，为每个块提取 N 个关键词以提高其排名得分。在“系统模型设置”中设置的 LLM 将消耗额外的 token。您可以在块列表中查看结果。`,
       autoQuestions: '自动问题',
       autoQuestionsTip: `在查询此类问题时，为每个块提取 N 个问题以提高其排名得分。在“系统模型设置”中设置的 LLM 将消耗额外的 token。您可以在块列表中查看结果。如果发生错误，此功能不会破坏整个分块过程，除了将空结果添加到原始块。`,
+      redo: '是否清空已有 {{chunkNum}}个 chunk？',
     },
     knowledgeConfiguration: {
       titleDescription: '在这里更新您的知识库详细信息，尤其是解析方法。',
