cb454314125a613b862b08f7d51e635f36b376da
Kevin Hu <kevinhu.sh@gmail.com>
2024-12-23 10:03:50 +0800

    Fix Voyage re-rank model. Limit file name length. (#4171)
    ### What problem does this PR solve?
    
    #4152 
    #4154
    
    ### Type of change
    
    - [x] Bug Fix (non-breaking change which fixes an issue)

--- api/db/services/file_service.py
+++ api/db/services/file_service.py
@@ -345,6 +345,8 @@ class FileService(CommonService):
                 MAX_FILE_NUM_PER_USER = int(os.environ.get('MAX_FILE_NUM_PER_USER', 0))
                 if MAX_FILE_NUM_PER_USER > 0 and DocumentService.get_doc_count(kb.tenant_id) >= MAX_FILE_NUM_PER_USER:
                     raise RuntimeError("Exceed the maximum file number of a free user!")
+                if len(file.filename) >= 128:
+                    raise RuntimeError("Exceed the maximum length of file name!")
 
                 filename = duplicate_name(
                     DocumentService.query,
