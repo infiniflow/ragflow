81bf3415546a34c0330713536ed8c76efb1c927f
Kevin Hu <kevinhu.sh@gmail.com>
2024-12-30 17:49:24 +0800

    Code format.

--- agent/component/answer.py
+++ agent/component/answer.py
@@ -78,7 +78,8 @@ class Answer(ComponentBase, ABC):
         self.exception = e
 
     def output(self, allow_partial=True) -> Tuple[str, Union[pd.DataFrame, partial]]:
-        if allow_partial: return super.output()
+        if allow_partial:
+            return super.output()
 
         for r, c in self._canvas.history[::-1]:
             if r == "user":
