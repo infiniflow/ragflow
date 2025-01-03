613e045a2c3ab3b9ee57205dd9550aedafeed487
bill <yibie_jingnian@163.com>
2025-01-03 18:59:45 +0800

    Feat: Add FilesTable

--- web/src/pages/flow/flow-tooltip.tsx
+++ web/src/pages/flow/flow-tooltip.tsx
@@ -1,7 +1,6 @@
 import {
   Tooltip,
   TooltipContent,
-  TooltipProvider,
   TooltipTrigger,
 } from '@/components/ui/tooltip';
 import { PropsWithChildren } from 'react';
@@ -10,13 +9,11 @@ import { useTranslation } from 'react-i18next';
 export const RunTooltip = ({ children }: PropsWithChildren) => {
   const { t } = useTranslation();
   return (
-    <TooltipProvider>
-      <Tooltip>
-        <TooltipTrigger>{children}</TooltipTrigger>
-        <TooltipContent>
-          <p>{t('flow.testRun')}</p>
-        </TooltipContent>
-      </Tooltip>
-    </TooltipProvider>
+    <Tooltip>
+      <TooltipTrigger>{children}</TooltipTrigger>
+      <TooltipContent>
+        <p>{t('flow.testRun')}</p>
+      </TooltipContent>
+    </Tooltip>
   );
 };
