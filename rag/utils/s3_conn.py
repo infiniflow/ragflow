e72f895d67adfa46dfd6148536160e31877aba21
Trung Dinh <xamarindevelopervietnam@outlook.com>
2025-01-03 09:08:09 +0700

    Removed endpoint when we work with aws s3

--- rag/utils/s3_conn.py
+++ rag/utils/s3_conn.py
@@ -12,7 +12,6 @@ class RAGFlowS3(object):
     def __init__(self):
         self.conn = None
         self.s3_config = settings.S3
-        self.endpoint = self.s3_config.get('endpoint', None)
         self.access_key = self.s3_config.get('access_key', None)
         self.secret_key = self.s3_config.get('secret_key', None)
         self.region = self.s3_config.get('region', None)
@@ -35,15 +34,13 @@ class RAGFlowS3(object):
 
             self.conn = boto3.client(
                 's3',
-                endpoint_url=self.endpoint,
                 region_name=self.region,
                 aws_access_key_id=self.access_key,
                 aws_secret_access_key=self.secret_key,
                 config=config
             )
         except Exception:
-            logging.exception(
-                "Fail to connect %s" % self.endpoint)
+            logging.exception(f"Fail to connect at region {self.region}")
 
     def __close__(self):
         del self.conn
