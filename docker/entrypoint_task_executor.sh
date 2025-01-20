ee3f2d7c6c2d055b50b73b82cfed08ab56334b3a
Zhichang Yu <yuzhichang@gmail.com>
2025-01-20 22:49:46 +0800

    Added entrypoint for task executor (#4551)
    ### What problem does this PR solve?
    
    Added entrypoint for task executor
    
    ### Type of change
    
    - [x] Refactoring

--- docker/entrypoint_task_executor.sh
+++ docker/entrypoint_task_executor.sh
@@ -7,28 +7,22 @@ while IFS= read -r line || [[ -n "$line" ]]; do
     eval "echo \"$line\"" >> /ragflow/conf/service_conf.yaml
 done < /ragflow/conf/service_conf.yaml.template
 
-/usr/sbin/nginx
-
 export LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu/
 
 PY=python3
-if [[ -z "$WS" || $WS -lt 1 ]]; then
-  WS=1
-fi
+
+CONSUMER_NO_BEG=$1
+CONSUMER_NO_END=$2
 
 function task_exe(){
-    while [ 1 -eq 1 ];do
+    while [ 1 -eq 1 ]; do
       $PY rag/svr/task_executor.py $1;
     done
 }
 
-for ((i=0;i<WS;i++))
+for ((i=CONSUMER_NO_BEG; i<CONSUMER_NO_END; i++))
 do
-  task_exe  $i &
-done
-
-while [ 1 -eq 1 ];do
-    $PY api/ragflow_server.py
+  task_exe $i &
 done
 
 wait;
