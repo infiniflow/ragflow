3894de895b8c9e9435503094ee6b6cc2e55eb4d3
Jin Hai <haijin.chn@gmail.com>
2025-01-21 20:52:28 +0800

    Update comments (#4569)
    ### What problem does this PR solve?
    
    Add license statement.
    
    ### Type of change
    
    - [x] Refactoring
    
    Signed-off-by: Jin Hai <haijin.chn@gmail.com>

--- sdk/python/ragflow_sdk/modules/session.py
+++ sdk/python/ragflow_sdk/modules/session.py
@@ -1,3 +1,19 @@
+#
+#  Copyright 2025 The InfiniFlow Authors. All Rights Reserved.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+
 import json
 
 from .base import Base
@@ -8,8 +24,8 @@ class Session(Base):
         self.id = None
         self.name = "New session"
         self.messages = [{"role": "assistant", "content": "Hi! I am your assistant，can I help you?"}]
-        for key,value in res_dict.items():
-            if key =="chat_id" and value is not None:
+        for key, value in res_dict.items():
+            if key == "chat_id" and value is not None:
                 self.chat_id = None
                 self.__session_type = "chat"
             if key == "agent_id" and value is not None:
@@ -17,11 +33,11 @@ class Session(Base):
                 self.__session_type = "agent"
         super().__init__(rag, res_dict)
 
-    def ask(self, question="",stream=True,**kwargs):
+    def ask(self, question="", stream=True, **kwargs):
         if self.__session_type == "agent":
-            res=self._ask_agent(question,stream)
+            res = self._ask_agent(question, stream)
         elif self.__session_type == "chat":
-            res=self._ask_chat(question,stream,**kwargs)
+            res = self._ask_chat(question, stream, **kwargs)
         for line in res.iter_lines():
             line = line.decode("utf-8")
             if line.startswith("{"):
@@ -44,24 +60,25 @@ class Session(Base):
             message = Message(self.rag, temp_dict)
             yield message
 
-    def _ask_chat(self, question: str, stream: bool,**kwargs):
-        json_data={"question": question, "stream": True,"session_id":self.id}
+    def _ask_chat(self, question: str, stream: bool, **kwargs):
+        json_data = {"question": question, "stream": True, "session_id": self.id}
         json_data.update(kwargs)
         res = self.post(f"/chats/{self.chat_id}/completions",
                         json_data, stream=stream)
         return res
 
-    def _ask_agent(self,question:str,stream:bool):
+    def _ask_agent(self, question: str, stream: bool):
         res = self.post(f"/agents/{self.agent_id}/completions",
-                        {"question": question, "stream": True,"session_id":self.id}, stream=stream)
+                        {"question": question, "stream": True, "session_id": self.id}, stream=stream)
         return res
 
-    def update(self,update_message):
+    def update(self, update_message):
         res = self.put(f"/chats/{self.chat_id}/sessions/{self.id}",
-                        update_message)
+                       update_message)
         res = res.json()
         if res.get("code") != 0:
             raise Exception(res.get("message"))
+
 
 class Message(Base):
     def __init__(self, rag, res_dict):
