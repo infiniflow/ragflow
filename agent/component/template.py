f932279aa24d3e8be90cc8797c3ee150e3bb4fdd
Kevin Hu <kevinhu.sh@gmail.com>
2025-02-08 18:09:02 +0800

    Inner prompt parameter setting. (#4806)
    ### What problem does this PR solve?
    
    #4764
    
    ### Type of change
    
    - [x] New Feature (non-breaking change which adds functionality)

--- agent/component/template.py
+++ agent/component/template.py
@@ -38,27 +38,39 @@ class Template(ComponentBase):
     component_name = "Template"
 
     def get_dependent_components(self):
-        cpnts = set(
-            [
-                para["component_id"].split("@")[0]
-                for para in self._param.parameters
-                if para.get("component_id")
-                and para["component_id"].lower().find("answer") < 0
-                and para["component_id"].lower().find("begin") < 0
-            ]
-        )
+        inputs = self.get_input_elements()
+        cpnts = set([i["key"] for i in inputs if i["key"].lower().find("answer") < 0 and i["key"].lower().find("begin") < 0])
         return list(cpnts)
+
+    def get_input_elements(self):
+        key_set = set([])
+        res = []
+        for r in re.finditer(r"\{([a-z]+[:@][a-z0-9_-]+)\}", self._param.content, flags=re.IGNORECASE):
+            cpn_id = r.group(1)
+            if cpn_id in key_set:
+                continue
+            if cpn_id.lower().find("begin@") == 0:
+                cpn_id, key = cpn_id.split("@")
+                for p in self._canvas.get_component(cpn_id)["obj"]._param.query:
+                    if p["key"] != key:
+                        continue
+                    res.append({"key": r.group(1), "name": p["name"]})
+                    key_set.add(r.group(1))
+                continue
+            cpn_nm = self._canvas.get_compnent_name(cpn_id)
+            if not cpn_nm:
+                continue
+            res.append({"key": cpn_id, "name": cpn_nm})
+            key_set.add(cpn_id)
+        return res
 
     def _run(self, history, **kwargs):
         content = self._param.content
 
         self._param.inputs = []
-        for para in self._param.parameters:
-            if not para.get("component_id"):
-                continue
-            component_id = para["component_id"].split("@")[0]
-            if para["component_id"].lower().find("@") >= 0:
-                cpn_id, key = para["component_id"].split("@")
+        for para in self.get_input_elements():
+            if para["key"].lower().find("begin@") == 0:
+                cpn_id, key = para["key"].split("@")
                 for p in self._canvas.get_component(cpn_id)["obj"]._param.query:
                     if p["key"] == key:
                         value = p.get("value", "")
@@ -68,6 +80,7 @@ class Template(ComponentBase):
                     assert False, f"Can't find parameter '{key}' for {cpn_id}"
                 continue
 
+            component_id = para["key"]
             cpn = self._canvas.get_component(component_id)["obj"]
             if cpn.component_name.lower() == "answer":
                 hist = self._canvas.get_history(1)
@@ -114,7 +127,7 @@ class Template(ComponentBase):
 
     def make_kwargs(self, para, kwargs, value):
         self._param.inputs.append(
-            {"component_id": para["component_id"], "content": value}
+            {"component_id": para["key"], "content": value}
         )
         try:
             value = json.loads(value)
