fc1a49fd866b778f8ca742672ee6172f5abe6a6a
balibabu <cike8899@users.noreply.github.com>
2025-02-14 10:43:59 +0800

    Feat: Add an id to the dataset testing route #3221 (#4951)
    ### What problem does this PR solve?
    
    Feat: Add an id to the dataset testing route #3221
    
    ### Type of change
    
    
    - [x] New Feature (non-breaking change which adds functionality)

--- web/src/pages/dataset/sidebar/hooks.tsx
+++ web/src/pages/dataset/sidebar/hooks.tsx
@@ -1,15 +1,16 @@
 import { Routes } from '@/routes';
 import { useCallback } from 'react';
-import { useNavigate } from 'umi';
+import { useNavigate, useParams } from 'umi';
 
 export const useHandleMenuClick = () => {
   const navigate = useNavigate();
+  const { id } = useParams();
 
   const handleMenuClick = useCallback(
     (key: Routes) => () => {
-      navigate(`${Routes.DatasetBase}${key}`);
+      navigate(`${Routes.DatasetBase}${key}/${id}`);
     },
-    [navigate],
+    [id, navigate],
   );
 
   return { handleMenuClick };
