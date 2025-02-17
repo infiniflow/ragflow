7b5d8312961874841b652512ac5495b2b46a017b
hy89 <31279043+hy89@users.noreply.github.com>
2025-02-17 19:33:49 +0800

    Fix: Starting the source code on Windows, the 'HTTP API' returns 404 (#5042)
    Fix: When starting the backend service from source code on Windows, the
    "HTTP API" no longer returns 404.

--- api/apps/__init__.py
+++ api/apps/__init__.py
@@ -119,8 +119,9 @@ def register_page(page_path):
     sys.modules[module_name] = page
     spec.loader.exec_module(page)
     page_name = getattr(page, "page_name", page_name)
+    sdk_path = "\\sdk\\" if sys.platform.startswith("win") else "/sdk/"
     url_prefix = (
-        f"/api/{API_VERSION}" if "/sdk/" in path else f"/{API_VERSION}/{page_name}"
+        f"/api/{API_VERSION}" if sdk_path in path else f"/{API_VERSION}/{page_name}"
     )
 
     app.register_blueprint(page.manager, url_prefix=url_prefix)
