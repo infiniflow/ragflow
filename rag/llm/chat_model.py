1fe2b8e163961f5001c2a05dc18173699dda7ee1
yrk111222 <2493404415@qq.com>
2025-02-19 14:16:59 +0800

    Update chat_model.py

--- rag/llm/chat_model.py
+++ rag/llm/chat_model.py
@@ -146,7 +146,10 @@ class HuggingFaceChat(Base):
 class ModelScopeChat(Base):
     def __init__(self, key=None, model_name="", base_url=""):
         if not base_url:
-            base_url = "https://api-inference.modelscope.cn/v1/"
+            raise ValueError("Local llm url cannot be None")
+        base_url = base_url.rstrip('/')
+        if base_url.split("/")[-1] != "v1":
+            base_url = os.path.join(base_url, "v1")
         super().__init__(key, model_name.split("___")[0], base_url)
 
 
