0be2e9622077e148709b446e35321233f4e2a653
gstrat88 <gstrat@innews.gr>
2025-02-19 22:03:39 +0200

    added the ability to pass parameters from the iframe shared chat

--- web/src/pages/chat/shared-hooks.ts
+++ web/src/pages/chat/shared-hooks.ts
@@ -22,11 +22,18 @@ export const useSendButtonDisabled = (value: string) => {
 
 export const useGetSharedChatSearchParams = () => {
   const [searchParams] = useSearchParams();
-
+  const data_prefix = 'data_';
+  const data = Object.fromEntries(
+    searchParams
+      .entries()
+      .filter(([key, value]) => key.startsWith(data_prefix))
+      .map(([key, value]) => [key.replace(data_prefix, ''), value]),
+  );
   return {
     from: searchParams.get('from') as SharedFrom,
     sharedId: searchParams.get('shared_id'),
     locale: searchParams.get('locale'),
+    data: data,
     visibleAvatar: searchParams.get('visible_avatar')
       ? searchParams.get('visible_avatar') !== '1'
       : true,
@@ -34,7 +41,11 @@ export const useGetSharedChatSearchParams = () => {
 };
 
 export const useSendSharedMessage = () => {
-  const { from, sharedId: conversationId } = useGetSharedChatSearchParams();
+  const {
+    from,
+    sharedId: conversationId,
+    data: data,
+  } = useGetSharedChatSearchParams();
   const { createSharedConversation: setConversation } =
     useCreateNextSharedConversation();
   const { handleInputChange, value, setValue } = useHandleMessageInputChange();
@@ -52,11 +63,15 @@ export const useSendSharedMessage = () => {
 
   const sendMessage = useCallback(
     async (message: Message, id?: string) => {
-      const res = await send({
+      const payload = {
         conversation_id: id ?? conversationId,
         quote: true,
         question: message.content,
         session_id: get(derivedMessages, '0.session_id'),
+      };
+      const res = await send({
+        ...payload,
+        ...data,
       });
 
       if (isCompletionError(res)) {
@@ -84,7 +99,8 @@ export const useSendSharedMessage = () => {
   );
 
   const fetchSessionId = useCallback(async () => {
-    const ret = await send({ question: '' });
+    const payload = { question: '' };
+    const ret = await send({ ...payload, ...data });
     if (isCompletionError(ret)) {
       message.error(ret?.data.message);
       setHasError(true);
