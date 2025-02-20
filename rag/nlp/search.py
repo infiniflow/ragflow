81b2234c1910237a743750bf05debd8b2ad60d24
Kevin Hu <kevinhu.sh@gmail.com>
2025-02-20 17:25:17 +0800

    Apply agentic searching.

--- rag/nlp/search.py
+++ rag/nlp/search.py
@@ -259,7 +259,7 @@ class Dealer:
         q_denor = np.sqrt(np.sum([s*s for t,s in query_rfea.items() if t != PAGERANK_FLD]))
         for i in search_res.ids:
             nor, denor = 0, 0
-            for t, sc in json.loads(search_res.field[i].get(TAG_FLD, "{}")).items():
+            for t, sc in eval(search_res.field[i].get(TAG_FLD, "{}")).items():
                 if t in query_rfea:
                     nor += query_rfea[t] * sc
                 denor += sc * sc
