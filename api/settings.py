00cabe9c3a36ded0cb07371a8b3ad192ca6b31fd
Kevin Hu <kevinhu.sh@gmail.com>
2025-02-24 10:11:23 +0800

    Update settings.py

--- api/settings.py
+++ api/settings.py
@@ -102,12 +102,6 @@ def init_settings():
                 "image2text_model": "",
                 "asr_model": "",
             },
-            "ModelScope": {
-                "chat_model": "Qwen/Qwen2.5-7B-Instruct",
-                "embedding_model": "",
-                "image2text_model": "",
-                "asr_model": "",
-            },
             "Moonshot": {
                 "chat_model": "moonshot-v1-8k",
                 "embedding_model": "",
