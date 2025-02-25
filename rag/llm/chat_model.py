7d243429ca56326c0a3b746358a9ac0b87cf0dab
Kevin Hu <kevinhu.sh@gmail.com>
2025-02-25 17:18:08 +0800

    Resolve supper class invokation error.

--- rag/llm/chat_model.py
+++ rag/llm/chat_model.py
@@ -265,7 +265,7 @@ class QWenChat(Base):
 
     def chat(self, system, history, gen_conf):
         if self.model_name.lower().find("deepseek") >= 0:
-            return super.chat(system, history, gen_conf)
+            return super().chat(system, history, gen_conf)
 
         stream_flag = str(os.environ.get('QWEN_CHAT_BY_STREAM', 'true')).lower() == 'true'
         if not stream_flag:
@@ -335,7 +335,7 @@ class QWenChat(Base):
 
     def chat_streamly(self, system, history, gen_conf):
         if self.model_name.lower().find("deepseek") >= 0:
-            return super.chat_streamly(system, history, gen_conf)
+            return super().chat_streamly(system, history, gen_conf)
 
         return self._chat_streamly(system, history, gen_conf)
 
