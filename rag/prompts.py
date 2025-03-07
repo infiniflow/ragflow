a32434df612da918bf50f08449adad9810fe20f3
Kevin Hu <kevinhu.sh@gmail.com>
2025-03-07 17:41:56 +0800

    Fix: truncate message issue. (#5776)
    ### What problem does this PR solve?
    
    Close #5761
    ### Type of change
    
    - [x] Bug Fix (non-breaking change which fixes an issue)

--- rag/prompts.py
+++ rag/prompts.py
@@ -86,9 +86,9 @@ def message_fit_in(msg, max_length=4000):
         msg[0]["content"] = m
         return max_length, msg
 
-    m = msg_[1]["content"]
+    m = msg_[-1]["content"]
     m = encoder.decode(encoder.encode(m)[:max_length - ll2])
-    msg[1]["content"] = m
+    msg[-1]["content"] = m
     return max_length, msg
 
 
