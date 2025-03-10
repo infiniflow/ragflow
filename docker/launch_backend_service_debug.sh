e5498f0e94c47b8ef15f5c3f33e49f444127ed99
kinglen <kinglen.maoxy@gmail.com>
2025-03-10 10:49:48 +0800

    增加调试脚本

--- docker/launch_backend_service_debug.sh
+++ docker/launch_backend_service_debug.sh
@@ -11,7 +11,7 @@ export PYTHONPATH=$(pwd)
 export LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu/
 JEMALLOC_PATH=$(pkg-config --variable=libdir jemalloc)/libjemalloc.so
 
-# 定义环境变量
+# 定义两个进程的调试端口
 PY_DEBUG_task_executor="python3 -m debugpy --log-to-stderr --listen 0.0.0.0:5678"
 PY_DEBUG_ragflow_server="python3 -m debugpy --log-to-stderr --listen 0.0.0.0:5679"
 
