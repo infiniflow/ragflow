1b7c215ed5e4377490b40262fc531efa13630999
so95 <is.thaison@gmail.com>
2025-03-15 10:06:24 +0700

    Update canvas.py

--- agent/canvas.py
+++ agent/canvas.py
@@ -210,6 +210,7 @@ class Canvas:
                 if cpn.component_name == "Answer":
                     self.answer.append(c)
                 else:
+                    logging.debug(f"Canvas.prepare2run: {c}")
                     if c not in without_dependent_checking:
                         cpids = cpn.get_dependent_components()
                         if any([cc not in self.path[-1] for cc in cpids]):
@@ -217,6 +218,7 @@ class Canvas:
                                 waiting.append(c)
                             continue
                     yield "*'{}'* is running...🕞".format(self.get_component_name(c))
+                    
                     if cpn.component_name.lower() == "iteration":
                         st_cpn = cpn.get_start()
                         assert st_cpn, "Start component not found for Iteration."
