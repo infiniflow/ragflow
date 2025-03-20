4407d21c252e45cb81f7069db06a07124b996830
wenju.li <wenju.li@deepctr.cn>
2025-03-20 11:43:20 +0800

    Feat: Add user registration toggle feature Changes Added a user registration toggle REGISTER_ENABLE in the settings and .env config file. The user creation interface now checks the state of this toggle to control the enabling and disabling of the user registration feature. Type [x] New feature (non-breaking change, added functionality)

--- api/settings.py
+++ api/settings.py
@@ -62,9 +62,12 @@ docStoreConn = None
 retrievaler = None
 kg_retrievaler = None
 
+# user registration switch
+REGISTER_ENABLE = True
+
 
 def init_settings():
-    global LLM, LLM_FACTORY, LLM_BASE_URL, LIGHTEN, DATABASE_TYPE, DATABASE, FACTORY_LLM_INFOS
+    global LLM, LLM_FACTORY, LLM_BASE_URL, LIGHTEN, DATABASE_TYPE, DATABASE, FACTORY_LLM_INFOS, REGISTER_ENABLE
     LIGHTEN = int(os.environ.get('LIGHTEN', "0"))
     DATABASE_TYPE = os.getenv("DB_TYPE", 'mysql')
     DATABASE = decrypt_database_config(name=DATABASE_TYPE)
@@ -72,6 +75,7 @@ def init_settings():
     LLM_DEFAULT_MODELS = LLM.get("default_models", {})
     LLM_FACTORY = LLM.get("factory", "Tongyi-Qianwen")
     LLM_BASE_URL = LLM.get("base_url")
+    REGISTER_ENABLE = os.environ.get("REGISTER_ENABLE", "true").lower() == "true"
     
     try:
         with open(os.path.join(get_project_base_directory(), "conf", "llm_factories.json"), "r") as f:
