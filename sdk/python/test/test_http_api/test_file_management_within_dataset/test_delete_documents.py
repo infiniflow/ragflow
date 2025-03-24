195d8c68065e172b9577a7aeb440e901cb7dcce7
asiroliu <asiro@qq.com>
2025-03-24 11:36:55 +0800

    Test: Update test cases for #6174 #6229 #6234

--- sdk/python/test/test_http_api/test_file_management_within_dataset/test_delete_documents.py
+++ sdk/python/test/test_http_api/test_file_management_within_dataset/test_delete_documents.py
@@ -38,9 +38,7 @@ class TestAuthorization:
             ),
         ],
     )
-    def test_invalid_auth(
-        self, get_http_api_auth, tmp_path, auth, expected_code, expected_message
-    ):
+    def test_invalid_auth(self, get_http_api_auth, tmp_path, auth, expected_code, expected_message):
         ids = create_datasets(get_http_api_auth, 1)
         document_ids = batch_upload_documents(get_http_api_auth, ids[0], 1, tmp_path)
         res = delete_documnet(auth, ids[0], {"ids": document_ids[0]})
@@ -54,11 +52,11 @@ class TestDocumentDeletion:
         [
             (None, 0, "", 0),
             ({"ids": []}, 0, "", 0),
-            ({"ids": ["invalid_id"]}, 102, "Document not found!", 3),
+            ({"ids": ["invalid_id"]}, 102, "Documents not found: ['invalid_id']", 3),
             (
                 {"ids": ["\n!?。；！？\"'"]},
                 102,
-                "Document not found!",
+                """Documents not found: [\'\\n!?。；！？"\\\'\']""",
                 3,
             ),
             (
@@ -86,8 +84,8 @@ class TestDocumentDeletion:
             payload = payload(document_ids)
         res = delete_documnet(get_http_api_auth, ids[0], payload)
         assert res["code"] == expected_code
-        #if res["code"] != 0:
-        #    assert res["message"] == expected_message
+        if res["code"] != 0:
+            assert res["message"] == expected_message
 
         res = list_documnet(get_http_api_auth, ids[0])
         assert len(res["data"]["docs"]) == remaining
@@ -104,16 +102,14 @@ class TestDocumentDeletion:
             ),
         ],
     )
-    def test_invalid_dataset_id(
-        self, get_http_api_auth, tmp_path, dataset_id, expected_code, expected_message
-    ):
+    def test_invalid_dataset_id(self, get_http_api_auth, tmp_path, dataset_id, expected_code, expected_message):
         ids = create_datasets(get_http_api_auth, 1)
         document_ids = batch_upload_documents(get_http_api_auth, ids[0], 3, tmp_path)
         res = delete_documnet(get_http_api_auth, dataset_id, {"ids": document_ids[:1]})
         assert res["code"] == expected_code
         assert res["message"] == expected_message
 
-    @pytest.mark.xfail(reason="issues/6174")
+    # @pytest.mark.xfail(reason="issues/6174")
     @pytest.mark.parametrize(
         "payload",
         [
@@ -128,9 +124,8 @@ class TestDocumentDeletion:
         if callable(payload):
             payload = payload(document_ids)
         res = delete_documnet(get_http_api_auth, ids[0], payload)
-        assert res["code"] == 0
-        assert res["data"]["errors"][0] == "You don't own the dataset invalid_id"
-        assert res["data"]["success_count"] == 3
+        assert res["code"] == 102
+        assert res["message"] == "Documents not found: ['invalid_id']"
 
         res = list_documnet(get_http_api_auth, ids[0])
         assert len(res["data"]["docs"]) == 0
@@ -143,17 +138,16 @@ class TestDocumentDeletion:
         assert res["code"] == 0
 
         res = delete_documnet(get_http_api_auth, ids[0], {"ids": document_ids})
-        assert res["code"] in [102, 500]
-        #assert res["message"] == "Document not found!"
+        assert res["code"] == 102
+        assert res["message"] == f"Documents not found: {document_ids}"
 
-    @pytest.mark.xfail(reason="issues/6234")
     def test_duplicate_deletion(self, get_http_api_auth, tmp_path):
         ids = create_datasets(get_http_api_auth, 1)
         document_ids = batch_upload_documents(get_http_api_auth, ids[0], 1, tmp_path)
-        res = delete_documnet(
-            get_http_api_auth, ids[0], {"ids": document_ids + document_ids}
-        )
+        res = delete_documnet(get_http_api_auth, ids[0], {"ids": document_ids + document_ids})
         assert res["code"] == 0
+        assert res["data"]["errors"][0] == f"Duplicate document ids: {document_ids[0]}"
+        assert res["data"]["success_count"] == 1
 
         res = list_documnet(get_http_api_auth, ids[0])
         assert len(res["data"]["docs"]) == 0
@@ -162,9 +156,7 @@ class TestDocumentDeletion:
     def test_concurrent_deletion(self, get_http_api_auth, tmp_path):
         documnets_num = 100
         ids = create_datasets(get_http_api_auth, 1)
-        document_ids = batch_upload_documents(
-            get_http_api_auth, ids[0], documnets_num, tmp_path
-        )
+        document_ids = batch_upload_documents(get_http_api_auth, ids[0], documnets_num, tmp_path)
 
         with ThreadPoolExecutor(max_workers=5) as executor:
             futures = [
@@ -183,9 +175,7 @@ class TestDocumentDeletion:
     def test_delete_1k(self, get_http_api_auth, tmp_path):
         documnets_num = 1_000
         ids = create_datasets(get_http_api_auth, 1)
-        document_ids = batch_upload_documents(
-            get_http_api_auth, ids[0], documnets_num, tmp_path
-        )
+        document_ids = batch_upload_documents(get_http_api_auth, ids[0], documnets_num, tmp_path)
         res = list_documnet(get_http_api_auth, ids[0])
         assert res["data"]["total"] == documnets_num
 
