b2b7ed892713760b335d81fd21dea3140a6ad102
Kevin Hu <kevinhu.sh@gmail.com>
2025-03-25 19:03:29 +0800

    Fix: abnormal chunk id (#6506)
    ### What problem does this PR solve?
    
    #6500
    
    ### Type of change
    
    - [x] Bug Fix (non-breaking change which fixes an issue)

--- api/apps/sdk/doc.py
+++ api/apps/sdk/doc.py
@@ -917,6 +917,8 @@ def list_chunks(tenant_id, dataset_id, document_id):
     res = {"total": 0, "chunks": [], "doc": renamed_doc}
     if req.get("id"):
         chunk = settings.docStoreConn.get(req.get("id"), search.index_name(tenant_id), [dataset_id])
+        if not chunk:
+            return get_result(message=f"Chunk not found: {dataset_id}/{req.get('id')}", code=settings.RetCode.NOT_FOUND)
         k = []
         for n in chunk.keys():
             if re.search(r"(_vec$|_sm_|_tks|_ltks)", n):
