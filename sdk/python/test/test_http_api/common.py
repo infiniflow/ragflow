9e88034b61cd03446980fb4094dc92483db38c3c
liu an <asiro@qq.com>
2025-03-26 16:47:47 +0800

    Test: Added test cases for Update Chunk HTTP API (#6556)
    ### What problem does this PR solve?
    
    cover [update
    chunk](https://ragflow.io/docs/v0.17.2/http_api_reference#update-chunk)
    endpoints
    
    ### Type of change
    
    - [x] add test cases

--- sdk/python/test/test_http_api/common.py
+++ sdk/python/test/test_http_api/common.py
@@ -190,6 +190,12 @@ def list_chunks(auth, dataset_id, document_id, params=None):
     return res.json()
 
 
+def update_chunk(auth, dataset_id, document_id, chunk_id, payload=None):
+    url = f"{HOST_ADDRESS}{CHUNK_API_URL}/{chunk_id}".format(dataset_id=dataset_id, document_id=document_id)
+    res = requests.put(url=url, headers=HEADERS, auth=auth, json=payload)
+    return res.json()
+
+
 def batch_add_chunks(auth, dataset_id, document_id, num):
     chunk_ids = []
     for i in range(num):
--- sdk/python/test/test_http_api/test_chunk_management_within_dataset/test_add_chunk.py
+++ sdk/python/test/test_http_api/test_chunk_management_within_dataset/test_add_chunk.py
@@ -43,9 +43,8 @@ class TestAuthorization:
             ),
         ],
     )
-    def test_invalid_auth(self, get_dataset_id_and_document_id, auth, expected_code, expected_message):
-        dataset_id, document_id = get_dataset_id_and_document_id
-        res = add_chunk(auth, dataset_id, document_id, {})
+    def test_invalid_auth(self, auth, expected_code, expected_message):
+        res = add_chunk(auth, "dataset_id", "document_id", {})
         assert res["code"] == expected_code
         assert res["message"] == expected_message
 
@@ -70,12 +69,16 @@ class TestAddChunk:
     def test_content(self, get_http_api_auth, get_dataset_id_and_document_id, payload, expected_code, expected_message):
         dataset_id, document_id = get_dataset_id_and_document_id
         res = list_chunks(get_http_api_auth, dataset_id, document_id)
+        if res["code"] != 0:
+            assert False, res
         chunks_count = res["data"]["doc"]["chunk_count"]
         res = add_chunk(get_http_api_auth, dataset_id, document_id, payload)
         assert res["code"] == expected_code
         if expected_code == 0:
             validate_chunk_details(dataset_id, document_id, payload, res)
             res = list_chunks(get_http_api_auth, dataset_id, document_id)
+            if res["code"] != 0:
+                assert False, res
             assert res["data"]["doc"]["chunk_count"] == chunks_count + 1
         else:
             assert res["message"] == expected_message
@@ -98,12 +101,16 @@ class TestAddChunk:
     def test_important_keywords(self, get_http_api_auth, get_dataset_id_and_document_id, payload, expected_code, expected_message):
         dataset_id, document_id = get_dataset_id_and_document_id
         res = list_chunks(get_http_api_auth, dataset_id, document_id)
+        if res["code"] != 0:
+            assert False, res
         chunks_count = res["data"]["doc"]["chunk_count"]
         res = add_chunk(get_http_api_auth, dataset_id, document_id, payload)
         assert res["code"] == expected_code
         if expected_code == 0:
             validate_chunk_details(dataset_id, document_id, payload, res)
             res = list_chunks(get_http_api_auth, dataset_id, document_id)
+            if res["code"] != 0:
+                assert False, res
             assert res["data"]["doc"]["chunk_count"] == chunks_count + 1
         else:
             assert res["message"] == expected_message
@@ -127,11 +134,15 @@ class TestAddChunk:
     def test_questions(self, get_http_api_auth, get_dataset_id_and_document_id, payload, expected_code, expected_message):
         dataset_id, document_id = get_dataset_id_and_document_id
         res = list_chunks(get_http_api_auth, dataset_id, document_id)
+        if res["code"] != 0:
+            assert False, res
         chunks_count = res["data"]["doc"]["chunk_count"]
         res = add_chunk(get_http_api_auth, dataset_id, document_id, payload)
         assert res["code"] == expected_code
         if expected_code == 0:
             validate_chunk_details(dataset_id, document_id, payload, res)
+            if res["code"] != 0:
+                assert False, res
             res = list_chunks(get_http_api_auth, dataset_id, document_id)
             assert res["data"]["doc"]["chunk_count"] == chunks_count + 1
         else:
@@ -182,17 +193,23 @@ class TestAddChunk:
         payload = {"content": "chunk test"}
         dataset_id, document_id = get_dataset_id_and_document_id
         res = list_chunks(get_http_api_auth, dataset_id, document_id)
+        if res["code"] != 0:
+            assert False, res
         chunks_count = res["data"]["doc"]["chunk_count"]
         res = add_chunk(get_http_api_auth, dataset_id, document_id, payload)
         assert res["code"] == 0
         validate_chunk_details(dataset_id, document_id, payload, res)
         res = list_chunks(get_http_api_auth, dataset_id, document_id)
+        if res["code"] != 0:
+            assert False, res
         assert res["data"]["doc"]["chunk_count"] == chunks_count + 1
 
         res = add_chunk(get_http_api_auth, dataset_id, document_id, payload)
         assert res["code"] == 0
         validate_chunk_details(dataset_id, document_id, payload, res)
         res = list_chunks(get_http_api_auth, dataset_id, document_id)
+        if res["code"] != 0:
+            assert False, res
         assert res["data"]["doc"]["chunk_count"] == chunks_count + 2
 
     def test_add_chunk_to_deleted_document(self, get_http_api_auth, get_dataset_id_and_document_id):
@@ -207,6 +224,8 @@ class TestAddChunk:
         chunk_num = 50
         dataset_id, document_id = get_dataset_id_and_document_id
         res = list_chunks(get_http_api_auth, dataset_id, document_id)
+        if res["code"] != 0:
+            assert False, res
         chunks_count = res["data"]["doc"]["chunk_count"]
 
         with ThreadPoolExecutor(max_workers=5) as executor:
@@ -223,4 +242,6 @@ class TestAddChunk:
         responses = [f.result() for f in futures]
         assert all(r["code"] == 0 for r in responses)
         res = list_chunks(get_http_api_auth, dataset_id, document_id)
+        if res["code"] != 0:
+            assert False, res
         assert res["data"]["doc"]["chunk_count"] == chunks_count + chunk_num
