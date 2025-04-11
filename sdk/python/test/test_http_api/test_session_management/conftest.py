c14569f0cd023bb06e1d3b34f0bd58d43083b055
liu an <asiro@qq.com>
2025-04-11 20:10:24 +0800

    Test: Added test cases for Update Session With Chat Assistant HTTP API (#6968)
    ### What problem does this PR solve?
    
    cover [Update chat assistant's
    sessions](https://ragflow.io/docs/dev/http_api_reference#update-chat-assistants-session)
    endpoints
    
    ### Type of change
    
    - [x] Update test cases

--- sdk/python/test/test_http_api/test_session_management/conftest.py
+++ sdk/python/test/test_http_api/test_session_management/conftest.py
@@ -33,3 +33,21 @@ def add_sessions_with_chat_assistant(request, get_http_api_auth, add_chat_assist
         session_ids.append(res["data"]["id"])
 
     return chat_assistant_ids[0], session_ids
+
+
+@pytest.fixture(scope="function")
+def add_sessions_with_chat_assistant_func(request, get_http_api_auth, add_chat_assistants):
+    _, _, chat_assistant_ids = add_chat_assistants
+
+    def cleanup():
+        for chat_assistant_id in chat_assistant_ids:
+            delete_session_with_chat_assistants(get_http_api_auth, chat_assistant_id)
+
+    request.addfinalizer(cleanup)
+
+    session_ids = []
+    for i in range(5):
+        res = create_session_with_chat_assistant(get_http_api_auth, chat_assistant_ids[0], {"name": f"session_with_chat_assistant_{i}"})
+        session_ids.append(res["data"]["id"])
+
+    return chat_assistant_ids[0], session_ids
