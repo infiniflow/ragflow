94181a990b957ed302952b4de17583d2b44f3099
Yongteng Lei <yongtengrey@outlook.com>
2025-04-23 13:01:46 +0800

    Refa: knowledge_graph chunk method is deprecated (#7220)
    ### What problem does this PR solve?
    
    The knowledge_graph chunk method is deprecated and should no longer be
    used. #7184.
    
    ### Type of change
    
    - [x] Refactoring

--- sdk/python/test/test_sdk_api/t_dataset.py
+++ sdk/python/test/test_sdk_api/t_dataset.py
@@ -38,8 +38,7 @@ def test_create_dataset_with_duplicated_name(get_api_key_fixture):
 def test_create_dataset_with_random_chunk_method(get_api_key_fixture):
     API_KEY = get_api_key_fixture
     rag = RAGFlow(API_KEY, HOST_ADDRESS)
-    valid_chunk_methods = ["naive", "manual", "qa", "table", "paper", "book", "laws", "presentation", "picture", "one",
-                           "knowledge_graph", "email"]
+    valid_chunk_methods = ["naive", "manual", "qa", "table", "paper", "book", "laws", "presentation", "picture", "one", "email"]
     random_chunk_method = random.choice(valid_chunk_methods)
     rag.create_dataset("test_create_dataset_with_random_chunk_method", chunk_method=random_chunk_method)
 
@@ -47,8 +46,7 @@ def test_create_dataset_with_random_chunk_method(get_api_key_fixture):
 def test_create_dataset_with_invalid_parameter(get_api_key_fixture):
     API_KEY = get_api_key_fixture
     rag = RAGFlow(API_KEY, HOST_ADDRESS)
-    valid_chunk_methods = ["naive", "manual", "qa", "table", "paper", "book", "laws", "presentation", "picture", "one",
-                           "knowledge_graph", "email", "tag"]
+    valid_chunk_methods = ["naive", "manual", "qa", "table", "paper", "book", "laws", "presentation", "picture", "one", "email", "tag"]
     chunk_method = "invalid_chunk_method"
     with pytest.raises(Exception) as exc_info:
         rag.create_dataset("test_create_dataset_with_invalid_chunk_method", chunk_method=chunk_method)
