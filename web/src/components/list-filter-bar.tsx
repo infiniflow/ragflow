51d9bde5a342c07aab18d8ffbb2b5b058ccbbf79
balibabu <cike8899@users.noreply.github.com>
2025-04-23 15:21:09 +0800

    Feat: Create a folder #3221 (#7228)
    ### What problem does this PR solve?
    
    Feat: Create a folder #3221
    
    ### Type of change
    
    
    - [x] New Feature (non-breaking change which adds functionality)

--- web/src/components/list-filter-bar.tsx
+++ web/src/components/list-filter-bar.tsx
@@ -3,18 +3,19 @@ import React, {
   ChangeEventHandler,
   FunctionComponent,
   PropsWithChildren,
+  ReactNode,
 } from 'react';
 import { Button, ButtonProps } from './ui/button';
 import { SearchInput } from './ui/input';
 
 interface IProps {
-  title: string;
-  showDialog?: () => void;
+  title?: string;
   FilterPopover?: FunctionComponent<any>;
   searchString?: string;
   onSearchChange?: ChangeEventHandler<HTMLInputElement>;
   count?: number;
   showFilter?: boolean;
+  leftPanel?: ReactNode;
 }
 
 const FilterButton = React.forwardRef<
@@ -31,16 +32,16 @@ const FilterButton = React.forwardRef<
 export default function ListFilterBar({
   title,
   children,
-  showDialog,
   FilterPopover,
   searchString,
   onSearchChange,
   count,
   showFilter = true,
+  leftPanel,
 }: PropsWithChildren<IProps>) {
   return (
-    <div className="flex justify-between mb-6">
-      <span className="text-3xl font-bold ">{title}</span>
+    <div className="flex justify-between mb-6 items-center">
+      <span className="text-3xl font-bold ">{leftPanel || title}</span>
       <div className="flex gap-4 items-center">
         {showFilter &&
           (FilterPopover ? (
@@ -55,9 +56,7 @@ export default function ListFilterBar({
           value={searchString}
           onChange={onSearchChange}
         ></SearchInput>
-        <Button variant={'tertiary'} size={'sm'} onClick={showDialog}>
-          {children}
-        </Button>
+        {children}
       </div>
     </div>
   );
