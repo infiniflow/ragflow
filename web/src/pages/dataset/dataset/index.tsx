bdebd1b2e305ded7067722f8b59fcc7892bb0943
balibabu <cike8899@users.noreply.github.com>
2025-04-27 14:39:05 +0800

    Feat: Filter document by running status and file type. #3221 (#7340)
    ### What problem does this PR solve?
    Feat: Filter document by running status and file type. #3221
    
    ### Type of change
    
    
    - [x] New Feature (non-breaking change which adds functionality)

--- web/src/pages/dataset/dataset/index.tsx
+++ web/src/pages/dataset/dataset/index.tsx
@@ -2,9 +2,11 @@ import { BulkOperateBar } from '@/components/bulk-operate-bar';
 import { FileUploadDialog } from '@/components/file-upload-dialog';
 import ListFilterBar from '@/components/list-filter-bar';
 import { Button } from '@/components/ui/button';
+import { useFetchDocumentList } from '@/hooks/use-document-request';
 import { Upload } from 'lucide-react';
 import { DatasetTable } from './dataset-table';
 import { useBulkOperateDataset } from './use-bulk-operate-dataset';
+import { useSelectDatasetFilters } from './use-select-filters';
 import { useHandleUploadDocument } from './use-upload-document';
 
 export default function Dataset() {
@@ -16,10 +18,27 @@ export default function Dataset() {
     documentUploadLoading,
   } = useHandleUploadDocument();
   const { list } = useBulkOperateDataset();
+  const {
+    searchString,
+    documents,
+    pagination,
+    handleInputChange,
+    setPagination,
+    filterValue,
+    handleFilterSubmit,
+  } = useFetchDocumentList();
+  const { filters } = useSelectDatasetFilters();
 
   return (
     <section className="p-8">
-      <ListFilterBar title="Dataset">
+      <ListFilterBar
+        title="Dataset"
+        onSearchChange={handleInputChange}
+        searchString={searchString}
+        value={filterValue}
+        onChange={handleFilterSubmit}
+        filters={filters}
+      >
         <Button
           variant={'tertiary'}
           size={'sm'}
@@ -30,7 +49,11 @@ export default function Dataset() {
         </Button>
       </ListFilterBar>
       <BulkOperateBar list={list}></BulkOperateBar>
-      <DatasetTable></DatasetTable>
+      <DatasetTable
+        documents={documents}
+        pagination={pagination}
+        setPagination={setPagination}
+      ></DatasetTable>
       {documentUploadVisible && (
         <FileUploadDialog
           hideModal={hideDocumentUploadModal}
