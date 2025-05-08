0e531fee35df3603f21fc7f286b2a19580a092eb
周欣愉 <zhou_xinyu@neusoft.com>
2025-05-08 09:25:37 +0800

    [fix]: fix the Switch component not fund

--- web/src/pages/agent/form/retrieval-form/next.tsx
+++ web/src/pages/agent/form/retrieval-form/next.tsx
@@ -9,8 +9,8 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-  Switch,
 } from '@/components/ui/form';
+import { Switch } from 'antd';
 import { Textarea } from '@/components/ui/textarea';
 import { useTranslation } from 'react-i18next';
 import { INextOperatorForm } from '../../interface';
