1477e18d35f5cb8434f16bdf80e5ef9ebb3da99f
Stephen Hu <stephenhu@seismic.com>
2025-05-14 11:19:12 +0800

    support agent running hint i10n

--- web/src/locales/en.ts
+++ web/src/locales/en.ts
@@ -1262,6 +1262,7 @@ This delimiter is used to split the input text into several text pieces echo of 
       knowledgeBasesTip:
         'Select the knowledge bases to associate with this chat assistant, or choose variables containing knowledge base IDs below.',
       knowledgeBaseVars: 'Knowledge base variables',
+      runningHintText: 'is running...🕞',
     },
   },
 };
