e041f55a4431caf8357e0b7bb2d6c1a3e05d8627
huangaoqin <huangaoqin@tecpie.com>
2025-05-19 11:16:36 +0800

    MinerU解析

--- rag/app/naive.py
+++ rag/app/naive.py
@@ -33,7 +33,7 @@ from deepdoc.parser.figure_parser import VisionFigureParser, vision_figure_parse
 from deepdoc.parser.pdf_parser import PlainParser, VisionParser
 from rag.nlp import concat_img, find_codec, naive_merge, naive_merge_with_images, naive_merge_docx, rag_tokenizer, tokenize_chunks, tokenize_chunks_with_images, tokenize_table
 from rag.utils import num_tokens_from_string
-
+from parser.minerU.MinerUParser import RemoteMinerUParser
 
 class Docx(DocxParser):
     def __init__(self):
@@ -438,10 +438,11 @@ def chunk(filename, binary=None, from_page=0, to_page=100000,
 
             res = tokenize_table(tables, doc, is_english)
             callback(0.8, "Finish parsing.")
-
         else:
             if layout_recognizer == "Plain Text":
                 pdf_parser = PlainParser()
+            elif layout_recognizer == "MinerU":
+                pdf_parser = RemoteMinerUParser()
             else:
                 vision_model = LLMBundle(kwargs["tenant_id"], LLMType.IMAGE2TEXT, llm_name=layout_recognizer, lang=lang)
                 pdf_parser = VisionParser(vision_model=vision_model, **kwargs)
