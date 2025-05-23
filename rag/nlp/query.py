f1b29bce100d68fbb51d5c3235bddbdb54a38756
Sol <55039727+Sol-Ghf@users.noreply.github.com>
2025-05-23 15:21:08 +0800

    Update query.py
    对于提问文本添加：
    1. 纯英文和中文间加空格（good想法——>good 想法）
    2. 英文加数字和中文间加空格（iPhone 5s价格——> iphone 5s 价格）
    3. 中文后的英文和英文数字间加空格（蓝色的5s——> 蓝色的 5s；美丽的world——>美丽的 world）

--- rag/nlp/query.py
+++ rag/nlp/query.py
@@ -71,7 +71,19 @@ class FulltextQueryer:
             txt = otxt
         return txt
 
+    @staticmethod
+    def addSpaceEnNumZh(txt):
+        # 英文或英文+数字后跟中文
+        txt = re.sub(r'([A-Za-z]+[0-9]+)([\u4e00-\u9fa5]+)', r'\1 \2', txt)
+        # 英文后跟中文
+        txt = re.sub(r'([A-Za-z])([\u4e00-\u9fa5]+)', r'\1 \2', txt)
+        # 中文后跟英文或英文+数字
+        txt = re.sub(r'([\u4e00-\u9fa5]+)([A-Za-z]+[0-9]+)', r'\1 \2', txt)
+        txt = re.sub(r'([\u4e00-\u9fa5]+)([A-Za-z])', r'\1 \2', txt)
+        return txt
+
     def question(self, txt, tbl="qa", min_match: float = 0.6):
+        txt = FulltextQueryer.addSpaceEnNumZh(txt)
         txt = re.sub(
             r"[ :|\r\n\t,，。？?/`!！&^%%()\[\]{}<>]+",
             " ",
