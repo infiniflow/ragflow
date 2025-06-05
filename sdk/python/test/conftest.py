b35b67021d96bdd12fba3dedfdaa3d2b77c5c8bd
Liu An <asiro@qq.com>
2025-06-05 12:04:09 +0800

    Fix: remove zhipu ai api key (#8066)
    ### What problem does this PR solve?
    
    - Removed hardcoded Zhipu API key from codebase
    - New requirement: Tests now require ZHIPU_AI_API_KEY environment
    variable
      Example: export ZHIPU_AI_API_KEY=your_api_key_here
    
    ### Type of change
    
    - [x] Bug Fix (non-breaking change which fixes an issue)

--- sdk/python/test/conftest.py
+++ sdk/python/test/conftest.py
@@ -20,7 +20,7 @@ import pytest
 import requests
 
 HOST_ADDRESS = os.getenv("HOST_ADDRESS", "http://127.0.0.1:9380")
-ZHIPU_AI_API_KEY = os.getenv("ZHIPU_AI_API_KEY", "ca148e43209c40109e2bc2f56281dd11.BltyA2N1B043B7Ra")
+ZHIPU_AI_API_KEY = os.getenv("ZHIPU_AI_API_KEY")
 if ZHIPU_AI_API_KEY is None:
     pytest.exit("Error: Environment variable ZHIPU_AI_API_KEY must be set")
 
