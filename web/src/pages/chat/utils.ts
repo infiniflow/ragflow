841291dda00c7e69b6180a35e5373f3833396cac
balibabu <cike8899@users.noreply.github.com>
2025-06-05 17:43:28 +0800

    Fix: Fixed an issue where using the new quote markers would cause dialogue output to have delete symbols #7623 (#8083)
    ### What problem does this PR solve?
    
    Fix: Fixed an issue where using the new quote markers would cause
    dialogue output to have delete symbols #7623
    ### Type of change
    
    - [x] Bug Fix (non-breaking change which fixes an issue)

--- web/src/pages/chat/utils.ts
+++ web/src/pages/chat/utils.ts
@@ -43,17 +43,15 @@ export const buildMessageItemReference = (
 };
 
 const oldReg = /(#{2}\d+\${2})/g;
-const currentReg = /\[ID:(\d+)\]/g;
-
-function transformReg(substring: string) {
-  return `~~${substring.slice(4, -1)}==`;
-}
+export const currentReg = /\[ID:(\d+)\]/g;
 
 // To be compatible with the old index matching mode
 export const replaceTextByOldReg = (text: string) => {
-  return text
-    ?.replace(currentReg, transformReg)
-    .replace(oldReg, (substring: string) => {
-      return `~~${substring.slice(2, -2)}==`;
-    });
+  return (
+    text
+      // ?.replace(currentReg, transformReg)
+      .replace(oldReg, (substring: string) => {
+        return `[ID:${substring.slice(2, -2)}]`;
+      })
+  );
 };
