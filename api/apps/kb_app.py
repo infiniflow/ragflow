e6695431078ba5ce9447076bb3ef252b854db16e
RyanFernandes23 <134831636+RyanFernandes23@users.noreply.github.com>
2025-06-19 07:24:30 +0530

    Fix typo in dataset name length error message (#8351)
    ### What problem does this PR solve?
    
    Fixes a minor grammar issue in a user-facing error message. The original
    message said "large than" instead of the correct comparative form
    "larger than". Just a quick fix I noticed while reading the code.
    
    ### Type of change
    
    - [x] Bug Fix (non-breaking change which fixes an issue)

--- api/apps/kb_app.py
+++ api/apps/kb_app.py
@@ -49,7 +49,7 @@ def create():
         return get_data_error_result(message="Dataset name can't be empty.")
     if len(dataset_name.encode("utf-8")) > DATASET_NAME_LIMIT:
         return get_data_error_result(
-            message=f"Dataset name length is {len(dataset_name)} which is large than {DATASET_NAME_LIMIT}")
+            message=f"Dataset name length is {len(dataset_name)} which is larger than {DATASET_NAME_LIMIT}")
 
     dataset_name = dataset_name.strip()
     dataset_name = duplicate_name(
