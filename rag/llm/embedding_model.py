2f3c50948e6007039328179222a14289c6038792
Rainman <a0933101297@gmail.com>
2025-06-23 17:15:40 +0800

    fix a bug when using huggingface embedding api

--- rag/llm/embedding_model.py
+++ rag/llm/embedding_model.py
@@ -871,14 +871,11 @@ class HuggingFaceEmbed(Base):
                 headers={'Content-Type': 'application/json'}
             )
             if response.status_code == 200:
-                try:
-                    embedding = response.json()
-                    embeddings.append(embedding[0])
-                    return np.array(embeddings), sum([num_tokens_from_string(text) for text in texts])
-                except Exception as _e:
-                    log_exception(_e, response)
+                embedding = response.json()
+                embeddings.append(embedding[0])
             else:
                 raise Exception(f"Error: {response.status_code} - {response.text}")
+        return np.array(embeddings), sum([num_tokens_from_string(text) for text in texts])
 
     def encode_queries(self, text):
         response = requests.post(
@@ -887,11 +884,8 @@ class HuggingFaceEmbed(Base):
             headers={'Content-Type': 'application/json'}
         )
         if response.status_code == 200:
-            try:
-                embedding = response.json()
-                return np.array(embedding[0]), num_tokens_from_string(text)
-            except Exception as _e:
-                log_exception(_e, response)
+            embedding = response.json()
+            return np.array(embedding[0]), num_tokens_from_string(text)
         else:
             raise Exception(f"Error: {response.status_code} - {response.text}")
 
