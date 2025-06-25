0d26bc5a9b435cd78f28f869a2111577a9ac2f7e
Rainman5042 <a0933101297@gmail.com>
2025-06-25 09:46:40 +0800

    fix the error 'Unknown field for GenerationConfig: max_tokens' when using gemini chat api

--- rag/llm/chat_model.py
+++ rag/llm/chat_model.py
@@ -1224,6 +1224,8 @@ class GeminiChat(Base):
     def _chat(self, history, gen_conf):
         from google.generativeai.types import content_types
 
+        gen_conf = self._clean_conf(gen_conf)
+
         system = history[0]["content"] if history and history[0]["role"] == "system" else ""
         hist = []
         for item in history:
@@ -1247,11 +1249,10 @@ class GeminiChat(Base):
     def chat_streamly(self, system, history, gen_conf):
         from google.generativeai.types import content_types
 
+        gen_conf = self._clean_conf(gen_conf)
+
         if system:
             self.model._system_instruction = content_types.to_content(system)
-        for k in list(gen_conf.keys()):
-            if k not in ["temperature", "top_p", "max_tokens"]:
-                del gen_conf[k]
         for item in history:
             if "role" in item and item["role"] == "assistant":
                 item["role"] = "model"
