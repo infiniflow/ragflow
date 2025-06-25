f5372abdccd6b267119dee861de63d7089ffa825
Kevin Hu <kevinhu.sh@gmail.com>
2025-06-25 15:34:22 +0800

    Update chat_model.py

--- rag/llm/chat_model.py
+++ rag/llm/chat_model.py
@@ -1217,15 +1217,12 @@ class GeminiChat(Base):
 
     def _clean_conf(self, gen_conf):
         for k in list(gen_conf.keys()):
-            if k not in ["temperature", "top_p"]:
+            if k not in ["temperature", "top_p", "max_tokens"]:
                 del gen_conf[k]
         return gen_conf
 
     def _chat(self, history, gen_conf):
         from google.generativeai.types import content_types
-
-        gen_conf = self._clean_conf(gen_conf)
-
         system = history[0]["content"] if history and history[0]["role"] == "system" else ""
         hist = []
         for item in history:
@@ -1248,9 +1245,7 @@ class GeminiChat(Base):
 
     def chat_streamly(self, system, history, gen_conf):
         from google.generativeai.types import content_types
-
         gen_conf = self._clean_conf(gen_conf)
-
         if system:
             self.model._system_instruction = content_types.to_content(system)
         for item in history:
