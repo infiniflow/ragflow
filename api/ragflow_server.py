40230ec1ab07ab5ac7842d298fb4439587014f20
Yongteng Lei <yongtengrey@outlook.com>
2025-06-26 13:52:01 +0800

    Feat: add MCP dashboard functionalities list_tools and test_tool (#8505)
    ### What problem does this PR solve?
    
    Add MCP dashboard functionalities list_tools and test_tool.
    
    ### Type of change
    
    - [x] New Feature (non-breaking change which adds functionality)

--- api/ragflow_server.py
+++ api/ragflow_server.py
@@ -19,6 +19,7 @@
 # beartype_all(conf=BeartypeConf(violation_type=UserWarning))    # <-- emit warnings from all code
 
 from api.utils.log_utils import init_root_logger
+from mcp_client.mcp_tool_call import shutdown_all_mcp_sessions
 from plugin import GlobalPluginManager
 init_root_logger("ragflow_server")
 
@@ -66,6 +67,7 @@ def update_progress():
 
 def signal_handler(sig, frame):
     logging.info("Received interrupt signal, shutting down...")
+    shutdown_all_mcp_sessions()
     stop_event.set()
     time.sleep(1)
     sys.exit(0)
