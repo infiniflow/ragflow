2463b8323fa6da67379310079be58bb5a1ceb3cd
RafaelFFAumo <rafael@aumo.ai>
2025-06-26 23:11:24 -0300

    adding websocket endpoint

--- sandbox/executor_manager/api/routes.py
+++ sandbox/executor_manager/api/routes.py
@@ -15,9 +15,10 @@
 #
 from fastapi import APIRouter
 
-from api.handlers import healthz_handler, run_code_handler
+from api.handlers import healthz_handler, run_code_handler, run_code_handler_ws
 
 router = APIRouter()
 
 router.get("/healthz")(healthz_handler)
 router.post("/run")(run_code_handler)
+router.websocket("/run_ws")(run_code_handler_ws)
--- sandbox/tests/sandbox_semaphore_test.py
+++ sandbox/tests/sandbox_semaphore_test.py
@@ -1,10 +1,12 @@
 import aiohttp
+import json
 import asyncio
 import textwrap
 import base64
 import time
 
 host = "http://localhost:9385"
+host_ws = "ws://localhost:9385"
 _CONCURRENT_TASKS = 100
 
 def code(        
@@ -23,6 +25,20 @@ def create_model(
         "arguments": {},
     }
 
+async def websocket_communication(
+    message: dict,
+    num: int,      
+) -> None:
+    start_time = time.time()
+    async with aiohttp.ClientSession() as session, session.ws_connect(f"{host_ws}/run_ws") as ws:
+        await ws.send_str(json.dumps(message))
+        async for msg in ws:
+            print(f"Server {num + 1}: {msg.data}")
+            if "status" in msg.data:
+                print(f"stdout {num + 1}: {json.loads(msg.data)['stdout']}")
+                break
+        print(f"Coroutine {num + 1} runtime : {time.time()-start_time}")
+
 async def http_communication(
     message: dict,
     num: int,      
@@ -37,7 +53,11 @@ async def http_communication(
 
 async def main():
     message = create_model(code=code())
+    websocket_list = []
     http_list = []
+    for i in range(_CONCURRENT_TASKS):
+        websocket_list.append(websocket_communication(message=message,num=i))
+    await asyncio.gather(*websocket_list)
     for i in range(_CONCURRENT_TASKS):
         http_list.append(http_communication(message=message,num=i))
     await asyncio.gather(*http_list)
