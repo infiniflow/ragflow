d918683834d2f7e662ca725c3cf7999536ccd76d
6607changchun <84566142+6607changchun@users.noreply.github.com>
2025-07-06 21:38:45 +0800

    fix: retry embedding with Qwen family models when limits temporarily reach.
    APIs of Qwen family models are limited by calling rates. When reached, the "output" attribute of the "resp" will be None, and in turn cause TypeError when trying to retrieve "embeddings". Since these limits are almost temporary, I have added a simple retry mechanism to avoid it. Besides, if retry_max reached, the error can be early raised, instead of hidden behind "TypeError".

--- rag/llm/embedding_model.py
+++ rag/llm/embedding_model.py
@@ -206,7 +206,14 @@ class QWenEmbed(Base):
         token_count = 0
         texts = [truncate(t, 2048) for t in texts]
         for i in range(0, len(texts), batch_size):
+            retry_max = 5
             resp = dashscope.TextEmbedding.call(model=self.model_name, input=texts[i : i + batch_size], api_key=self.key, text_type="document")
+            while resp["output"] is None and retry_max > 0:
+                resp = dashscope.TextEmbedding.call(model=self.model_name, input=texts[i : i + batch_size], api_key=self.key, text_type="document")
+                retry_max -= 1
+            if retry_max == 0 and resp["output"] is None:
+                log_exception(ValueException("Retry_max reached, calling embedding model failed"))
+                raise
             try:
                 embds = [[] for _ in range(len(resp["output"]["embeddings"]))]
                 for e in resp["output"]["embeddings"]:
