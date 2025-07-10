2e0905d06a1a148aa7a65c51abb2f0d180ad09b8
balibabu <cike8899@users.noreply.github.com>
2025-07-10 13:48:12 +0800

    Feat: Avoid the form sheet covering the chat sheet #3221 (#8768)
    ### What problem does this PR solve?
    
    Feat: Avoid the form sheet covering the chat sheet #3221
    
    ### Type of change
    
    
    - [x] New Feature (non-breaking change which adds functionality)

--- web/src/pages/agent/form-sheet/next.tsx
+++ web/src/pages/agent/form-sheet/next.tsx
@@ -25,6 +25,7 @@ interface IProps {
   singleDebugDrawerVisible: IModalProps<any>['visible'];
   hideSingleDebugDrawer: IModalProps<any>['hideModal'];
   showSingleDebugDrawer: IModalProps<any>['showModal'];
+  chatVisible: boolean;
 }
 
 const EmptyContent = () => <div></div>;
@@ -34,6 +35,7 @@ const FormSheet = ({
   hideModal,
   node,
   singleDebugDrawerVisible,
+  chatVisible,
   hideSingleDebugDrawer,
   showSingleDebugDrawer,
 }: IModalProps<any> & IProps) => {
@@ -55,7 +57,9 @@ const FormSheet = ({
   return (
     <Sheet open={visible} modal={false}>
       <SheetContent
-        className={cn('top-20 p-0 flex flex-col pb-20')}
+        className={cn('top-20 p-0 flex flex-col pb-20', {
+          'right-[620px]': chatVisible,
+        })}
         closeIcon={false}
       >
         <SheetHeader>
