d05b405394b7679e82040c2f4d0decc9710d5084
balibabu <cike8899@users.noreply.github.com>
2025-07-11 18:18:31 +0800

    Feat: Import and export MCP Server #3221 (#8806)
    ### What problem does this PR solve?
    
    Feat: Import and export MCP Server #3221
    
    ### Type of change
    
    
    - [x] New Feature (non-breaking change which adds functionality)

--- web/src/pages/profile-setting/mcp/index.tsx
+++ web/src/pages/profile-setting/mcp/index.tsx
@@ -3,29 +3,37 @@ import { Button } from '@/components/ui/button';
 import { SearchInput } from '@/components/ui/input';
 import { useListMcpServer } from '@/hooks/use-mcp-request';
 import { Import, Plus } from 'lucide-react';
+import { useTranslation } from 'react-i18next';
 import { EditMcpDialog } from './edit-mcp-dialog';
+import { ImportMcpDialog } from './import-mcp-dialog';
 import { McpCard } from './mcp-card';
 import { useBulkOperateMCP } from './use-bulk-operate-mcp';
 import { useEditMcp } from './use-edit-mcp';
+import { useImportMcp } from './use-import-mcp';
 
 export default function McpServer() {
   const { data } = useListMcpServer();
   const { editVisible, showEditModal, hideEditModal, handleOk, id } =
     useEditMcp();
   const { list, selectedList, handleSelectChange } = useBulkOperateMCP();
+  const { t } = useTranslation();
+  const { importVisible, showImportModal, hideImportModal, onImportOk } =
+    useImportMcp();
 
   return (
     <section className="p-4">
       <div className="text-text-title text-2xl">MCP Servers</div>
       <section className="flex items-center justify-between pb-5">
-        <div className="text-text-sub-title">自定义 MCP Server 的列表</div>
+        <div className="text-text-sub-title">
+          Customize the list of MCP servers
+        </div>
         <div className="flex gap-5">
           <SearchInput className="w-40"></SearchInput>
-          <Button variant={'secondary'}>
-            <Import /> Import
+          <Button variant={'secondary'} onClick={showImportModal}>
+            <Import /> {t('mcp.import')}
           </Button>
           <Button onClick={showEditModal('')}>
-            <Plus /> Add MCP
+            <Plus /> {t('mcp.addMcp')}
           </Button>
         </div>
       </section>
@@ -53,6 +61,12 @@ export default function McpServer() {
           onOk={handleOk}
           id={id}
         ></EditMcpDialog>
+      )}
+      {importVisible && (
+        <ImportMcpDialog
+          hideModal={hideImportModal}
+          onOk={onImportOk}
+        ></ImportMcpDialog>
       )}
     </section>
   );
