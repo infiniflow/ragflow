ece3f5dc621d7680ea1da7d92de7b4d7394b4f94
Stephen Hu <stephenhu@seismic.com>
2025-07-14 13:35:54 +0800

    Update embedding_model.py

--- rag/llm/embedding_model.py
+++ rag/llm/embedding_model.py
@@ -104,9 +104,12 @@ class DefaultEmbedding(Base):
         token_count = 0
         for t in texts:
             token_count += num_tokens_from_string(t)
-        ress = np.array([])
+        ress = None
         for i in range(0, len(texts), batch_size):
-            ress = np.concatenate((ress, self._model.encode(texts[i : i + batch_size], convert_to_numpy=True)), axis=0)
+            if ress is None:
+                ress = self._model.encode(texts[i : i + batch_size], convert_to_numpy=True)
+            else:
+                ress = np.concatenate((ress, self._model.encode(texts[i : i + batch_size], convert_to_numpy=True)), axis=0)
         return ress, token_count
 
     def encode_queries(self, text: str):
