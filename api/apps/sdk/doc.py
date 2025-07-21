e632bb4181c51ed3aa9d58b10a15b0d04f101748
tuankg1028 <lethanhtuan1028@gmail.com>
2025-07-21 14:48:18 +0700

    Handles missing request.json gracefully
    Ensures the application doesn't crash when request.json is empty.
    Now, it safely defaults to an empty dictionary when accessing request.json to retrieve the "user_type" value.

--- api/apps/sdk/doc.py
+++ api/apps/sdk/doc.py
@@ -147,7 +147,9 @@ def upload(dataset_id, tenant_id):
         )
     """
     e, kb = KnowledgebaseService.get_by_id(dataset_id)
-    kb["user_type"] = request.json.get("user_type", "homefarm")
+    print(f"request.json: {request.json}")
+    print(f"request.form: {request.form}")
+    kb["user_type"] = (request.json or {}).get("user_type", "homefarm")
     if not e:
         raise LookupError(f"Can't find the dataset with ID {dataset_id}!")
     err, files = FileService.upload_document(kb, file_objs, tenant_id)
