f9cfda385f96f20273369e39e045fd8401bfbd2e
balibabu <cike8899@users.noreply.github.com>
2025-07-22 09:27:21 +0800

    Fix: Fixed the issue of clicking to run the agent causing an error #3221 (#8956)
    ### What problem does this PR solve?
    
    Fix: Fixed the issue of clicking to run the agent causing an error #3221
    
    ### Type of change
    
    - [x] Bug Fix (non-breaking change which fixes an issue)

--- web/src/locales/en.ts
+++ web/src/locales/en.ts
@@ -1295,6 +1295,9 @@ This delimiter is used to split the input text into several text pieces echo of 
       agentDescription:
         'Builds agent components equipped with reasoning, tool usage, and multi-agent collaboration. ',
       maxRecords: 'Max records',
+      stringTransform: 'String transform',
+      userFillUp: 'Input',
+      codeExec: 'Code',
     },
     llmTools: {
       bad_calculator: {
