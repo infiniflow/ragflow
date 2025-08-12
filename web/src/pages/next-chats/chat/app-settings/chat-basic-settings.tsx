3d18284dd6c743dd6ccf9b84c1224d8de5755134
balibabu <cike8899@users.noreply.github.com>
2025-08-12 16:19:23 +0800

    Feat: Added meta data to the chat configuration page #8531 (#9417)
    ### What problem does this PR solve?
    
    Feat: Added meta data to the chat configuration page #8531
    
    ### Type of change
    
    
    - [x] New Feature (non-breaking change which adds functionality)

--- web/src/pages/next-chats/chat/app-settings/chat-basic-settings.tsx
+++ web/src/pages/next-chats/chat/app-settings/chat-basic-settings.tsx
@@ -11,108 +11,101 @@ import {
   FormMessage,
 } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
+import { Textarea } from '@/components/ui/textarea';
 import { useTranslate } from '@/hooks/common-hooks';
 import { useFormContext } from 'react-hook-form';
-import { Subhead } from './subhead';
 
 export default function ChatBasicSetting() {
   const { t } = useTranslate('chat');
   const form = useFormContext();
 
   return (
-    <section>
-      <Subhead>Basic settings</Subhead>
-      <div className="space-y-8">
-        <FormField
-          control={form.control}
-          name={'icon'}
-          render={({ field }) => (
-            <div className="space-y-6">
-              <FormItem className="w-full">
-                <FormLabel>{t('assistantAvatar')}</FormLabel>
-                <FormControl>
-                  <FileUploader
-                    value={field.value}
-                    onValueChange={field.onChange}
-                    maxFileCount={1}
-                    maxSize={4 * 1024 * 1024}
-                    // progresses={progresses}
-                    // pass the onUpload function here for direct upload
-                    // onUpload={uploadFiles}
-                    // disabled={isUploading}
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            </div>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="name"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>{t('assistantName')}</FormLabel>
+    <div className="space-y-8">
+      <FormField
+        control={form.control}
+        name={'icon'}
+        render={({ field }) => (
+          <div className="space-y-6">
+            <FormItem className="w-full">
+              <FormLabel>{t('assistantAvatar')}</FormLabel>
               <FormControl>
-                <Input {...field}></Input>
+                <FileUploader
+                  value={field.value}
+                  onValueChange={field.onChange}
+                  maxFileCount={1}
+                  maxSize={4 * 1024 * 1024}
+                />
               </FormControl>
               <FormMessage />
             </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="description"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>{t('description')}</FormLabel>
-              <FormControl>
-                <Input {...field}></Input>
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name={'prompt_config.empty_response'}
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>{t('emptyResponse')}</FormLabel>
-              <FormControl>
-                <Input {...field}></Input>
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name={'prompt_config.prologue'}
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>{t('setAnOpener')}</FormLabel>
-              <FormControl>
-                <Input {...field}></Input>
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <SwitchFormField
-          name={'prompt_config.quote'}
-          label={t('quote')}
-        ></SwitchFormField>
-        <SwitchFormField
-          name={'prompt_config.keyword'}
-          label={t('keyword')}
-        ></SwitchFormField>
-        <SwitchFormField
-          name={'prompt_config.tts'}
-          label={t('tts')}
-        ></SwitchFormField>
-        <KnowledgeBaseFormField></KnowledgeBaseFormField>
-      </div>
-    </section>
+          </div>
+        )}
+      />
+      <FormField
+        control={form.control}
+        name="name"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>{t('assistantName')}</FormLabel>
+            <FormControl>
+              <Input {...field}></Input>
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      <FormField
+        control={form.control}
+        name="description"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>{t('description')}</FormLabel>
+            <FormControl>
+              <Textarea {...field}></Textarea>
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      <FormField
+        control={form.control}
+        name={'prompt_config.empty_response'}
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>{t('emptyResponse')}</FormLabel>
+            <FormControl>
+              <Textarea {...field}></Textarea>
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      <FormField
+        control={form.control}
+        name={'prompt_config.prologue'}
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>{t('setAnOpener')}</FormLabel>
+            <FormControl>
+              <Textarea {...field}></Textarea>
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      <SwitchFormField
+        name={'prompt_config.quote'}
+        label={t('quote')}
+      ></SwitchFormField>
+      <SwitchFormField
+        name={'prompt_config.keyword'}
+        label={t('keyword')}
+      ></SwitchFormField>
+      <SwitchFormField
+        name={'prompt_config.tts'}
+        label={t('tts')}
+      ></SwitchFormField>
+      <KnowledgeBaseFormField></KnowledgeBaseFormField>
+    </div>
   );
 }
--- web/src/pages/next-chats/chat/app-settings/chat-settings.tsx
+++ web/src/pages/next-chats/chat/app-settings/chat-settings.tsx
@@ -1,6 +1,10 @@
 import { Button } from '@/components/ui/button';
+import { Separator } from '@/components/ui/separator';
+import { useFetchDialog } from '@/hooks/use-chat-request';
+import { transformBase64ToFile } from '@/utils/file-util';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { PanelRightClose } from 'lucide-react';
+import { useEffect } from 'react';
 import { FormProvider, useForm } from 'react-hook-form';
 import { z } from 'zod';
 import ChatBasicSetting from './chat-basic-settings';
@@ -11,6 +15,7 @@ import { useChatSettingSchema } from './use-chat-setting-schema';
 type ChatSettingsProps = { switchSettingVisible(): void };
 export function ChatSettings({ switchSettingVisible }: ChatSettingsProps) {
   const formSchema = useChatSettingSchema();
+  const { data } = useFetchDialog();
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -34,6 +39,14 @@ export function ChatSettings({ switchSettingVisible }: ChatSettingsProps) {
     console.log(values);
   }
 
+  useEffect(() => {
+    const nextData = {
+      ...data,
+      icon: data.icon ? [transformBase64ToFile(data.icon)] : [],
+    };
+    form.reset(nextData as z.infer<typeof formSchema>);
+  }, [data, form]);
+
   return (
     <section className="p-5  w-[400px] max-w-[20%]">
       <div className="flex justify-between items-center text-base">
@@ -49,7 +62,9 @@ export function ChatSettings({ switchSettingVisible }: ChatSettingsProps) {
           className="space-y-6 overflow-auto max-h-[87vh] pr-4"
         >
           <ChatBasicSetting></ChatBasicSetting>
+          <Separator />
           <ChatPromptEngine></ChatPromptEngine>
+          <Separator />
           <ChatModelSettings></ChatModelSettings>
         </form>
       </FormProvider>
