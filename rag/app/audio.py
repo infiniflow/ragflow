273fc0d0643df5ba72c5d6e3502f20f7d46ddea9
Yongteng Lei <yongtengrey@outlook.com>
2025-08-19 16:41:18 +0800

    Refa: OpenAI whisper-1 (#9552)
    ### What problem does this PR solve?
    
    Refactor OpenAI to enable audio parsing.
    
    ### Type of change
    
    - [x] Bug Fix (non-breaking change which fixes an issue)
    - [x] Refactoring

--- rag/app/audio.py
+++ rag/app/audio.py
@@ -14,31 +14,48 @@
 #  limitations under the License.
 #
 
+import os
 import re
+import tempfile
 
 from api.db import LLMType
-from rag.nlp import rag_tokenizer
 from api.db.services.llm_service import LLMBundle
-from rag.nlp import tokenize
+from rag.nlp import rag_tokenizer, tokenize
 
 
 def chunk(filename, binary, tenant_id, lang, callback=None, **kwargs):
-    doc = {
-        "docnm_kwd": filename,
-        "title_tks": rag_tokenizer.tokenize(re.sub(r"\.[a-zA-Z]+$", "", filename))
-    }
+    doc = {"docnm_kwd": filename, "title_tks": rag_tokenizer.tokenize(re.sub(r"\.[a-zA-Z]+$", "", filename))}
     doc["title_sm_tks"] = rag_tokenizer.fine_grained_tokenize(doc["title_tks"])
 
     # is it English
     eng = lang.lower() == "english"  # is_english(sections)
     try:
+        _, ext = os.path.splitext(filename)
+        if not ext:
+            raise RuntimeError("No extension detected.")
+
+        if ext not in [".da", ".wave", ".wav", ".mp3", ".wav", ".aac", ".flac", ".ogg", ".aiff", ".au", ".midi", ".wma", ".realaudio", ".vqf", ".oggvorbis", ".aac", ".ape"]:
+            raise RuntimeError(f"Extension {ext} is not supported yet.")
+
+        tmp_path = ""
+        with tempfile.NamedTemporaryFile(suffix=ext, delete=False) as tmpf:
+            tmpf.write(binary)
+            tmpf.flush()
+            tmp_path = os.path.abspath(tmpf.name)
+
         callback(0.1, "USE Sequence2Txt LLM to transcription the audio")
         seq2txt_mdl = LLMBundle(tenant_id, LLMType.SPEECH2TEXT, lang=lang)
-        ans = seq2txt_mdl.transcription(binary)
+        ans = seq2txt_mdl.transcription(tmp_path)
         callback(0.8, "Sequence2Txt LLM respond: %s ..." % ans[:32])
+
         tokenize(doc, ans, eng)
         return [doc]
     except Exception as e:
         callback(prog=-1, msg=str(e))
-
+    finally:
+        if tmp_path and os.path.exists(tmp_path):
+            try:
+                os.unlink(tmp_path)
+            except Exception:
+                pass
     return []
