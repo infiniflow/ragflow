e9a7552fd25ca47ee4079c7e97ece9ab9c86f42a
bill <yibie_jingnian@163.com>
2025-08-20 11:49:47 +0800

    Feat: Make the old page accessible via URL #3221

--- web/src/routes.ts
+++ web/src/routes.ts
@@ -70,116 +70,73 @@ const routes = [
     component: `@/pages${Routes.AgentShare}`,
     layout: false,
   },
-  // {
-  //   path: '/',
-  //   component: '@/layouts',
-  //   layout: false,
-  //   wrappers: ['@/wrappers/auth'],
-  //   routes: [
-  //     { path: '/', redirect: '/knowledge' },
-  //     {
-  //       path: '/knowledge',
-  //       component: '@/pages/knowledge',
-  //       // component: '@/pages/knowledge/datasets',
-  //     },
-  //     {
-  //       path: '/knowledge',
-  //       component: '@/pages/add-knowledge',
-  //       routes: [
-  //         {
-  //           path: '/knowledge/dataset',
-  //           component: '@/pages/add-knowledge/components/knowledge-dataset',
-  //           routes: [
-  //             {
-  //               path: '/knowledge/dataset',
-  //               component: '@/pages/add-knowledge/components/knowledge-file',
-  //             },
-  //             {
-  //               path: '/knowledge/dataset/chunk',
-  //               component: '@/pages/add-knowledge/components/knowledge-chunk',
-  //             },
-  //           ],
-  //         },
-  //         {
-  //           path: '/knowledge/configuration',
-  //           component: '@/pages/add-knowledge/components/knowledge-setting',
-  //         },
-  //         {
-  //           path: '/knowledge/testing',
-  //           component: '@/pages/add-knowledge/components/knowledge-testing',
-  //         },
-  //         {
-  //           path: '/knowledge/knowledgeGraph',
-  //           component: '@/pages/add-knowledge/components/knowledge-graph',
-  //         },
-  //       ],
-  //     },
-  //     {
-  //       path: '/chat',
-  //       component: '@/pages/chat',
-  //     },
-  //     {
-  //       path: '/user-setting',
-  //       component: '@/pages/user-setting',
-  //       routes: [
-  //         { path: '/user-setting', redirect: '/user-setting/profile' },
-  //         {
-  //           path: '/user-setting/profile',
-  //           // component: '@/pages/user-setting/setting-profile',
-  //           component: '@/pages/user-setting/setting-profile',
-  //         },
-  //         {
-  //           path: '/user-setting/locale',
-  //           component: '@/pages/user-setting/setting-locale',
-  //         },
-  //         {
-  //           path: '/user-setting/password',
-  //           component: '@/pages/user-setting/setting-password',
-  //         },
-  //         {
-  //           path: '/user-setting/model',
-  //           component: '@/pages/user-setting/setting-model',
-  //         },
-  //         {
-  //           path: '/user-setting/team',
-  //           component: '@/pages/user-setting/setting-team',
-  //         },
-  //         {
-  //           path: '/user-setting/system',
-  //           component: '@/pages/user-setting/setting-system',
-  //         },
-  //         {
-  //           path: '/user-setting/api',
-  //           component: '@/pages/user-setting/setting-api',
-  //         },
-  //         {
-  //           path: `/user-setting${Routes.Mcp}`,
-  //           component: `@/pages${Routes.ProfileMcp}`,
-  //         },
-  //       ],
-  //     },
-  //     {
-  //       path: '/file',
-  //       component: '@/pages/file-manager',
-  //     },
-  //     {
-  //       path: '/flow',
-  //       component: '@/pages/flow/list',
-  //     },
-  //     {
-  //       path: Routes.AgentList,
-  //       component: `@/pages/${Routes.Agents}`,
-  //     },
-  //     {
-  //       path: '/flow/:id',
-  //       component: '@/pages/flow',
-  //     },
-  //     {
-  //       path: '/search',
-  //       component: '@/pages/search',
-  //     },
-  //   ],
-  // },
+  {
+    path: Routes.Home,
+    component: '@/layouts',
+    layout: false,
+    redirect: '/knowledge',
+  },
+  {
+    path: '/knowledge',
+    component: '@/pages/knowledge',
+  },
+  {
+    path: '/knowledge',
+    component: '@/pages/add-knowledge',
+    routes: [
+      {
+        path: 'dataset',
+        component: '@/pages/add-knowledge/components/knowledge-dataset',
+        routes: [
+          {
+            path: '',
+            component: '@/pages/add-knowledge/components/knowledge-file',
+          },
+          {
+            path: 'chunk',
+            component: '@/pages/add-knowledge/components/knowledge-chunk',
+          },
+        ],
+      },
+      {
+        path: 'configuration',
+        component: '@/pages/add-knowledge/components/knowledge-setting',
+      },
+      {
+        path: 'testing',
+        component: '@/pages/add-knowledge/components/knowledge-testing',
+      },
+      {
+        path: 'knowledgeGraph',
+        component: '@/pages/add-knowledge/components/knowledge-graph',
+      },
+    ],
+  },
+
+  {
+    path: '/chat',
+    component: '@/pages/chat',
+  },
+  {
+    path: '/file',
+    component: '@/pages/file-manager',
+  },
+  {
+    path: '/flow',
+    component: '@/pages/flow/list',
+  },
+  {
+    path: Routes.AgentList,
+    component: `@/pages/${Routes.Agents}`,
+  },
+  {
+    path: '/flow/:id',
+    component: '@/pages/flow',
+  },
+  {
+    path: '/search',
+    component: '@/pages/search',
+  },
   {
     path: '/document/:id',
     component: '@/pages/document-viewer',
