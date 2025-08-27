cbdabbb58f43ec98e1bbf215b01da81817f96db8
balibabu <cike8899@users.noreply.github.com>
2025-08-27 14:18:00 +0800

    Fix: Fixed the issue that the agent embedded page needs to be logged in #9750 (#9751)
    ### What problem does this PR solve?
    
    Fix: Fixed the issue that the agent embedded page needs to be logged in
    #9750
    
    ### Type of change
    
    - [x] Bug Fix (non-breaking change which fixes an issue)

--- web/src/pages/next-chats/chat/app-settings/chat-basic-settings.tsx
+++ web/src/pages/next-chats/chat/app-settings/chat-basic-settings.tsx
@@ -13,8 +13,14 @@ import {
   FormMessage,
 } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
 import { Textarea } from '@/components/ui/textarea';
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 import { useTranslate } from '@/hooks/common-hooks';
 import { useFormContext } from 'react-hook-form';
 
@@ -47,6 +53,9 @@ export default function ChatBasicSetting() {
                   value={field.value}
                   onValueChange={field.onChange}
                   maxFileCount={1}
+                  description={t('photoTip', {
+                    keyPrefix: 'knowledgeConfiguration',
+                  })}
                 />
               </FormControl>
               <FormMessage />
