aa1251af9a349f228a1fdf28e53f5da13f99acb5
balibabu <cike8899@users.noreply.github.com>
2025-09-04 17:03:36 +0800

    Feat: Use storybook to display public components. #9914 (#9915)
    ### What problem does this PR solve?
    Feat: Use storybook to display public components. #9914
    ### Type of change
    
    
    - [x] New Feature (non-breaking change which adds functionality)

---
+++ web/.storybook/preview.ts
@@ -0,0 +1,15 @@
+import type { Preview } from '@storybook/react-webpack5';
+import '../tailwind.css';
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+export default preview;
