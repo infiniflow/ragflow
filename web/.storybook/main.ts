8fdd2d87293da7f36aa797bea04b3914af832bea
bill <yibie_jingnian@163.com>
2025-09-04 15:17:51 +0800

    Feat: Enable Storybook to use TailwindCSS

--- web/.storybook/main.ts
+++ web/.storybook/main.ts
@@ -1,4 +1,5 @@
 import type { StorybookConfig } from '@storybook/react-webpack5';
+import path from 'path';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
@@ -6,10 +7,41 @@ const config: StorybookConfig = {
     '@storybook/addon-webpack5-compiler-swc',
     '@storybook/addon-docs',
     '@storybook/addon-onboarding',
+    {
+      name: '@storybook/addon-styling-webpack',
+      options: {
+        rules: [
+          {
+            test: /\.css$/,
+            use: [
+              'style-loader',
+              'css-loader',
+              {
+                loader: 'postcss-loader',
+                options: {
+                  postcssOptions: {
+                    plugins: [require('tailwindcss'), require('autoprefixer')],
+                  },
+                },
+              },
+            ],
+          },
+        ],
+      },
+    },
   ],
   framework: {
     name: '@storybook/react-webpack5',
     options: {},
+  },
+  webpackFinal: async (config) => {
+    if (config.resolve) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        '@': path.resolve(__dirname, '../src'),
+      };
+    }
+    return config;
   },
 };
 export default config;
--- web/.storybook/preview.ts
+++ web/.storybook/preview.ts
@@ -1,4 +1,5 @@
 import type { Preview } from '@storybook/react-webpack5';
+import '../tailwind.css';
 
 const preview: Preview = {
   parameters: {
