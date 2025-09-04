2c20fdaae53a60af51fdb266e9bd61a5a0fd0165
bill <yibie_jingnian@163.com>
2025-09-04 11:04:45 +0800

    Feat: Initialize Storybook

---
+++ web/.storybook/main.ts
@@ -0,0 +1,15 @@
+import type { StorybookConfig } from '@storybook/react-webpack5';
+
+const config: StorybookConfig = {
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    '@storybook/addon-webpack5-compiler-swc',
+    '@storybook/addon-docs',
+    '@storybook/addon-onboarding',
+  ],
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {},
+  },
+};
+export default config;
---
+++ web/.storybook/preview.ts
@@ -0,0 +1,14 @@
+import type { Preview } from '@storybook/react-webpack5';
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+export default preview;
