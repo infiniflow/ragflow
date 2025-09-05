c6fcdd671097ded173304c1825180bb7222de0ef
Yongteng Lei <yongtengrey@outlook.com>
2025-09-05 18:50:46 +0800

    Feat: refine dataflow and initialize dataflow app (#9952)
    ### What problem does this PR solve?
    
    Refine dataflow and initialize dataflow app.
    
    ### Type of change
    
    - [x] New Feature (non-breaking change which adds functionality)

--- rag/flow/pipeline.py
+++ rag/flow/pipeline.py
@@ -1,5 +1,5 @@
 #
-#  Copyright 2024 The InfiniFlow Authors. All Rights Reserved.
+#  Copyright 2025 The InfiniFlow Authors. All Rights Reserved.
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
@@ -18,14 +18,15 @@ import json
 import logging
 import random
 import time
+
 import trio
+
 from agent.canvas import Graph
 from api.db.services.document_service import DocumentService
 from rag.utils.redis_conn import REDIS_CONN
 
 
 class Pipeline(Graph):
-
     def __init__(self, dsl: str, tenant_id=None, doc_id=None, task_id=None, flow_id=None):
         super().__init__(dsl, tenant_id, task_id)
         self._doc_id = doc_id
@@ -35,7 +36,7 @@ class Pipeline(Graph):
             self._kb_id = DocumentService.get_knowledgebase_id(doc_id)
             assert self._kb_id, f"Can't find KB of this document: {doc_id}"
 
-    def callback(self, component_name: str, progress: float|int|None=None, message: str = "") -> None:
+    def callback(self, component_name: str, progress: float | int | None = None, message: str = "") -> None:
         log_key = f"{self._flow_id}-{self.task_id}-logs"
         try:
             bin = REDIS_CONN.get(log_key)
@@ -44,16 +45,10 @@ class Pipeline(Graph):
                 if obj[-1]["component_name"] == component_name:
                     obj[-1]["trace"].append({"progress": progress, "message": message, "datetime": datetime.datetime.now().strftime("%H:%M:%S")})
                 else:
-                    obj.append({
-                    "component_name": component_name,
-                    "trace": [{"progress": progress, "message": message, "datetime": datetime.datetime.now().strftime("%H:%M:%S")}]
-                })
+                    obj.append({"component_name": component_name, "trace": [{"progress": progress, "message": message, "datetime": datetime.datetime.now().strftime("%H:%M:%S")}]})
             else:
-                obj = [{
-                    "component_name": component_name,
-                    "trace": [{"progress": progress, "message": message, "datetime": datetime.datetime.now().strftime("%H:%M:%S")}]
-                }]
-            REDIS_CONN.set_obj(log_key, obj, 60*10)
+                obj = [{"component_name": component_name, "trace": [{"progress": progress, "message": message, "datetime": datetime.datetime.now().strftime("%H:%M:%S")}]}]
+            REDIS_CONN.set_obj(log_key, obj, 60 * 10)
         except Exception as e:
             logging.exception(e)
 
@@ -71,21 +66,19 @@ class Pipeline(Graph):
         super().reset()
         log_key = f"{self._flow_id}-{self.task_id}-logs"
         try:
-            REDIS_CONN.set_obj(log_key, [], 60*10)
+            REDIS_CONN.set_obj(log_key, [], 60 * 10)
         except Exception as e:
             logging.exception(e)
 
     async def run(self, **kwargs):
         st = time.perf_counter()
         if not self.path:
-            self.path.append("begin")
+            self.path.append("File")
 
         if self._doc_id:
-            DocumentService.update_by_id(self._doc_id, {
-                "progress": random.randint(0,5)/100.,
-                "progress_msg": "Start the pipeline...",
-                "process_begin_at": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            })
+            DocumentService.update_by_id(
+                self._doc_id, {"progress": random.randint(0, 5) / 100.0, "progress_msg": "Start the pipeline...", "process_begin_at": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}
+            )
 
         self.error = ""
         idx = len(self.path) - 1
@@ -99,23 +92,21 @@ class Pipeline(Graph):
                 self.path.extend(cpn_obj.get_downstream())
 
         while idx < len(self.path) and not self.error:
-            last_cpn = self.get_component_obj(self.path[idx-1])
+            last_cpn = self.get_component_obj(self.path[idx - 1])
             cpn_obj = self.get_component_obj(self.path[idx])
+
             async def invoke():
                 nonlocal last_cpn, cpn_obj
                 await cpn_obj.invoke(**last_cpn.output())
+
             async with trio.open_nursery() as nursery:
                 nursery.start_soon(invoke)
             if cpn_obj.error():
                 self.error = "[ERROR]" + cpn_obj.error()
+                self.callback(cpn_obj.component_name, -1, self.error)
                 break
             idx += 1
             self.path.extend(cpn_obj.get_downstream())
 
         if self._doc_id:
-            DocumentService.update_by_id(self._doc_id, {
-                "progress": 1 if not self.error else -1,
-                "progress_msg": "Pipeline finished...\n" + self.error,
-                "process_duration": time.perf_counter() - st
-            })
-
+            DocumentService.update_by_id(self._doc_id, {"progress": 1 if not self.error else -1, "progress_msg": "Pipeline finished...\n" + self.error, "process_duration": time.perf_counter() - st})
