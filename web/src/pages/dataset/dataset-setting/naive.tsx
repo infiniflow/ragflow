1bdd61b647a3b510243b3201ae33c1def9a00901
chanx <1243304602@qq.com>
2025-09-11 13:07:31 +0800

    fix: Displays the dataset creation and settings page

--- web/src/pages/dataset/dataset-setting/naive.tsx
+++ web/src/pages/dataset/dataset-setting/naive.tsx
@@ -1,33 +1,20 @@
 import GraphRagItems from '@/components/parse-configuration/graph-rag-form-fields';
 import RaptorFormFields from '@/components/parse-configuration/raptor-form-fields';
+import Divider from '@/components/ui/divider';
 import {
   ConfigurationFormContainer,
   MainContainer,
 } from './configuration-form-container';
-import { EnableAutoGenerateItem } from './configuration/common-item';
 
 export function NaiveConfiguration() {
   return (
     <MainContainer>
       <GraphRagItems className="border-none p-0"></GraphRagItems>
+      <Divider />
       <ConfigurationFormContainer>
         <RaptorFormFields></RaptorFormFields>
       </ConfigurationFormContainer>
-      <EnableAutoGenerateItem />
-      {/* <ConfigurationFormContainer>
-        <ChunkMethodItem></ChunkMethodItem>
-        <LayoutRecognizeFormField></LayoutRecognizeFormField>
-
-        <MaxTokenNumberFormField initialValue={512}></MaxTokenNumberFormField>
-        <DelimiterFormField></DelimiterFormField>
-      </ConfigurationFormContainer>
-      <ConfigurationFormContainer>
-        <PageRankFormField></PageRankFormField>
-        <AutoKeywordsFormField></AutoKeywordsFormField>
-        <AutoQuestionsFormField></AutoQuestionsFormField>
-        <ExcelToHtmlFormField></ExcelToHtmlFormField>
-        <TagItems></TagItems>
-      </ConfigurationFormContainer> */}
+      <Divider />
     </MainContainer>
   );
 }
