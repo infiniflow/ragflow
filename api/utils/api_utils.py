8e9ea415e877fe5ad2804de99a636fc956443a6b
jinhai <haijin.chn@gmail.com>
2025-09-22 18:51:28 +0800

    Refactor api/utils/__init__.py
    Signed-off-by: jinhai <haijin.chn@gmail.com>

--- api/utils/api_utils.py
+++ api/utils/api_utils.py
@@ -51,7 +51,8 @@ from api.constants import REQUEST_MAX_WAIT_SEC, REQUEST_WAIT_SEC
 from api.db.db_models import APIToken
 from api.db.services.llm_service import LLMService
 from api.db.services.tenant_llm_service import TenantLLMService
-from api.utils import CustomJSONEncoder, get_uuid, json_dumps
+from api.utils.json import CustomJSONEncoder, json_dumps
+from api.utils import get_uuid
 from rag.utils.mcp_tool_call_conn import MCPToolCallSession, close_multiple_mcp_toolcall_sessions
 
 requests.models.complexjson.dumps = functools.partial(json.dumps, cls=CustomJSONEncoder)
