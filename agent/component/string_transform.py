f0e1be32b8d646adaae51763b2f79b53618a1857
mxc <mxc@example.com>
2025-09-22 17:22:52 +0800

    Fix: string merge bug about agent TextProcessing. (\m)

--- agent/component/string_transform.py
+++ agent/component/string_transform.py
@@ -90,6 +90,8 @@ class StringTransform(Message, ABC):
         for k,v in kwargs.items():
             if not v:
                 v = ""
+            k = re.sub(r'\\m', 'm', k)
+            v = re.sub(r'\\m', 'm', v)
             script = re.sub(k, v, script)
 
         self.set_output("result", script)
