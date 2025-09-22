36c7d4c786396c9373ffb52f5ddb4248dd4f3994
bill <yibie_jingnian@163.com>
2025-09-22 17:22:17 +0800

    Feat: Exporting the results of data flow tests #9869

---
+++ web/src/pages/data-flow/hooks/use-download-output.ts
@@ -0,0 +1,35 @@
+import { ITraceData } from '@/interfaces/database/agent';
+import { downloadJsonFile } from '@/utils/file-util';
+import { get, isEmpty } from 'lodash';
+import { useCallback } from 'react';
+
+export function findEndOutput(list?: ITraceData[]) {
+  if (Array.isArray(list)) {
+    const trace = list.find((x) => x.component_id === 'END')?.trace;
+
+    const str = get(trace, '0.message');
+
+    try {
+      if (!isEmpty(str)) {
+        const json = JSON.parse(str);
+        return json;
+      }
+    } catch (error) {}
+  }
+}
+
+export function isEndOutputEmpty(list?: ITraceData[]) {
+  return isEmpty(findEndOutput(list));
+}
+export function useDownloadOutput(data?: ITraceData[]) {
+  const handleDownloadJson = useCallback(() => {
+    const output = findEndOutput(data);
+    if (!isEndOutputEmpty(data)) {
+      downloadJsonFile(output, `output.json`);
+    }
+  }, [data]);
+
+  return {
+    handleDownloadJson,
+  };
+}
