62b7c655c511ce458e0ecde650cc69a949b00d54
Jin Hai <haijin.chn@gmail.com>
2025-09-25 23:37:50 +0800

    Refactor: migrate the function to specific file (#10201)
    ### What problem does this PR solve?
    
    Move base64 related function to api/common/base64.py
    
    ### Type of change
    
    - [x] Refactoring
    
    ---------
    
    Signed-off-by: jinhai <haijin.chn@gmail.com>
    Signed-off-by: Jin Hai <haijin.chn@gmail.com>

--- admin/config.py
+++ admin/config.py
@@ -4,7 +4,7 @@ from enum import Enum
 
 from pydantic import BaseModel
 from typing import Any
-from api.utils import read_config
+from api.utils.configs import read_config
 from urllib.parse import urlparse
 
 
