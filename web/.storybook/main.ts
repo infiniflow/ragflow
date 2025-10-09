b3070eb63f04ebe4d36688e1bf24acc4dc6c335e
balibabu <cike8899@users.noreply.github.com>
2025-10-09 17:21:38 +0800

    Feat: Replace the collapse icon #9869 (#10442)
    ### What problem does this PR solve?
    
    Feat: Replace the collapse icon #9869
    
    ### Type of change
    
    
    - [x] New Feature (non-breaking change which adds functionality)

--- web/.storybook/main.ts
+++ web/.storybook/main.ts
@@ -3,6 +3,7 @@ import path from 'path';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  staticDirs: ['../public'],
   addons: [
     '@storybook/addon-webpack5-compiler-swc',
     '@storybook/addon-docs',
