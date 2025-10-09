f4324e89d96fd2249f40a288b3db2a2bd52ad16f
balibabu <cike8899@users.noreply.github.com>
2025-10-09 19:03:29 +0800

    Feat: Importing data flow files from the list page #9869 (#10446)
    ### What problem does this PR solve?
    
    Feat: Importing data flow files from the list page #9869
    
    ### Type of change
    
    
    - [x] New Feature (non-breaking change which adds functionality)

--- web/src/pages/data-flow/form/parser-form/video-form-fields.tsx
+++ web/src/pages/data-flow/form/parser-form/video-form-fields.tsx
@@ -1,13 +1,24 @@
+import { LlmModelType } from '@/constants/knowledge';
+import { useComposeLlmOptionsByModelTypes } from '@/hooks/llm-hooks';
 import {
   LargeModelFormField,
   OutputFormatFormFieldProps,
 } from './common-form-fields';
 
 export function VideoFormFields({ prefix }: OutputFormatFormFieldProps) {
+  const modelOptions = useComposeLlmOptionsByModelTypes([
+    LlmModelType.Chat,
+    LlmModelType.Image2text,
+    LlmModelType.Speech2text,
+  ]);
+
   return (
     <>
       {/* Multimodal Model */}
-      <LargeModelFormField prefix={prefix}></LargeModelFormField>
+      <LargeModelFormField
+        prefix={prefix}
+        options={modelOptions}
+      ></LargeModelFormField>
     </>
   );
 }
