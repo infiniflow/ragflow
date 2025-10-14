f1fccbb6f0c6e6678dc97084c2f868ef183f4b90
Lynn <lynn_inf@hotmail.com>
2025-10-14 14:30:16 +0800

    feat: make cli sdk

--- admin/admin_client.py
+++ admin/admin_client.py
@@ -23,7 +23,6 @@ from typing import Dict, List, Any
 from lark import Lark, Transformer, Tree
 import requests
 from requests.auth import HTTPBasicAuth
-from api.common.base64 import encode_to_base64
 
 GRAMMAR = r"""
 start: command
@@ -182,6 +181,11 @@ class AdminTransformer(Transformer):
 
     def meta_args(self, items):
         return items
+
+
+def encode_to_base64(input_string):
+    base64_encoded = base64.b64encode(input_string.encode('utf-8'))
+    return base64_encoded.decode('utf-8')
 
 
 def encrypt(input_string):
