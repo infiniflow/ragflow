7816e8d446ede49429c16fb7703cfd758b3f02b8
Jimmy Ben Klieve <j.b.klieve@live.cn>
2025-10-28 19:59:17 +0800

    Feat: Admin UI

--- web/src/pages/404.jsx
+++ web/src/pages/404.jsx
@@ -1,14 +1,24 @@
+import { Routes } from '@/routes';
 import { Button, Result } from 'antd';
-import { history } from 'umi';
+import { history, useLocation } from 'umi';
 
 const NoFoundPage = () => {
+  const location = useLocation();
+
   return (
     <Result
       status="404"
       title="404"
       subTitle="Page not found, please enter a correct address."
       extra={
-        <Button type="primary" onClick={() => history.push('/')}>
+        <Button
+          type="primary"
+          onClick={() => {
+            history.push(
+              location.pathname.startsWith(Routes.Admin) ? Routes.Admin : '/',
+            );
+          }}
+        >
           Business
         </Button>
       }
