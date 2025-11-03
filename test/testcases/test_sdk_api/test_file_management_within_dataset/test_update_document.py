471638b8f9ff63740a8c5673dacd34f79fc9e9b8
Billy Bao <newyorkupperbay@gmail.com>
2025-11-03 19:45:19 +0800

    fix import issue

--- test/testcases/test_sdk_api/test_file_management_within_dataset/test_update_document.py
+++ test/testcases/test_sdk_api/test_file_management_within_dataset/test_update_document.py
@@ -17,7 +17,7 @@
 import pytest
 from configs import DOCUMENT_NAME_LIMIT
 from ragflow_sdk import DataSet
-from testcases.utils.config_utils import DEFAULT_PARSER_CONFIG  
+from utils.config_utils import DEFAULT_PARSER_CONFIG  
 
 class TestDocumentsUpdated:
     @pytest.mark.p1
