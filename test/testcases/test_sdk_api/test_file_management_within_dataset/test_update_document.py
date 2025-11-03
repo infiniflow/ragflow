2f89ac9e64c422ee7d21a47a1a0dcbd1ce3c96f7
Billy Bao <newyorkupperbay@gmail.com>
2025-11-03 17:44:04 +0800

    fix testcase

--- test/testcases/test_sdk_api/test_file_management_within_dataset/test_update_document.py
+++ test/testcases/test_sdk_api/test_file_management_within_dataset/test_update_document.py
@@ -206,14 +206,34 @@ class TestUpdateDocumentParserConfig:
             ("naive", {}, ""),
             (
                 "naive",
-                {
-                    "chunk_token_num": 512,
-                    "layout_recognize": "DeepDOC",
-                    "html4excel": False,
-                    "delimiter": r"\n",
-                    "task_page_size": 12,
-                    "raptor": {"use_raptor": False},
-                },
+            {
+            "layout_recognize": "DeepDOC",
+            "chunk_token_num": 512,
+            "delimiter": "\n",
+            "auto_keywords": 0,
+            "auto_questions": 0,
+            "html4excel": False,
+            "topn_tags": 3,
+            "raptor": {
+                "use_raptor": True,
+                "prompt": "Please summarize the following paragraphs. Be careful with the numbers, do not make things up. Paragraphs as following:\n      {cluster_content}\nThe above is the content you need to summarize.",
+                "max_token": 256,
+                "threshold": 0.1,
+                "max_cluster": 64,
+                "random_seed": 0,
+            },
+            "graphrag": {
+                "use_graphrag": True,
+                "entity_types": [
+                    "organization",
+                    "person",
+                    "geo",
+                    "event",
+                    "category",
+                ],
+                "method": "light",
+            },
+        },
                 "",
             ),
             pytest.param(
