4a57d14945fc3dd9cc096f176e2e5afef566800a
Kevin Hu <kevinhu.sh@gmail.com>
2025-11-11 19:43:08 +0800

    Raptor docid.

--- rag/svr/task_executor.py
+++ rag/svr/task_executor.py
@@ -646,7 +646,7 @@ async def run_raptor_for_kb(row, kb_parser_config, chat_mdl, embd_mdl, vector_si
 
     res = []
     tk_count = 0
-    async def generate(chunks):
+    async def generate(chunks, did):
         nonlocal tk_count, res
         raptor = Raptor(
             raptor_config.get("max_cluster", 64),
@@ -659,7 +659,7 @@ async def run_raptor_for_kb(row, kb_parser_config, chat_mdl, embd_mdl, vector_si
         original_length = len(chunks)
         chunks = await raptor(chunks, kb_parser_config["raptor"]["random_seed"], callback, row["id"])
         doc = {
-            "doc_id": fake_doc_id,
+            "doc_id": did,
             "kb_id": [str(row["kb_id"])],
             "docnm_kwd": row["name"],
             "title_tks": rag_tokenizer.tokenize(row["name"]),
@@ -687,7 +687,7 @@ async def run_raptor_for_kb(row, kb_parser_config, chat_mdl, embd_mdl, vector_si
                                                  fields=["content_with_weight", vctr_nm],
                                                  sort_by_position=True):
                 chunks.append((d["content_with_weight"], np.array(d[vctr_nm])))
-            await generate(chunks)
+            await generate(chunks, doc_id)
             callback(prog=(x+1.)/len(doc_ids))
     else:
         chunks = []
@@ -697,7 +697,7 @@ async def run_raptor_for_kb(row, kb_parser_config, chat_mdl, embd_mdl, vector_si
                                                  sort_by_position=True):
                 chunks.append((d["content_with_weight"], np.array(d[vctr_nm])))
 
-        await generate(chunks)
+        await generate(chunks, fake_doc_id)
 
     return res, tk_count
 
