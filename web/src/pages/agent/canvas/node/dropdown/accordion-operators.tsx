db4fd19c8269a64f8d213a64c37f41e5325f22cd
buua436 <66937541+buua436@users.noreply.github.com>
2025-11-14 16:33:20 +0800

    Feat:new component list operations (#11276)
    ### What problem does this PR solve?
    issue:
    https://github.com/infiniflow/ragflow/issues/10427
    change:
    new component list operations
    
    ### Type of change
    
    - [x] New Feature (non-breaking change which adds functionality)

--- web/src/pages/agent/canvas/node/dropdown/accordion-operators.tsx
+++ web/src/pages/agent/canvas/node/dropdown/accordion-operators.tsx
@@ -79,6 +79,7 @@ export function AccordionOperators({
               Operator.Code,
               Operator.StringTransform,
               Operator.DataOperations,
+              Operator.ListOperations,
               // Operator.VariableAssigner,
               Operator.VariableAggregator,
             ]}
