5a2bf7cd11c249b9a5306d77dead3fb3d0c1e0d6
Billy Bao <newyorkupperbay@gmail.com>
2025-11-14 19:52:11 +0800

    Feat: extract message output to file (#11251)
    ### What problem does this PR solve?
    
    Feat: extract message output to file
    
    ### Type of change
    
    - [x] New Feature (non-breaking change which adds functionality)

--- api/apps/document_app.py
+++ api/apps/document_app.py
@@ -508,11 +508,28 @@ def get(doc_id):
         ext = ext.group(1) if ext else None
         if ext:
             if doc.type == FileType.VISUAL.value:
+
                 content_type = CONTENT_TYPE_MAP.get(ext, f"image/{ext}")
             else:
                 content_type = CONTENT_TYPE_MAP.get(ext, f"application/{ext}")
             response.headers.set("Content-Type", content_type)
         return response
+    except Exception as e:
+        return server_error_response(e)
+
+
+@manager.route("/download/<attachment_id>", methods=["GET"])  # noqa: F821
+@login_required
+def download_attachment(attachment_id):
+    try:
+        ext = request.args.get("ext", "markdown")
+        data = settings.STORAGE_IMPL.get(current_user.id, attachment_id)
+        # data = settings.STORAGE_IMPL.get("eb500d50bb0411f0907561d2782adda5", attachment_id)
+        response = flask.make_response(data)
+        response.headers.set("Content-Type", CONTENT_TYPE_MAP.get(ext, f"application/{ext}"))
+
+        return response
+
     except Exception as e:
         return server_error_response(e)
 
