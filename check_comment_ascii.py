0bd32901a58da1da4f2ff074e55095bb1b724218
Zhichang Yu <yuzhichang@gmail.com>
2025-11-14 11:45:32 +0800

    Fix check_comment_ascii.py

--- check_comment_ascii.py
+++ check_comment_ascii.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
+
+"""
+Check whether given python files contain non-ASCII comments.
+
+How to check the whole git repo:
+
+```
+$ git ls-files -z -- '*.py' | xargs -0 python3 check_comment_ascii.py
+```
+"""
+
 import sys
 import tokenize
 import ast
