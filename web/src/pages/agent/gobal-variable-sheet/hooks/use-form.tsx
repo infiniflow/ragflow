ef318a884b21040f3b8b85516570761ebf675140
chanx <1243304602@qq.com>
2025-11-18 09:39:02 +0800

    Fix: Fixed an issue where adding session variables multiple times would overwrite them. (#11308)
    ### What problem does this PR solve?
    
    Fix: Fixed an issue where adding session variables multiple times would
    overwrite them.
    ### Type of change
    
    - [x] Bug Fix (non-breaking change which fixes an issue)

--- web/src/pages/agent/gobal-variable-sheet/hooks/use-form.tsx
+++ web/src/pages/agent/gobal-variable-sheet/hooks/use-form.tsx
@@ -15,27 +15,30 @@ export const useHandleForm = () => {
       return value;
     }
   };
-  const handleSubmit = useCallback(async (fieldValue: FieldValues) => {
-    const param = {
-      ...(data.dsl?.variables || {}),
-      [fieldValue.name]: {
-        ...fieldValue,
-        value:
-          fieldValue.type === TypesWithArray.Object ||
-          fieldValue.type === TypesWithArray.ArrayObject
-            ? handleObjectData(fieldValue.value)
-            : fieldValue.value,
-      },
-    } as Record<string, GlobalVariableType>;
+  const handleSubmit = useCallback(
+    async (fieldValue: FieldValues) => {
+      const param = {
+        ...(data.dsl?.variables || {}),
+        [fieldValue.name]: {
+          ...fieldValue,
+          value:
+            fieldValue.type === TypesWithArray.Object ||
+            fieldValue.type === TypesWithArray.ArrayObject
+              ? handleObjectData(fieldValue.value)
+              : fieldValue.value,
+        },
+      } as Record<string, GlobalVariableType>;
 
-    const res = await saveGraph(undefined, {
-      globalVariables: param,
-    });
+      const res = await saveGraph(undefined, {
+        globalVariables: param,
+      });
 
-    if (res.code === 0) {
-      refetch();
-    }
-  }, []);
+      if (res.code === 0) {
+        refetch();
+      }
+    },
+    [data.dsl?.variables, refetch, saveGraph],
+  );
 
   return { handleSubmit, loading };
 };
