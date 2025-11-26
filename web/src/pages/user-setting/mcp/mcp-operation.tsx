376eb15c638a8b1b960113ce940b418cc09b469c
chanx <1243304602@qq.com>
2025-11-26 19:49:21 +0800

    Fix: Refactoring and enhancing the functionality of the delete confirmation dialog component #10703 (#11542)
    ### What problem does this PR solve?
    
    Fix: Refactoring and enhancing the functionality of the delete
    confirmation dialog component
    
    - Refactoring and enhancing the functionality of the delete confirmation
    dialog component
    - Modifying the style of the user center
    
    ### Type of change
    
    - [x] Bug Fix (non-breaking change which fixes an issue)

--- web/src/pages/user-setting/mcp/mcp-operation.tsx
+++ web/src/pages/user-setting/mcp/mcp-operation.tsx
@@ -1,5 +1,9 @@
-import { ConfirmDeleteDialog } from '@/components/confirm-delete-dialog';
+import {
+  ConfirmDeleteDialog,
+  ConfirmDeleteDialogNode,
+} from '@/components/confirm-delete-dialog';
 import { useDeleteMcpServer } from '@/hooks/use-mcp-request';
+import { IMcpServer } from '@/interfaces/database/mcp';
 import { PenLine, Trash2, Upload } from 'lucide-react';
 import { MouseEventHandler, useCallback } from 'react';
 import { useTranslation } from 'react-i18next';
@@ -7,33 +11,41 @@ import { UseEditMcpReturnType } from './use-edit-mcp';
 import { useExportMcp } from './use-export-mcp';
 
 export function McpOperation({
-  mcpId,
+  mcp,
   showEditModal,
-}: { mcpId: string } & Pick<UseEditMcpReturnType, 'showEditModal'>) {
+}: { mcp: IMcpServer } & Pick<UseEditMcpReturnType, 'showEditModal'>) {
   const { t } = useTranslation();
   const { deleteMcpServer } = useDeleteMcpServer();
   const { handleExportMcpJson } = useExportMcp();
 
   const handleDelete: MouseEventHandler<HTMLDivElement> = useCallback(() => {
-    deleteMcpServer([mcpId]);
-  }, [deleteMcpServer, mcpId]);
+    deleteMcpServer([mcp.id]);
+  }, [deleteMcpServer, mcp.id]);
 
   return (
     <div className="hidden gap-1  group-hover:flex text-text-secondary">
       {/* <RAGFlowTooltip tooltip={t('mcp.export')}> */}
       <Upload
         className="size-5 cursor-pointer p-1 rounded-sm hover:text-text-primary hover:bg-bg-card"
-        onClick={handleExportMcpJson([mcpId])}
+        onClick={handleExportMcpJson([mcp.id])}
       />
       {/* </RAGFlowTooltip>
       <RAGFlowTooltip tooltip={t('common.edit')}> */}
       <PenLine
         className="size-5 cursor-pointer p-1 rounded-sm hover:text-text-primary hover:bg-bg-card"
-        onClick={showEditModal(mcpId)}
+        onClick={showEditModal(mcp.id)}
       />
       {/* </RAGFlowTooltip>
       <RAGFlowTooltip tooltip={t('common.delete')}> */}
-      <ConfirmDeleteDialog onOk={handleDelete}>
+      <ConfirmDeleteDialog
+        onOk={handleDelete}
+        title={t('common.delete') + ' ' + t('mcp.mcpServer')}
+        content={{
+          node: (
+            <ConfirmDeleteDialogNode name={mcp.name}></ConfirmDeleteDialogNode>
+          ),
+        }}
+      >
         <Trash2 className="size-5 cursor-pointer p-1 rounded-sm hover:text-state-error hover:bg-state-error-5" />
       </ConfirmDeleteDialog>
       {/* </RAGFlowTooltip> */}
