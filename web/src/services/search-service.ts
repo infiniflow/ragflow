c9484528a424ea64a042a6b7a62efe97a7d0e8a4
chanx <1243304602@qq.com>
2025-12-08 10:36:43 +0800

    Fix: Fixed the issue where the detail request failed in the search function.

--- web/src/services/search-service.ts
+++ web/src/services/search-service.ts
@@ -1,5 +1,6 @@
 import api from '@/utils/api';
-import { registerNextServer } from '@/utils/register-server';
+import registerServer from '@/utils/register-server';
+import request from '@/utils/request';
 
 const {
   createSearch,
@@ -48,6 +49,6 @@ const methods = {
     method: 'get',
   },
 } as const;
-const searchService = registerNextServer<keyof typeof methods>(methods);
+const searchService = registerServer<keyof typeof methods>(methods, request);
 
 export default searchService;
