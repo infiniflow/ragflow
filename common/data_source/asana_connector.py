5aa45468a92ef9edfe4a54f2ae504b89313062fd
buua436 <sz_buua@foxmail.com>
2025-12-26 16:26:15 +0800

    update

--- common/data_source/asana_connector.py
+++ common/data_source/asana_connector.py
@@ -7,7 +7,7 @@ import asana
 import requests
 from common.data_source.config import CONTINUE_ON_CONNECTOR_FAILURE, INDEX_BATCH_SIZE, DocumentSource
 from common.data_source.interfaces import LoadConnector, PollConnector
-from common.data_source.models import Document, GenerateDocumentsOutput, SecondsSinceUnixEpoch, TextSection
+from common.data_source.models import Document, GenerateDocumentsOutput, SecondsSinceUnixEpoch
 from common.data_source.utils import extract_size_bytes, get_file_ext
 
 
