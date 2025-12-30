dc1d640e808ad44b21591a8a6bdfdb6f30e2cb37
Billy Bao <newyorkupperbay@gmail.com>
2025-12-30 17:53:20 +0800

    initial commit

--- common/data_source/config.py
+++ common/data_source/config.py
@@ -13,6 +13,9 @@ def get_current_tz_offset() -> int:
     return round(time_diff.total_seconds() / 3600)
 
 
+# Default request timeout, mostly used by connectors
+REQUEST_TIMEOUT_SECONDS = int(os.environ.get("REQUEST_TIMEOUT_SECONDS") or 60)
+
 ONE_MINUTE = 60
 ONE_HOUR = 3600
 ONE_DAY = ONE_HOUR * 24
@@ -58,7 +61,7 @@ class DocumentSource(str, Enum):
     GITHUB = "github"
     GITLAB = "gitlab"
     IMAP = "imap"
-
+    BITBUCKET = "bitbucket"
     
 class FileOrigin(str, Enum):
     """File origins"""
