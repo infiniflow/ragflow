6b54b1342a9fb406ae51e34aabac00e943ae7244
chanx <1243304602@qq.com>
2026-01-05 15:28:57 +0800

    Fix: Some bugs (#12441)
    ### What problem does this PR solve?
    
    Fix: Some bugs
    - In a production environment, a second-level page refresh results in a
    white screen.
    - The knowledge graph cannot be opened.
    
    ### Type of change
    
    - [x] Bug Fix (non-breaking change which fixes an issue)

--- web/src/components/ui/breadcrumb.tsx
+++ web/src/components/ui/breadcrumb.tsx
@@ -69,7 +69,7 @@ const BreadcrumbPage = React.forwardRef<
     role="link"
     aria-disabled="true"
     aria-current="page"
-    className={cn('font-normal text-foreground', className)}
+    className={cn('font-normal text-foreground truncate max-w-40', className)}
     {...props}
   />
 ));
