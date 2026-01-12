10be40fce42904aa47daff43cc8de96bdbc856d2
chanx <1243304602@qq.com>
2026-01-12 18:05:54 +0800

    Feat: Enhanced metadata functionality - Metadata filtering supports searching. - Values ​​can be directly modified.

--- web/src/pages/dataset/dataset/index.tsx
+++ web/src/pages/dataset/dataset/index.tsx
@@ -127,6 +127,7 @@ export default function Dataset() {
                   type: MetadataType.Manage,
                   isCanAdd: false,
                   isEditField: true,
+                  isDeleteSingleValue: true,
                   title: (
                     <div className="flex flex-col gap-2">
                       <div className="text-base font-normal">
