9dcb676241bfa65564373bfba12d0ac4d788f76c
Jin Hai <haijin.chn@gmail.com>
2026-01-27 18:31:32 +0800

    Add user_tenant data model
    Signed-off-by: Jin Hai <haijin.chn@gmail.com>

--- internal/model/tenant.go
+++ internal/model/tenant.go
@@ -21,19 +21,3 @@ type Tenant struct {
 func (Tenant) TableName() string {
 	return "tenant"
 }
-
-// UserTenant user tenant relationship model
-type UserTenant struct {
-	ID        string  `gorm:"column:id;primaryKey;size:32" json:"id"`
-	UserID    string  `gorm:"column:user_id;size:32;not null;index" json:"user_id"`
-	TenantID  string  `gorm:"column:tenant_id;size:32;not null;index" json:"tenant_id"`
-	Role      string  `gorm:"column:role;size:32;not null;index" json:"role"`
-	InvitedBy string  `gorm:"column:invited_by;size:32;not null;index" json:"invited_by"`
-	Status    *string `gorm:"column:status;size:1;index" json:"status,omitempty"`
-	BaseModel
-}
-
-// TableName specify table name
-func (UserTenant) TableName() string {
-	return "user_tenant"
-}
