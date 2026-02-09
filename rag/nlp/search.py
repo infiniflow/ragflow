480ec494abeaacc80f7bf80f40cf1d2a4957352a
qinling0210 <88864212+qinling0210@users.noreply.github.com>
2026-02-09 17:56:59 +0800

    Fix parameter of calling self.dataStore.get() and warning info during parser (#13068)
    ### What problem does this PR solve?
    
    Fix parameter of calling self.dataStore.get() and warning info during
    parser
    
    https://github.com/infiniflow/ragflow/issues/13036
    
    ### Type of change
    
    - [x] Bug Fix (non-breaking change which fixes an issue)

--- rag/nlp/search.py
+++ rag/nlp/search.py
@@ -627,7 +627,7 @@ class Dealer:
             if cid in id2idx:
                 chunks[id2idx[cid]]["similarity"] += sim
                 continue
-            chunk = self.dataStore.get(cid, idx_nms, kb_ids)
+            chunk = self.dataStore.get(cid, idx_nms[0], kb_ids)
             if not chunk:
                 continue
             d = {
@@ -677,7 +677,7 @@ class Dealer:
 
         vector_size = 1024
         for id, cks in mom_chunks.items():
-            chunk = self.dataStore.get(id, idx_nms, [ck["kb_id"] for ck in cks])
+            chunk = self.dataStore.get(id, idx_nms[0], [ck["kb_id"] for ck in cks])
             d = {
                 "chunk_id": id,
                 "content_ltks": " ".join([ck["content_ltks"] for ck in cks]),
