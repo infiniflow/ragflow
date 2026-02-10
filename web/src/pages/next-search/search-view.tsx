9fd2346061f253a1bc283899b50fe1e7abca3c76
chanx <1243304602@qq.com>
2026-02-10 13:25:12 +0800

    Fix: Bugs fixed - metadata icon - search page's image

--- web/src/pages/next-search/search-view.tsx
+++ web/src/pages/next-search/search-view.tsx
@@ -201,9 +201,11 @@ export default function SearchingView({
                       <div key={index}>
                         <div className="w-full flex flex-col">
                           <div className="w-full highlightContent">
-                            <ImageWithPopover
-                              id={chunk.image_id || chunk.img_id}
-                            ></ImageWithPopover>
+                            {(chunk.image_id || chunk.img_id) && (
+                              <ImageWithPopover
+                                id={chunk.image_id || chunk.img_id}
+                              ></ImageWithPopover>
+                            )}
                             <Popover>
                               <PopoverTrigger asChild>
                                 <div
@@ -239,7 +241,7 @@ export default function SearchingView({
                           </div>
                         </div>
                         {index < chunks.length - 1 && (
-                          <div className="w-full border-b border-border-default/80 mt-6"></div>
+                          <div className="w-full border-b border-border-default/80 mt-6 mb-2"></div>
                         )}
                       </div>
                     );
