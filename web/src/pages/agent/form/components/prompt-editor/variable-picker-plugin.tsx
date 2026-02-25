013fc252809a268daf8a7dc244f151175d3b368f
Jimmy Ben Klieve <j.b.klieve@live.cn>
2026-02-25 17:22:48 +0800

    refactor: ux improvements for variable picker in prompt editor (#13213)
    ### What problem does this PR solve?
    
    User experience enhancement for variable picker in prompt editor:
    
    - Add case-insensitive string search for variables.
    - Add basic keyboard navigation in variable picker:
       - Hit <kbd>UpArrow</kbd> and <kbd>DownArrow</kbd> for navigating.
    - Hit <kbd>Tab</kbd> or <kbd>Enter</kbd> for selecting focused item into
    editor.
    - Fix unexpectedly inserting invalid variable into editor by hitting
    <kbd>Tab</kbd>.
    
    _Note: you still need to pick variables inside secondary menu (agent
    structured output, etc.) by using your pointing device. May finish these
    later._
    
    ### Type of change
    
    - [x] Refactoring

--- web/src/pages/agent/form/components/prompt-editor/variable-picker-plugin.tsx
+++ web/src/pages/agent/form/components/prompt-editor/variable-picker-plugin.tsx
@@ -18,36 +18,50 @@ import {
   $getRoot,
   $getSelection,
   $isRangeSelection,
+  COMMAND_PRIORITY_CRITICAL,
   TextNode,
 } from 'lexical';
 import React, {
+  createContext,
+  ForwardedRef,
+  forwardRef,
+  HTMLAttributes,
   ReactElement,
   ReactNode,
   useCallback,
+  useContext,
   useEffect,
+  useMemo,
   useRef,
 } from 'react';
 import * as ReactDOM from 'react-dom';
 
 import { $createVariableNode } from './variable-node';
 
+import { ScrollArea } from '@/components/ui/scroll-area';
+import { cn } from '@/lib/utils';
 import { JsonSchemaDataType, VariableRegex } from '@/pages/agent/constant';
 import {
   useFindAgentStructuredOutputLabel,
+  useGetStructuredOutputByValue,
   useShowSecondaryMenu,
 } from '@/pages/agent/hooks/use-build-structured-output';
 import { useFilterQueryVariableOptionsByTypes } from '@/pages/agent/hooks/use-get-begin-query';
-import { get } from 'lodash';
+import { LucideChevronRight } from 'lucide-react';
 import { PromptIdentity } from '../../agent-form/use-build-prompt-options';
 import { StructuredOutputSecondaryMenu } from '../structured-output-secondary-menu';
 import { ProgrammaticTag } from './constant';
 import './index.css';
-class VariableInnerOption extends MenuOption {
+
+const SelectedValueContext = createContext<string>('');
+
+class VariableOption extends MenuOption {
   label: string;
   value: string;
   parentLabel: string | JSX.Element;
   icon?: ReactNode;
   type?: string;
+  options?: VariableOption[];
 
   constructor(
     label: string,
@@ -65,15 +79,15 @@ class VariableInnerOption extends MenuOption {
   }
 }
 
-class VariableOption extends MenuOption {
+class VariableOptionGroup extends MenuOption {
   label: ReactElement | string;
   title: string;
-  options: VariableInnerOption[];
+  options: VariableOption[];
 
   constructor(
     label: ReactElement | string,
     title: string,
-    options: VariableInnerOption[],
+    options: VariableOption[],
   ) {
     super(title);
     this.label = label;
@@ -82,65 +96,236 @@ class VariableOption extends MenuOption {
   }
 }
 
-function VariablePickerMenuItem({
-  index,
-  option,
-  selectOptionAndCleanUp,
-  types,
-}: {
-  index: number;
-  option: VariableOption;
-  types?: JsonSchemaDataType[];
-  selectOptionAndCleanUp: (
-    option: VariableOption | VariableInnerOption,
-  ) => void;
-}) {
-  const showSecondaryMenu = useShowSecondaryMenu();
+const VariablePickerOption = forwardRef(function VariablePickerOption(
+  {
+    option,
+    label,
+    hasSubMenu = false,
+    className,
+    onClick,
+    ...props
+  }: {
+    option: VariableOption;
+    label?: string;
+    hasSubMenu?: boolean;
+    className?: string;
+    onClick?: () => void;
+  } & HTMLAttributes<HTMLLIElement>,
+  ref: ForwardedRef<HTMLElement>,
+) {
+  const selectedValue = useContext(SelectedValueContext);
+  const isSelected = option.value === selectedValue;
 
   return (
     <li
+      {...props}
+      ref={ref as ForwardedRef<HTMLLIElement>}
       key={option.key}
-      tabIndex={-1}
-      ref={option.setRefElement}
+      onClick={onClick}
+      className={cn(
+        'px-2 py-1 text-text-primary rounded-sm flex justify-between items-center',
+        'hover:bg-bg-card focus-visible:bg-bg-card',
+        isSelected && 'bg-bg-card',
+        className,
+      )}
       role="option"
-      id={'typeahead-item-' + index}
+      aria-label={option.label}
+      aria-selected={isSelected}
     >
-      <div>
-        <span className="text text-text-secondary">{option.title}</span>
-        <ul className="pl-2 py-1">
-          {option.options.map((x) => {
-            const shouldShowSecondary = showSecondaryMenu(x.value, x.label);
-
-            if (shouldShowSecondary) {
-              return (
-                <StructuredOutputSecondaryMenu
-                  key={x.value}
-                  data={x}
-                  types={types}
-                  click={(y) =>
-                    selectOptionAndCleanUp({
-                      ...x,
-                      ...y,
-                    } as VariableInnerOption)
-                  }
-                ></StructuredOutputSecondaryMenu>
-              );
-            }
-
-            return (
-              <li
-                key={x.value}
-                onClick={() => selectOptionAndCleanUp(x)}
-                className="hover:bg-bg-card p-1 text-text-primary rounded-sm flex justify-between items-center"
-              >
-                <span className="truncate flex-1 min-w-0">{x.label}</span>
-                <span className="text-text-secondary">{get(x, 'type')}</span>
-              </li>
-            );
-          })}
-        </ul>
-      </div>
+      <span className="truncate flex-1 min-w-0">{label ?? option.label}</span>
+      <span className="text-text-secondary text-xs">{option.type}</span>
+      {hasSubMenu ? (
+        <LucideChevronRight className="ml-2 size-[1em] text-text-secondary" />
+      ) : null}
     </li>
+  );
+});
+
+// TODO: Stage 2
+/*
+function VariableStructuredOptions({
+  option,
+  children,
+  selectOptionAndCleanUp,
+}: {
+  option: VariableOption;
+  children?: ReactNode;
+  selectOptionAndCleanUp: (option: VariableOption) => void;
+}) {
+  const selectedValue = useContext(SelectedValueContext);
+
+  const hasSelectedDescendant = useMemo(() => {
+    const _hasSelectedDescendant = (options: VariableOption[]): boolean => {
+      let result = false;
+
+      for (const x of options) {
+        if (x.value === selectedValue) {
+          return true;
+        }
+
+        if (x.options?.length) {
+          result = result || _hasSelectedDescendant(x.options);
+        }
+      }
+
+      return result;
+    };
+
+    return _hasSelectedDescendant(option?.options ?? []);
+  }, [option?.options, selectedValue]);
+
+  const renderStructuredOptions = useCallback((options?: VariableOption[], level = 0) => {
+    if (!options?.length) {
+      return null;
+    }
+
+    return (
+      <ul
+        className={cn(
+          level > 0 && 'border-l !ml-2',
+        )}
+      >
+        {options.map((o) => {
+          if (o.options?.length) {
+            return (
+              <div key={o.key}>
+                <VariablePickerOption
+                  key={o.key}
+                  ref={o.setRefElement}
+                  option={o}
+                  label={o.label.split('.').pop()}
+                  className={o.value === selectedValue ? 'bg-bg-card' : ''}
+                  onClick={() => selectOptionAndCleanUp(o)}
+                />
+
+                {renderStructuredOptions(o.options, level + 1)}
+              </div>
+            );
+          }
+
+          return (
+            <VariablePickerOption
+              key={o.key}
+              ref={o.setRefElement}
+              option={o}
+              label={o.label.split('.').pop()}
+              onClick={() => selectOptionAndCleanUp(o)}
+            />
+          );
+        })}
+      </ul>
+    );
+  }, []);
+
+  return (
+    <HoverCard
+      open={hasSelectedDescendant || undefined}
+      openDelay={100}
+      closeDelay={100}
+    >
+      <HoverCardTrigger asChild>
+        <VariablePickerOption
+          key={option?.key}
+          ref={option?.setRefElement}
+          option={option}
+          onClick={() => selectOptionAndCleanUp(option)}
+          hasSubMenu
+        />
+      </HoverCardTrigger>
+
+      <HoverCardContent
+        side="left"
+        align="start"
+        className="bg-bg-base min-w-72 border-0.5 border-border rounded-md shadow-lg p-0"
+      >
+        <ScrollArea className="p-2">
+          <section>
+            <header className="mb-2 text-text-secondary text-xs">
+              {t('flow.structuredOutput.structuredOutput')}
+            </header>
+
+            {renderStructuredOptions(option?.options)}
+          </section>
+        </ScrollArea>
+      </HoverCardContent>
+    </HoverCard>
+  );
+}
+*/
+
+function VariablePickerOptionGroup({
+  title,
+  options = [],
+  selectOptionAndCleanUp,
+  types,
+}: {
+  title?: string;
+  options?: VariableOption[];
+  types?: JsonSchemaDataType[];
+  selectOptionAndCleanUp: (option: VariableOption) => void;
+}) {
+  const showSecondaryMenu = useShowSecondaryMenu();
+  const selectedValue = useContext(SelectedValueContext);
+
+  return (
+    <ul role="group" aria-label={title}>
+      <li key={title} role="presentation">
+        <div className="text-xs text-text-secondary cursor-auto py-1">
+          {title}
+        </div>
+      </li>
+
+      {options.map((x) => {
+        const shouldShowSecondary = showSecondaryMenu(x.value, x.label);
+        const isSelected = x.value === selectedValue;
+
+        if (shouldShowSecondary) {
+          // TODO: Stage 2
+          /*
+          if (
+            !isEmpty(types)
+            && !hasSpecificTypeChild(x ?? {}, types)
+            && !types?.some((x) => x === JsonSchemaDataType.Object)
+          ) {
+            return null;
+          }
+
+          return (
+            <VariableStructuredOptions
+              key={`subMenu:${x.key}`}
+              option={x}
+              selectOptionAndCleanUp={selectOptionAndCleanUp}
+            />
+          );
+          */
+
+          return (
+            <StructuredOutputSecondaryMenu
+              ref={x.setRefElement}
+              key={x.value}
+              data={x}
+              types={types}
+              className={isSelected ? 'bg-bg-card' : ''}
+              click={(y) =>
+                selectOptionAndCleanUp({
+                  ...x,
+                  ...y,
+                } as VariableOption)
+              }
+            />
+          );
+        }
+
+        return (
+          <VariablePickerOption
+            key={x.key}
+            ref={x.setRefElement}
+            option={x}
+            onClick={() => selectOptionAndCleanUp(x)}
+          />
+        );
+      })}
+    </ul>
   );
 }
 
@@ -162,6 +347,7 @@ export type VariablePickerMenuPluginProps = {
   baseOptions?: VariablePickerMenuOptionType[];
   types?: JsonSchemaDataType[];
 };
+
 export default function VariablePickerMenuPlugin({
   value,
   extraOptions,
@@ -169,29 +355,29 @@ export default function VariablePickerMenuPlugin({
   types,
 }: VariablePickerMenuPluginProps): JSX.Element {
   const [editor] = useLexicalComposerContext();
-
+  // const shouldShowSecondaryMenu = useShowSecondaryMenu();
   const findAgentStructuredOutputLabel = useFindAgentStructuredOutputLabel();
-
-  // const checkForTriggerMatch = useBasicTypeaheadTriggerMatch('/', {
-  //   minLength: 0,
-  // });
+  const filterStructuredOutput = useGetStructuredOutputByValue();
 
   const testTriggerFn = React.useCallback((text: string) => {
-    const lastChar = text.slice(-1);
-    if (lastChar === '/') {
-      console.log('Found trigger character "/"');
+    const triggerRegex = /(^|\s|\()([/]((?:[^/\s\()])*))$/;
+    const match = triggerRegex.exec(text);
+
+    if (match !== null) {
+      const mayLeadingWhitespace = match[1];
+
       return {
-        leadOffset: text.length - 1,
-        matchingString: '',
-        replaceableString: '/',
+        leadOffset: match.index + mayLeadingWhitespace.length,
+        matchingString: match[3], // This will send to onQueryChange() event handler
+        replaceableString: match[2],
       };
     }
+
     return null;
   }, []);
 
   const previousValue = useRef<string | undefined>();
-
-  const [queryString, setQueryString] = React.useState<string | null>('');
+  const [queryString, setQueryString] = React.useState<string>('');
 
   let options = useFilterQueryVariableOptionsByTypes({ types });
 
@@ -199,40 +385,128 @@ export default function VariablePickerMenuPlugin({
     options = baseOptions as typeof options;
   }
 
-  const buildNextOptions = useCallback(() => {
-    let filteredOptions = [...options, ...(extraOptions ?? [])];
-    if (queryString) {
-      const lowerQuery = queryString.toLowerCase();
-      filteredOptions = options
-        .map((x) => ({
-          ...x,
-          options: x.options.filter(
-            (y) =>
-              y.label.toLowerCase().includes(lowerQuery) ||
-              y.value.toLowerCase().includes(lowerQuery),
-          ),
-        }))
-        .filter((x) => x.options.length > 0);
-    }
-
-    const finalOptions: VariableOption[] = filteredOptions.map(
-      (x) =>
-        new VariableOption(
-          x.label,
-          x.title,
-          x.options.map((y) => {
-            return new VariableInnerOption(
-              y.label,
-              y.value,
-              x.label,
-              y.icon,
-              y.type,
-            );
-          }),
+  const unifiedOptions = useMemo(() => {
+    const allGroups = Array.from(
+      [...options, ...(extraOptions ?? [])],
+      (g) =>
+        new VariableOptionGroup(
+          g.label,
+          g.title,
+          g.options as VariableOption[],
         ),
     );
-    return finalOptions;
-  }, [extraOptions, options, queryString]);
+
+    // TODO: Stage 2
+    /*
+    const _treeify = (values: any, option: VariableOption): void | VariableOption[] => {
+      if (values == null) {
+        return;
+      }
+
+      const properties = get(values, 'properties') || get(values, 'items.properties');
+
+      if (isPlainObject(values) && properties) {
+        option.options = Object.entries(properties).map(([key, value]) => {
+          const nextOption = new VariableOption(
+            `${option.label}.${key}`,
+            `${option.value}.${key}`,
+            option.label,
+          );
+
+          const {
+            dataType,
+            compositeDataType,
+          } = getStructuredDatatype(value);
+
+          if (
+            isEmpty(types)
+            || types?.some((x) => x === compositeDataType)
+            || hasSpecificTypeChild(value ?? {}, types)
+          ) {
+
+            nextOption.type = compositeDataType;
+
+            if ([JsonSchemaDataType.Object, JsonSchemaDataType.Array].some(x => x === dataType)) {
+              _treeify(value, nextOption)!;
+            }
+          }
+
+          return nextOption;
+        });
+      }
+    };
+    */
+
+    const treeified = allGroups.map((group) => {
+      group.options = group.options.map((option) => {
+        const newOption = new VariableOption(
+          option.label,
+          option.value,
+          option.parentLabel,
+          option.icon,
+          option.type,
+        );
+
+        // TODO: Stage 2
+        /*
+        if (shouldShowSecondaryMenu(newOption.value, newOption.label)) {
+          const structuredOutput = _treeify(filterStructuredOutput(newOption.value), newOption);
+
+          if (structuredOutput) {
+            newOption.options = structuredOutput;
+          }
+        }
+        */
+
+        return newOption;
+      });
+
+      return group;
+    });
+
+    const filtered = treeified
+      .map((g) => ({
+        ...g,
+        options: g.options.filter((y) => {
+          if (!queryString) {
+            return true;
+          }
+
+          // TODO: Stage 2
+          // Stage 1: Allow filtering by component label, such as: "agent_0.content", "retrieval_0.json", etc.
+          const parentLabel =
+            typeof y.parentLabel === 'string'
+              ? `${y.parentLabel.toLowerCase()}.`
+              : '';
+          const thisLabel = `${parentLabel}${y.label.toLowerCase()}`;
+          const thisValue = `${parentLabel}${y.value.toLowerCase()}`;
+
+          return (
+            thisLabel.includes(queryString) || thisValue.includes(queryString)
+          );
+        }),
+      }))
+      .filter((x) => x.options.length);
+
+    const _flat = (
+      option: VariableOption,
+    ): VariableOption | VariableOption[] => {
+      if (option.options) {
+        return [option, ...option.options.flatMap((x) => _flat(x))];
+      }
+
+      return option;
+    };
+
+    const flattened: VariableOption[] = filtered
+      .flatMap((x) => x?.options ?? [])
+      .flatMap((x) => _flat(x));
+
+    return {
+      treeified: filtered,
+      flattened,
+    };
+  }, [queryString, options, extraOptions, filterStructuredOutput]);
 
   const findItemByValue = useCallback(
     (value: string) => {
@@ -263,7 +537,7 @@ export default function VariablePickerMenuPlugin({
 
   const onSelectOption = useCallback(
     (
-      selectedOption: VariableInnerOption,
+      selectedOption: VariableOption,
       nodeToRemove: TextNode | null,
       closeMenu: () => void,
     ) => {
@@ -278,7 +552,7 @@ export default function VariablePickerMenuPlugin({
           nodeToRemove.remove();
         }
         const variableNode = $createVariableNode(
-          (selectedOption as VariableInnerOption).value,
+          (selectedOption as VariableOption).value,
           selectedOption.label as string,
           selectedOption.parentLabel as string | ReactNode,
           selectedOption.icon as ReactNode,
@@ -390,37 +664,53 @@ export default function VariablePickerMenuPlugin({
   }, [editor]);
 
   return (
-    <LexicalTypeaheadMenuPlugin<VariableOption | VariableInnerOption>
-      onQueryChange={setQueryString}
+    <LexicalTypeaheadMenuPlugin
+      // Lift the priority to the highest level to prevent the default Enter key behavior from being triggered
+      commandPriority={COMMAND_PRIORITY_CRITICAL}
+      onQueryChange={(s) => setQueryString(s?.toLowerCase() ?? '')}
       onSelectOption={(option, textNodeContainingQuery, closeMenu) =>
         onSelectOption(
-          option as VariableInnerOption, // Only the second level menu can be selected
+          option as VariableOption, // Only the second level menu can be selected
           textNodeContainingQuery,
           closeMenu,
         )
       }
       triggerFn={testTriggerFn}
-      options={buildNextOptions()}
-      menuRenderFn={(anchorElementRef, { selectOptionAndCleanUp }) => {
-        const nextOptions = buildNextOptions();
-        return anchorElementRef.current && nextOptions.length
-          ? ReactDOM.createPortal(
-              <div className="typeahead-popover w-80 p-2 bg-bg-base">
-                <ul className="scroll-auto overflow-x-hidden">
-                  {nextOptions.map((option, i: number) => (
-                    <VariablePickerMenuItem
-                      index={i}
-                      key={option.key}
-                      option={option}
+      options={unifiedOptions.flattened}
+      menuRenderFn={(
+        anchorElementRef,
+        { selectOptionAndCleanUp, options, selectedIndex },
+      ) => {
+        if (!anchorElementRef.current || !unifiedOptions.flattened.length) {
+          return null;
+        }
+
+        return ReactDOM.createPortal(
+          <SelectedValueContext.Provider
+            value={
+              (selectedIndex !== null &&
+                (options[selectedIndex] as VariableOption)?.value) ||
+              ''
+            }
+          >
+            <div className="typeahead-popover w-80 bg-bg-base border-0.5 border-border">
+              <ScrollArea className="p-2">
+                <div className="max-h-64 space-y-2">
+                  {unifiedOptions.treeified.map((group) => (
+                    <VariablePickerOptionGroup
+                      key={group.title}
+                      title={group.title}
+                      options={group.options}
                       types={types}
                       selectOptionAndCleanUp={selectOptionAndCleanUp}
                     />
                   ))}
-                </ul>
-              </div>,
-              anchorElementRef.current,
-            )
-          : null;
+                </div>
+              </ScrollArea>
+            </div>
+          </SelectedValueContext.Provider>,
+          anchorElementRef.current,
+        );
       }}
     />
   );
