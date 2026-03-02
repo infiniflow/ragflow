b557e6ffbd1e94a8c90193ebe5f7ec3d81cbbb8d
Alexander Strasser <alexander.strasser@ondewo.com>
2026-03-02 12:41:44 +0100

    remove unused imports

--- test/testcases/test_http_api/test_dataset_management/test_delete_datasets.py
+++ test/testcases/test_http_api/test_dataset_management/test_delete_datasets.py
@@ -13,10 +13,8 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 #
-import sys
 import uuid
 from concurrent.futures import ThreadPoolExecutor, as_completed
-from pathlib import Path
 
 import pytest
 from common import (
