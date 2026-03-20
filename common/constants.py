9d083268f613a7d744f74e176751d7b589cc33cb
kejun <kejun1997@gmail.com>
2026-03-20 13:58:38 +0800

    feat: support rss datasource

--- common/constants.py
+++ common/constants.py
@@ -111,6 +111,7 @@ class ParserType(StrEnum):
 class FileSource(StrEnum):
     LOCAL = ""
     KNOWLEDGEBASE = "knowledgebase"
+    RSS = "rss"
     S3 = "s3"
     NOTION = "notion"
     DISCORD = "discord"
