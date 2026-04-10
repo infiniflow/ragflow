1d81c4c4de5de4e285827711892832619dc7978b
Jin Hai <haijin.chn@gmail.com>
2026-04-10 19:25:13 +0800

    Remove unused API
    Signed-off-by: Jin Hai <haijin.chn@gmail.com>

--- api/apps/api_app.py
+++ api/apps/api_app.py
@@ -22,66 +22,6 @@ from api.utils.api_utils import generate_confirmation_token, get_data_error_resu
 from common.time_utils import current_timestamp, datetime_format
 from api.apps import login_required, current_user
 
-
-@manager.route('/new_token', methods=['POST'])  # noqa: F821
-@login_required
-async def new_token():
-    req = await get_request_json()
-    try:
-        tenants = UserTenantService.query(user_id=current_user.id)
-        if not tenants:
-            return get_data_error_result(message="Tenant not found!")
-
-        tenant_id = tenants[0].tenant_id
-        obj = {"tenant_id": tenant_id, "token": generate_confirmation_token(),
-               "create_time": current_timestamp(),
-               "create_date": datetime_format(datetime.now()),
-               "update_time": None,
-               "update_date": None
-               }
-        if req.get("canvas_id"):
-            obj["dialog_id"] = req["canvas_id"]
-            obj["source"] = "agent"
-        else:
-            obj["dialog_id"] = req["dialog_id"]
-
-        if not APITokenService.save(**obj):
-            return get_data_error_result(message="Fail to new a dialog!")
-
-        return get_json_result(data=obj)
-    except Exception as e:
-        return server_error_response(e)
-
-
-@manager.route('/token_list', methods=['GET'])  # noqa: F821
-@login_required
-def token_list():
-    try:
-        tenants = UserTenantService.query(user_id=current_user.id)
-        if not tenants:
-            return get_data_error_result(message="Tenant not found!")
-
-        id = request.args["dialog_id"] if "dialog_id" in request.args else request.args["canvas_id"]
-        objs = APITokenService.query(tenant_id=tenants[0].tenant_id, dialog_id=id)
-        return get_json_result(data=[o.to_dict() for o in objs])
-    except Exception as e:
-        return server_error_response(e)
-
-
-@manager.route('/rm', methods=['POST'])  # noqa: F821
-@validate_request("tokens", "tenant_id")
-@login_required
-async def rm():
-    req = await get_request_json()
-    try:
-        for token in req["tokens"]:
-            APITokenService.filter_delete(
-                [APIToken.tenant_id == req["tenant_id"], APIToken.token == token])
-        return get_json_result(data=True)
-    except Exception as e:
-        return server_error_response(e)
-
-
 @manager.route('/stats', methods=['GET'])  # noqa: F821
 @login_required
 def stats():
