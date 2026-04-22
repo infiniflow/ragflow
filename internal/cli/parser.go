e2f93ce5bdc2274177695e58c837d23a374c925a
Jin Hai <haijin.chn@gmail.com>
2026-04-22 18:19:03 +0800

    Go: add new provider minimax
    Signed-off-by: Jin Hai <haijin.chn@gmail.com>

--- internal/cli/parser.go
+++ internal/cli/parser.go
@@ -196,6 +196,8 @@ func (p *Parser) parseUserCommand() (*Command, error) {
 		return p.parseChatCommand()
 	case TokenThink:
 		return p.parseThinkCommand()
+	case TokenCheck:
+		return p.parseCheckCommand()
 	case TokenLS:
 		return p.parseContextListCommand()
 	case TokenCat:
