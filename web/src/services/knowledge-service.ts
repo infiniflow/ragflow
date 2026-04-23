2aeb041ebe584972d2de769266a15edec8b7ce01
xugangqiang <xugangqiang@hotmail.com>
2026-04-23 10:26:18 +0800

    fix

--- web/src/services/knowledge-service.ts
+++ web/src/services/knowledge-service.ts
@@ -301,19 +301,6 @@ export const updateDocumentMetaDataConfig = ({
     data: { ...data },
   });
 
-export const updateDocumentMetaDataConfig = ({
-  kb_id,
-  doc_id,
-  data,
-}: {
-  kb_id: string;
-  doc_id: string;
-  data: any;
-}) =>
-  request.put(api.documentUpdateMetaDataConfig(kb_id, doc_id), {
-    data: { ...data },
-  });
-
 export const listDataPipelineLogDocument = (
   params?: IFetchKnowledgeListRequestParams,
   body?: IFetchDocumentListRequestBody,
