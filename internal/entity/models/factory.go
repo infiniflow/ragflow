57a0e7742bf0e4d1fd6523f239a2c6a2135fff73
dripsmvcp <138900956+dripsmvcp@users.noreply.github.com>
2026-05-21 12:27:04 +0900

    feat(go): implement provider TokenPony (#15086)

--- internal/entity/models/factory.go
+++ internal/entity/models/factory.go
@@ -93,6 +93,8 @@ func (f *ModelFactory) CreateModelDriver(providerName string, baseURL map[string
 		return NewXinferenceModel(baseURL, urlSuffix), nil
 	case "longcat":
 		return NewLongCatModel(baseURL, urlSuffix), nil
+	case "tokenpony":
+		return NewTokenPonyModel(baseURL, urlSuffix), nil
 	case "novita":
 		return NewNovitaModel(baseURL, urlSuffix), nil
 	case "replicate":
