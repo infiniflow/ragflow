3746335537a3110cca2124427c0a64dc6115ab86
Wang Qi <wangq8@outlook.com>
2026-06-09 17:38:01 +0800

    Fix VLLLM cannot add without /v1

--- rag/llm/chat_model.py
+++ rag/llm/chat_model.py
@@ -916,6 +916,10 @@ class OpenAI_APIChat(Base):
     def __init__(self, key, model_name, base_url, **kwargs):
         if not base_url:
             raise ValueError("url cannot be None")
+        if kwargs.get("provider") == "VLLM":
+            base_url = base_url.rstrip("/")
+            if not base_url.endswith("/v1"):
+                base_url += "/v1"
         model_name = model_name.split("___")[0]
         super().__init__(key, model_name, base_url, **kwargs)
 
