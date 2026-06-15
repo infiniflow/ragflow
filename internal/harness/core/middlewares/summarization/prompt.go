9fb973e9ea518b794298f675759827197ca82d58
Yingfeng Zhang <yingfeng.zhang@gmail.com>
2026-06-15 20:14:19 +0800

    Delete core

--- internal/harness/core/middlewares/summarization/prompt.go
+++
@@ -1,9 +0,0 @@
-// Package summarization provides prompt templates.
-package summarization
-
-// Action types for EmitInternalEvents
-const (
-	ActionTypeBeforeSummarize  = "summarize:before"
-	ActionTypeAfterSummarize   = "summarize:after"
-	ActionTypeGenerateSummary  = "summarize:generate"
-)
