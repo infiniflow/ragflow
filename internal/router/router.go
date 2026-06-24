c7d7ba067d396e78e9996c281efa5867fa7079a6
haruko386 <tryeverypossible@163.com>
2026-06-24 11:50:26 +0800

    Discarded DeleteKnowledgeGraph route

--- internal/router/router.go
+++ internal/router/router.go
@@ -276,7 +276,7 @@ func (r *Router) Setup(engine *gin.Engine) {
 				datasets.POST("/:dataset_id/index", r.datasetsHandler.RunIndex)
 				datasets.DELETE("/:dataset_id/index", r.datasetsHandler.DeleteIndex)
 				datasets.DELETE("/:dataset_id/:index_type", r.datasetsHandler.DeleteIndex)
-				datasets.DELETE("/:dataset_id/graph", r.datasetsHandler.DeleteKnowledgeGraph)
+				//datasets.DELETE("/:dataset_id/graph", r.datasetsHandler.DeleteKnowledgeGraph)
 				datasets.POST("", r.datasetsHandler.CreateDataset)
 				datasets.DELETE("", r.datasetsHandler.DeleteDatasets)
 				datasets.POST("/search", r.datasetsHandler.SearchDatasets)
