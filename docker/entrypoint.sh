127304f5f41106e17c1f63336347c67f710c50a0
Lynn <lynn_inf@hotmail.com>
2026-07-06 13:54:05 +0800

    feat: move to run_migrations.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -255,21 +255,7 @@ ensure_docling
 ensure_db_init
 
 if [[ "${INIT_MODEL_PROVIDER_TABLES}" -eq 1 ]]; then
-    echo "Running model provider table migrations..."
-    "$PY" tools/scripts/mysql_migration.py \
-        --stages tenant_model_provider,tenant_model_instance,tenant_model,model_id_config \
-        --config conf/service_conf.yaml \
-        --execute \
-        --database-version "v0.26.0" \
-        --mark-database-version-on-success
-
-    "$PY" tools/scripts/mysql_migration.py \
-        --stages tenant_model_seeding,model_type_merge,tenant_model_id_migration \
-        --config conf/service_conf.yaml \
-        --execute \
-        --database-version "v0.26.3" \
-        --mark-database-version-on-success
-    echo "Model provider table migrations completed."
+    tools/scripts/run_migrations.sh
 fi
 
 if [[ "${ENABLE_ADMIN_SERVER}" -eq 1 ]]; then
