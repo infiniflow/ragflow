87796b5d8be2bcd6edf9d81f196ba4f61193586e
Jin Hai <haijin.chn@gmail.com>
2026-07-10 16:14:38 +0800

    Go: format code
    Signed-off-by: Jin Hai <haijin.chn@gmail.com>

--- internal/admin/enterprise_service.go
+++ internal/admin/enterprise_service.go
@@ -1157,3 +1157,7 @@ func (s *Service) RemoveIngestionTasksByCondition(tasks []string, email, status 
 
 	return []map[string]interface{}{element}, nil
 }
+
+func CheckLicense() (common.ErrorCode, string) {
+	return common.CodeLicenseValid, ""
+}
