4be1e8eb7c5ff93a4b1e50dfad36c4fc82547247
Jin Hai <haijin.chn@gmail.com>
2026-07-15 15:40:32 +0800

    Fix error
    Signed-off-by: Jin Hai <haijin.chn@gmail.com>

--- internal/entity/tenant_langfuse.go
+++ internal/entity/tenant_langfuse.go
@@ -19,9 +19,9 @@ package entity
 // TenantLangfuse tenant langfuse model
 type TenantLangfuse struct {
 	TenantID  string `gorm:"column:tenant_id;primaryKey;size:32" json:"tenant_id"`
-	SecretKey string `gorm:"column:secret_key;size:2048;not null;index" json:"secret_key"`
-	PublicKey string `gorm:"column:public_key;size:2048;not null;index" json:"public_key"`
-	Host      string `gorm:"column:host;size:128;not null;index" json:"host"`
+	SecretKey string `gorm:"column:secret_key;size:2048;not null" json:"secret_key"`
+	PublicKey string `gorm:"column:public_key;size:2048;not null" json:"public_key"`
+	Host      string `gorm:"column:host;size:128;not null" json:"host"`
 	BaseModel
 }
 
