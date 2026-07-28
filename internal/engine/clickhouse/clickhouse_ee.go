2fbb8b8be96ccc879f9e20d15ed3c2795a25916b
haruko386 <tryeverypossible@163.com>
2026-07-28 17:25:25 +0800

    Go: implement embed, rerank for PPIO provider

--- internal/engine/clickhouse/clickhouse_ee.go
+++ internal/engine/clickhouse/clickhouse_ee.go
@@ -19,6 +19,8 @@ package clickhouse
 import (
 	"ragflow/internal/common"
 	"sync"
+
+	"go.uber.org/zap"
 )
 
 var (
@@ -35,9 +37,9 @@ func GetDriver() *Driver {
 }
 
 func (d *Driver) CollectModelUsage(modelUsage *common.ModelUsage) error {
-	//if modelUsage != nil {
-	//	common.Info("CollectModelUsage", zap.Any("modelUsage", modelUsage.String()))
-	//}
+	if modelUsage != nil {
+		common.Info("CollectModelUsage", zap.Any("modelUsage", modelUsage.String()))
+	}
 	return nil
 }
 
