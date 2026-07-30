522605f1dbeeea7bcbf2fc2d22715e553a68983e
bill <yibie_jingnian@163.com>
2026-07-30 15:17:30 +0800

    Feat: Add a link to create a template to the operator parameter of the compiler operator.

--- web/src/components/ragflow-form.tsx
+++ web/src/components/ragflow-form.tsx
@@ -6,6 +6,7 @@ import {
   FormMessage,
 } from '@/components/ui/form';
 import { cn } from '@/lib/utils';
+import { ArrowUpRight } from 'lucide-react';
 import { ReactNode, cloneElement, isValidElement } from 'react';
 import {
   ControllerRenderProps,
@@ -23,6 +24,10 @@ type RAGFlowFormItemProps = {
   labelClassName?: string;
   valueClassName?: string;
   className?: string;
+  labelLink?: {
+    text: ReactNode;
+    onClick: () => void;
+  };
 } & Pick<UseControllerProps<any>, 'rules'>;
 
 export function RAGFlowFormItem({
@@ -36,6 +41,7 @@ export function RAGFlowFormItem({
   valueClassName,
   className,
   rules,
+  labelLink,
 }: RAGFlowFormItemProps) {
   const form = useFormContext();
   return (
@@ -53,13 +59,29 @@ export function RAGFlowFormItem({
           )}
         >
           {label && (
-            <FormLabel
-              required={required}
-              tooltip={tooltip}
-              className={cn({ 'w-1/4': horizontal }, labelClassName)}
+            <div
+              className={cn('flex items-center gap-2', {
+                'w-full justify-between': !horizontal,
+                'w-1/4': horizontal,
+              })}
             >
-              {label}
-            </FormLabel>
+              <FormLabel
+                required={required}
+                tooltip={tooltip}
+                className={cn(labelClassName)}
+              >
+                {label}
+              </FormLabel>
+              {labelLink && (
+                <div
+                  className="text-sm flex text-text-primary cursor-pointer items-center shrink-0"
+                  onClick={labelLink.onClick}
+                >
+                  {labelLink.text}
+                  <ArrowUpRight size={14} />
+                </div>
+              )}
+            </div>
           )}
           <div
             className={cn(
