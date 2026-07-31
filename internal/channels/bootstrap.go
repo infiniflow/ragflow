6b879b718a4a7ebc07ab4eae284d8cc8aee04b2e
zhanghongze <zhanghongze186@gmail.com>
2026-07-31 10:33:15 +0800

    fix conflict

--- internal/channels/bootstrap.go
+++ internal/channels/bootstrap.go
@@ -279,6 +279,8 @@ func buildChannel(accountID string, wanted desiredChannel) (core.Channel, error)
 	switch wanted.channel {
 	case "discord":
 		return newDiscordChannelFromConfig(accountID, wanted.credential)
+	case "qqbot":
+		return newQQBotChannelFromConfig(accountID, wanted.credential)
 	case "whatsapp":
 		return newWhatsAppChannelFromConfig(accountID, wanted.credential)
 	case "dingtalk":
