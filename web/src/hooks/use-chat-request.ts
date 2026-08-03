62457075d4c4013ce1db706d357eb5fbfe1a7113
balibabu <cike8899@users.noreply.github.com>
2026-08-03 13:45:35 +0800

    Fix: Accidentally deleted the previously selected chat. (#17710)

--- web/src/hooks/use-chat-request.ts
+++ web/src/hooks/use-chat-request.ts
@@ -273,7 +273,8 @@ export const useFetchChat = () => {
 export const useFetchSessionList = () => {
   const { id } = useParams();
 
-  const { searchString, handleInputChange } = useHandleSearchStrChange();
+  const { searchString, handleInputChange, setSearchString } =
+    useHandleSearchStrChange();
 
   const {
     data,
@@ -299,7 +300,14 @@ export const useFetchSessionList = () => {
     },
   });
 
-  return { data, loading, refetch, searchString, handleInputChange };
+  return {
+    data,
+    loading,
+    refetch,
+    searchString,
+    handleInputChange,
+    setSearchString,
+  };
 };
 
 export function useFetchSessionManually() {
