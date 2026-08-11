6cceeb6d34db058fc87893b55a59aa541d334406
fioncat <lazycat7706@gmail.com>
2026-08-11 15:11:56 +0800

    feat: add UCloud agent sandbox provider

--- admin/server/services.py
+++ admin/server/services.py
@@ -500,6 +500,11 @@ class SandboxMgr:
             "description": "Tenki - Disposable microVM code sandboxes",
             "tags": ["saas", "cloud", "microvm", "isolated"],
         },
+        "ucloud_agent_sandbox": {
+            "name": "UCloud Agent Sandbox",
+            "description": "UCloud Agent Sandbox - Disposable cloud sandboxes for agent code execution",
+            "tags": ["saas", "cloud", "isolated", "ucloud"],
+        },
     }
 
     @staticmethod
@@ -520,6 +525,7 @@ class SandboxMgr:
             AliyunCodeInterpreterProvider,
             E2BProvider,
             TenkiProvider,
+            UCloudAgentSandboxProvider,
         )
 
         schemas = {
@@ -529,6 +535,7 @@ class SandboxMgr:
             "aliyun_codeinterpreter": AliyunCodeInterpreterProvider.get_config_schema(),
             "e2b": E2BProvider.get_config_schema(),
             "tenki": TenkiProvider.get_config_schema(),
+            "ucloud_agent_sandbox": UCloudAgentSandboxProvider.get_config_schema(),
         }
 
         if provider_id not in schemas:
@@ -595,6 +602,7 @@ class SandboxMgr:
             AliyunCodeInterpreterProvider,
             E2BProvider,
             TenkiProvider,
+            UCloudAgentSandboxProvider,
         )
 
         try:
@@ -640,6 +648,7 @@ class SandboxMgr:
                 "aliyun_codeinterpreter": AliyunCodeInterpreterProvider,
                 "e2b": E2BProvider,
                 "tenki": TenkiProvider,
+                "ucloud_agent_sandbox": UCloudAgentSandboxProvider,
             }
             provider = provider_classes[provider_type]()
             is_valid, error_msg = provider.validate_config(config)
@@ -688,6 +697,7 @@ class SandboxMgr:
                 AliyunCodeInterpreterProvider,
                 E2BProvider,
                 TenkiProvider,
+                UCloudAgentSandboxProvider,
             )
 
             # Instantiate provider based on type
@@ -698,6 +708,7 @@ class SandboxMgr:
                 "aliyun_codeinterpreter": AliyunCodeInterpreterProvider,
                 "e2b": E2BProvider,
                 "tenki": TenkiProvider,
+                "ucloud_agent_sandbox": UCloudAgentSandboxProvider,
             }
 
             if provider_type not in provider_classes:
