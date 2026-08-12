5b701f23e58a99a2be3da985c8140aed6454c588
balibabu <cike8899@users.noreply.github.com>
2026-08-12 19:08:48 +0800

    Fix: Preserve the text format returned by the API. (#18167)

--- web/src/components/top-select.tsx
+++ web/src/components/top-select.tsx
@@ -14,7 +14,7 @@
  *  limitations under the License.
  */
 
-import { useMemo } from 'react';
+import { forwardRef, useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { SelectWithSearch } from './originui/select-with-search';
 import { RAGFlowFormItem } from './ragflow-form';
@@ -24,24 +24,27 @@ type TopSelectProps = {
   onChange?(value: number): void;
 };
 
-export function TopSelect({ value = 10, onChange }: TopSelectProps) {
-  const { t } = useTranslation();
+export const TopSelect = forwardRef<HTMLButtonElement, TopSelectProps>(
+  function TopSelect({ value = 10, onChange }, ref) {
+    const { t } = useTranslation();
 
-  const sizeChangerOptions = useMemo(() => {
-    return [10, 20, 50, 100].map((x) => ({
-      label: <span>{t('common.top', { top: x })}</span>,
-      value: x.toString(),
-    }));
-  }, [t]);
+    const sizeChangerOptions = useMemo(() => {
+      return [10, 20, 50, 100].map((x) => ({
+        label: <span>{t('common.top', { top: x })}</span>,
+        value: x.toString(),
+      }));
+    }, [t]);
 
-  return (
-    <SelectWithSearch
-      options={sizeChangerOptions}
-      value={value.toString()}
-      onChange={(val) => onChange?.(Number(val))}
-    ></SelectWithSearch>
-  );
-}
+    return (
+      <SelectWithSearch
+        ref={ref}
+        options={sizeChangerOptions}
+        value={value.toString()}
+        onChange={(val) => onChange?.(Number(val))}
+      ></SelectWithSearch>
+    );
+  },
+);
 
 export function TopSelectFormItem() {
   const { t } = useTranslation();
