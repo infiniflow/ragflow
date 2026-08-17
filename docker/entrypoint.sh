68a55ee2282946768fe5986501779d7f88444e87
Evgeniy Goroshkin <engor@yandex.ru>
2026-08-17 15:19:28 +0700

    feat: send 'sigterm' into child processes of entrypoint, so they can gracefully stop

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -184,6 +184,15 @@ done < "${TEMPLATE_FILE}"
 export LD_LIBRARY_PATH="/usr/lib/x86_64-linux-gnu/"
 PY=python3
 
+shutdown() {
+    trap '' SIGTERM SIGINT
+    kill -TERM -- "-$$" 2>/dev/null || true
+    wait || true
+    exit 0
+}
+
+trap shutdown SIGTERM SIGINT
+
 # -----------------------------------------------------------------------------
 # Select Nginx Configuration based on API_PROXY_SCHEME
 # -----------------------------------------------------------------------------
