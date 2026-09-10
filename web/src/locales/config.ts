a3db8d28f689316566bb09ba69db57cfbedf3045
Annabelle <3141330934@qq.com>
2026-09-10 13:09:16 +0800

    Web - Add a "Cable industry" template category and render only sidebar categories   that actually have templates; extract the canvas-category helpers into   template-categories.ts with its own test. - Blank create and import JSON on /agents remain the native flows.
    Signed-off-by: Annabelle <3141330934@qq.com>

--- web/src/locales/config.ts
+++ web/src/locales/config.ts
@@ -14,20 +14,6 @@ import translation_en from './en';
 const languageImports: Record<string, () => Promise<{ default: any }>> = {
   [LanguageAbbreviation.En]: () => import('./en'),
   [LanguageAbbreviation.Zh]: () => import('./zh'),
-  [LanguageAbbreviation.ZhTraditional]: () => import('./zh-traditional'),
-  [LanguageAbbreviation.Id]: () => import('./id'),
-  [LanguageAbbreviation.Ja]: () => import('./ja'),
-  [LanguageAbbreviation.Es]: () => import('./es'),
-  [LanguageAbbreviation.Vi]: () => import('./vi'),
-  [LanguageAbbreviation.Ru]: () => import('./ru'),
-  [LanguageAbbreviation.PtBr]: () => import('./pt-br'),
-  [LanguageAbbreviation.De]: () => import('./de'),
-  [LanguageAbbreviation.Fr]: () => import('./fr'),
-  [LanguageAbbreviation.It]: () => import('./it'),
-  [LanguageAbbreviation.Bg]: () => import('./bg'),
-  [LanguageAbbreviation.Ar]: () => import('./ar'),
-  [LanguageAbbreviation.Tr]: () => import('./tr'),
-  [LanguageAbbreviation.Ko]: () => import('./ko'),
 };
 
 const supportedLanguageCodes: Intl.UnicodeBCP47LocaleIdentifier[] =
