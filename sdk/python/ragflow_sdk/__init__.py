ebd2b8e7a5dad8fa70ae684c3f2520d58af97ecd
idriss <isbaaoui09@gmail.com>
2026-09-11 15:40:18 +0800

    Remove remaining package-wide beartype hooks from imports

--- sdk/python/ragflow_sdk/__init__.py
+++ sdk/python/ragflow_sdk/__init__.py
@@ -14,10 +14,6 @@
 #  limitations under the License.
 #
 
-from beartype.claw import beartype_this_package
-
-beartype_this_package()
-
 import importlib.metadata
 
 from .ragflow import RAGFlow
--- tools/chatgpt-on-wechat/plugins/__init__.py
+++ tools/chatgpt-on-wechat/plugins/__init__.py
@@ -14,10 +14,6 @@
 #  limitations under the License.
 #
 
-from beartype.claw import beartype_this_package
-
-beartype_this_package()
-
 from .ragflow_chat import RAGFlowChat
 
 __all__ = ["RAGFlowChat"]
