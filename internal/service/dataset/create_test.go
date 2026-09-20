1f95a07248d7d93a436ca9458915240aa114178b
Wang Qi <wangq8@outlook.com>
2026-09-20 10:55:16 +0800

    Fix create build-in dataset parser_config should be pipeline config (#19860)

--- internal/service/dataset/create_test.go
+++ internal/service/dataset/create_test.go
@@ -87,9 +87,10 @@ func TestCreateDataset_ComponentParamsPopulated(t *testing.T) {
 	chunkMethod := "general"
 	parseType := 1
 	result, code, err := testDatasetCreateService(t).CreateDataset(ctx, &service.CreateDatasetRequest{
-		Name:      "ds-with-cp",
-		ParserID:  &chunkMethod,
-		ParseType: &parseType,
+		Name:         "ds-with-cp",
+		ParserID:     &chunkMethod,
+		ParserConfig: map[string]interface{}{},
+		ParseType:    &parseType,
 	}, "tenant-1")
 	if err != nil {
 		t.Fatalf("CreateDataset failed: %v", err)
