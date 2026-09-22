071f45998dd9eec48092643fa5e2a2f2aa55dce6
idriss <isbaaoui09@gmail.com>
2026-09-22 14:44:37 +0800

    fix ci

--- internal/handler/dataset.go
+++ internal/handler/dataset.go
@@ -1115,7 +1115,7 @@ func (h *DatasetsHandler) SearchDatasets(c *gin.Context) {
 		return
 	}
 	if req.DatasetIDs == nil {
-		common.ResponseWithCodeData(c, common.CodeDataError, nil, "`dataset_ids` is required.")
+		common.ResponseWithCodeData(c, common.CodeArgumentError, nil, "`dataset_ids` is required.")
 		return
 	}
 
