dd61963bfbb1f4727cf03bcd8f612e687ebcba8f
bill <yibie_jingnian@163.com>
2026-09-24 19:23:23 +0800

    Feat: Display wiki-related icons on the dataset's log page.

--- web/src/pages/dataset/dataset-overview/index.tsx
+++ web/src/pages/dataset/dataset-overview/index.tsx
@@ -256,7 +256,7 @@ const FileLogsPage: FC = () => {
     <Card
       className="
       p-5 min-w-[880px] mr-5 mb-5 bg-transparent shadow-none
-      flex flex-col overflow-y-auto scrollbar-auto"
+      flex flex-col min-h-0"
     >
       {/* Stats Cards */}
       <div className="grid grid-cols-3 md:grid-cols-3 gap-7 mb-6">
